8cd5af15627c351ba1e3c46421d12daa77addb72
VienNguyen24hdev <viennguyendeveloper@gmail.com>
2020-08-09 20:01:03 +0700

    feat(ui): add component Form

--- libs/ui/jest.config.js
+++ libs/ui/jest.config.js
@@ -9,5 +9,4 @@ module.exports = {
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
   coverageDirectory: '../../coverage/libs/ui',
-  setupFilesAfterEnv: ['./jest.setup.js'],
 }
--- libs/ui/src/components/form/Form.tsx
+++ libs/ui/src/components/form/Form.tsx
@@ -1,10 +1,7 @@
 import { MinusCircleOutlined, PlusOutlined } from '@ant-design/icons'
 import { Button, Space, Form as AntForm } from 'antd'
-import { FormProps as AntFormProps } from 'antd/lib/form/Form'
-import { FormItemProps as AntFormItemProps } from 'antd/lib/form/FormItem'
 import { StoreValue } from 'rc-field-form/lib/interface'
 import React from 'react'
-// import { FormListProps as AntFormListProps } from 'antd/lib/form/FormList'
 
 // Copy because not exported from antd
 interface FieldData {
@@ -24,50 +21,11 @@ interface AntFormListProps {
   children: Array<(fields: FieldData, operation: Operation) => React.ReactNode>
 }
 
-export type FormProps = AntFormProps
-
-export interface FormItemProps extends AntFormItemProps {
-  renderProps?: { field: FieldData; operation: Operation }
-}
-
 export type FormListProps = Omit<AntFormListProps, 'children'> & {
   children: React.ReactElement[]
 }
 
 export namespace Form {
-  export const Default: React.FC<FormProps> = ({ children, ...props }) => {
-    const { initialValues, ...rest } = props
-
-    return (
-      <AntForm initialValues={initialValues} {...rest}>
-        {children}
-      </AntForm>
-    )
-  }
-
-  export const Item: React.FC<FormItemProps> = ({ children, ...props }) => {
-    // console.log(props)
-
-    // const { label, name, renderProps, ...rest } = props
-    //
-    // /**
-    //  * Used for layout only
-    //  */
-    // if (!name) {
-    //   return <AntForm.Item>{children}</AntForm.Item>
-    // }
-    //
-    // // Convert name to array
-    // const normalizedName = Array.isArray(name) ? name : [name]
-    //
-    // const itemName =
-    //   renderProps?.field.name !== undefined
-    //     ? [renderProps?.field.name, ...normalizedName]
-    //     : normalizedName
-
-    return <AntForm.Item {...props}>{children}</AntForm.Item>
-  }
-
   export const List: React.FC<FormListProps> = ({ children, ...props }) => {
     const { name } = props
 
