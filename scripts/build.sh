7d0732128fd66910692c9bf219b98a4d9aa9de0d
Webber Wang <webber@codelab.ai>
2020-08-15 01:22:38 -0700

    ci: remove parallel build

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 rm -rf build
-nx run-many --target=build --all --parallel $@
-# nx run-many --target=build --all $@
+# yarn nx run-many --target=build --all --parallel $@
+yarn nx run-many --target=build --all $@
