d03833137eeea50e846fda21a48af2fe3af1b20d
Webber Wang <webber@codelab.ai>
2020-08-15 03:23:09 -0700

    ci: test eslint.sh

---
+++ scripts/eslint.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+FILES_SINCE_BASE_REVISION=$(git diff-tree --no-commit-id --name-only --diff-filter=d -r ${CIRCLE_BASE_REVISION} ${CIRCLE_REVISION})
+
+echo $FILES_SINCE_BASE_REVISION
