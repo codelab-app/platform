97f0bcc5be2b98a99e3bb5ea2821d74c82f53b4c
Webber Wang <webber@codelab.ai>
2020-08-31 16:05:17 -0700

    fix(libs-factory): fix eslint errors

--- libs/factory/src/app/builder/web/AppWeb-builder.tsx
+++ libs/factory/src/app/builder/web/AppWeb-builder.tsx
@@ -4,25 +4,24 @@ import {
   isTreeNode,
   traversePostOrder,
 } from '@codelab/graph'
-import { elementParameterFactory } from '@codelab/ui'
-import { UIOrder } from './AppWeb-builder--order'
 import { AppBuilder } from '../App-builder.i'
-import { WebAppProduct } from '../../products/web/WebApp-product'
 import { AppFactory } from '../../factories/App-factory.i'
 import { AppWebFactory } from '../../factories/AppWeb-factory'
+import { AppOrder } from './AppWeb-builder--order'
+import { AppWebProduct } from '../../products/web/AppWeb-product'
 
 export class AppWebBuilder extends AppBuilder {
-  protected create(order: UIOrder): WebAppProduct {
-    let product: WebAppProduct
+  protected create(order: AppOrder): AppWebProduct {
+    let product: AppWebProduct
 
     if (isReactNode(order)) {
       const factory: AppFactory = new AppWebFactory(order)
 
       // Initialize which functions to use
-      factory.factory = elementParameterFactory
-      factory.traversal = traversePostOrder
+      // factory.factory = elementParameterFactory
+      // factory.traversal = traversePostOrder
 
-      product = new WebAppProduct(factory)
+      product = new AppWebProduct(factory)
     } else if (isTreeNode(order)) {
       //
     } else if (isRefNode(order)) {
