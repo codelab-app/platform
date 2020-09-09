22aebd3515605a740199c42121e6153a6755eca4
Webber Wang <webber@codelabstudios.com>
2020-09-09 03:00:15 -0700

    refactor(libs-tools-plugins-codelab): move plugins under tools dir

--- libs/tools/plugins/codelab/jest.config.js
+++ libs/tools/plugins/codelab/jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  name: 'plugins-plug',
-  preset: '../../../jest.config.js',
+  name: 'tools-plugins-codelab',
+  preset: '../../../../jest.config.js',
   globals: {
     'ts-jest': {
       tsConfig: '<rootDir>/tsconfig.spec.json',
@@ -10,5 +10,5 @@ module.exports = {
     '^.+\\.[tj]sx?$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
-  coverageDirectory: '../../../coverage/libs/plugins/plug',
+  coverageDirectory: '../../../../coverage/libs/plugins-codelab',
 }
--- libs/tools/plugins/plug/jest.config.js
+++ libs/tools/plugins/plug/jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  name: 'plugins-codelab',
-  preset: '../../../jest.config.js',
+  name: 'tools-plugins-plug',
+  preset: '../../../../jest.config.js',
   globals: {
     'ts-jest': {
       tsConfig: '<rootDir>/tsconfig.spec.json',
@@ -10,5 +10,5 @@ module.exports = {
     '^.+\\.[tj]sx?$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
-  coverageDirectory: '../../../coverage/libs/plugins-codelab',
+  coverageDirectory: '../../../../coverage/libs/tools/plugins/plug',
 }
