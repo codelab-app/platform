15af886283787518bd586a7619b8fa5824cfe130
Webber Wang <webber@codelab.ai>
2020-10-05 19:18:18 -0700

    ci: remove unused scripts

--- scripts/test.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-if [ "$CI" == true ]; then
-  npx nx run-many --target=test --all --parallel --maxWorkers=4 --silent "$@"
-else
-  npx nx affected:test --parallel --silent "$@"
-fi
