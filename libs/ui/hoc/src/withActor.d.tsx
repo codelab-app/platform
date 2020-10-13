b3d090b261b65740ec2fadeadb32e5f44d2b0c99
Webber Wang <webber@codelab.ai>
2020-10-12 23:57:02 -0700

    feat: add custom typeroots directory

--- libs/ui/hoc/src/withActor.d.tsx
+++
@@ -1,9 +0,0 @@
-/**
- * Add TContext to useActor
- */
-// declare module '@xstate/react' {
-//   function useActor<TContext, TEvent extends EventObject, TEmitted = any>(
-//     actorRef: ActorRefLike<TEvent, TEmitted> | Actor<TContext, TEvent>,
-//     getSnapshot?: (actor: typeof actorRef) => TEmitted,
-//   ): [TEmitted, Sender<TEvent>]
-// }
--- types/@xstate/react/index.d.ts
+++ types/@xstate/react/index.d.ts
@@ -1,8 +1,6 @@
 import { Actor, EventObject, Sender } from 'xstate'
 import { ActorRefLike } from '@xstate/react/lib/types'
 
-declare module 'express-restify-mongoose'
-declare module 'deepdash/standalone'
 declare module '@xstate/react' {
   function useActor<TContext, TEvent extends EventObject, TEmitted = any>(
     actorRef: ActorRefLike<TEvent, TEmitted> | Actor<TContext, TEvent>,
