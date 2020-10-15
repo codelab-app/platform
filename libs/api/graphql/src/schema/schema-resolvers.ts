8f05b0ce75230b9f2b1d17d77c1b15c763390b29
Webber Wang <webber@codelabstudios.com>
2020-10-15 15:13:46 -0700

    feat(libs-api-graphql): load from schema graphql file
    feat(libs-api-graphql): load from schema graphql file

---
+++ libs/api/graphql/src/schema/schema-resolvers.ts
@@ -0,0 +1,23 @@
+export const resolvers = {
+  // root entry point to GraphQL service
+  Query: {
+    // Movie(object: any, params: any, ctx: any, resolveInfo: any) {
+    //   return neo4jgraphql(object, params, ctx, resolveInfo, true)
+    // },
+    // MoviesByYear(object: any, params: any, ctx: any, resolveInfo: any) {
+    //   return neo4jgraphql(object, params, ctx, resolveInfo, true)
+    // },
+    // AllMovies(object: any, params: any, ctx: any, resolveInfo: any) {
+    //   return neo4jgraphql(object, params, ctx, resolveInfo, true)
+    // },
+    // MovieById(object: any, params: any, ctx: any, resolveInfo: any) {
+    //   return neo4jgraphql(object, params, ctx, resolveInfo, true)
+    // },
+    // GenresBySubstring(object: any, params: any, ctx: any, resolveInfo: any) {
+    //   return neo4jgraphql(object, params, ctx, resolveInfo, true)
+    // },
+    // Books(object: any, params: any, ctx: any, resolveInfo: any) {
+    //   return neo4jgraphql(object, params, ctx, resolveInfo, true)
+    // },
+  },
+}
