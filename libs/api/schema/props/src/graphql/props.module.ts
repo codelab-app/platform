c7bcc0a28929e2466db9530ca1e656da0122742c
Webber Wang <webber@codelab.ai>
2020-10-18 15:55:29 -0700

    revert(apps-api-gateway): re-add restify mongoose

--- libs/api/schema/props/src/graphql/props.module.ts
+++ libs/api/schema/props/src/graphql/props.module.ts
@@ -1,31 +1,8 @@
-import { join } from 'path'
-import { Logger, Module } from '@nestjs/common'
-import { GraphQLFederationModule } from '@nestjs/graphql'
-import { Node } from './model'
+import { Module } from '@nestjs/common'
 import { PropsService } from './props.service'
 import { NodesResolver, PropsResolver } from './resolvers'
 
 @Module({
-  // Resolvers must be added to same level as GraphQLFederationModule
-  imports: [
-    GraphQLFederationModule.forRootAsync({
-      useFactory: () => {
-        return {
-          autoSchemaFile: join(
-            process.cwd(),
-            'apps/api/services/props/src/schema.gql',
-          ),
-          // here provide all the types that are missing in schema
-          // since we're not importing .graphql typedefs
-          buildSchemaOptions: { orphanedTypes: [Node] },
-        }
-      },
-    }),
-  ],
   providers: [PropsResolver, NodesResolver, PropsService],
 })
-export class PropsModule {
-  constructor() {
-    Logger.log('Starting!')
-  }
-}
+export class PropsModule {}
