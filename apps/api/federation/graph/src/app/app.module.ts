7c9cc7b7e14767069ed26ec8d605b9b3b21ab7ce
Webber Wang <webber@codelab.ai>
2020-10-23 15:06:28 -0700

    refactor(libs-api-services-node): create neo4j schema service
    refactor(libs-api-services-node): create neo4j schema service
    
    refactor(libs-api-services-node): create neo4j schema service

--- apps/api/federation/graph/src/app/app.module.ts
+++ apps/api/federation/graph/src/app/app.module.ts
@@ -1,13 +1,42 @@
 import { Module } from '@nestjs/common'
+import { GraphQLModule } from '@nestjs/graphql'
+import { Driver } from 'neo4j-driver'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { ConfigModule } from '@codelab/api/providers/config'
 import { LoggerModule } from '@codelab/api/providers/logger'
-import { Neo4jNodeModule } from '@codelab/api/services/node'
+import {
+  GraphqlNodeModule,
+  NEO4J_DRIVERS_PROVIDER,
+  Neo4jNodeModule,
+  Neo4jSchemaService,
+} from '@codelab/api/services/node'
 import { GrpcPropsClientModule } from '@codelab/api/services/props'
 
 @Module({
-  imports: [GrpcPropsClientModule, LoggerModule, ConfigModule, Neo4jNodeModule],
+  imports: [
+    GrpcPropsClientModule,
+    LoggerModule,
+    ConfigModule,
+    Neo4jNodeModule,
+    GraphQLModule.forRootAsync({
+      imports: [Neo4jNodeModule],
+      inject: [NEO4J_DRIVERS_PROVIDER, Neo4jSchemaService],
+      useFactory: (driver: Driver, schemaService: Neo4jSchemaService) => {
+        return {
+          include: [GraphqlNodeModule],
+          autoSchemaFile: true,
+          transformSchema: schemaService.transformSchema,
+          transformAutoSchemaFile: true,
+          context: (ctx) => ({
+            ...ctx,
+            driver,
+          }),
+        }
+      },
+    }),
+    GraphqlNodeModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- libs/api/services/node/src/neo4j/neo4j-node.module.ts
+++ libs/api/services/node/src/neo4j/neo4j-node.module.ts
@@ -1,54 +1,14 @@
-import { buildFederatedSchema } from '@apollo/federation'
 import { Module } from '@nestjs/common'
-import { GraphQLModule } from '@nestjs/graphql'
-import { GraphQLSchema } from 'graphql'
-import { printSchema } from 'graphql/utilities'
-import { makeAugmentedSchema } from 'neo4j-graphql-js'
-import { extractResolversFromSchema } from 'neo4j-graphql-js/dist/augment/resolvers'
 import { GraphqlNodeModule } from '../graphql'
-import { NEO4J_DATABASE_PROVIDER, Neo4jDatabaseModule } from './database'
 import { Neo4jNodeController } from './neo4j-node.controller'
 import { Neo4jNodeService } from './neo4j-node.service'
+import { Neo4jSchemaService } from './neo4j-schema.service'
+import { NEO4J_DRIVERS_PROVIDER, neo4DriversProvider } from './neo4j.provider'
 
 @Module({
-  imports: [
-    Neo4jDatabaseModule,
-    GraphqlNodeModule,
-    GraphQLModule.forRootAsync({
-      imports: [Neo4jDatabaseModule],
-      inject: [NEO4J_DATABASE_PROVIDER],
-      useFactory: (driver) => {
-        return {
-          include: [GraphqlNodeModule],
-          autoSchemaFile: true,
-          // Here schema already has federation added
-          transformSchema: (schema: GraphQLSchema) => {
-            const resolvers = extractResolversFromSchema(schema)
-
-            // Our user defined schema
-            const typeDefs: string = printSchema(schema)
-
-            const neo4jExtendedSchema = makeAugmentedSchema({
-              resolvers,
-              typeDefs,
-              config: {
-                isFederated: true,
-              },
-            })
-
-            return buildFederatedSchema([neo4jExtendedSchema])
-          },
-          transformAutoSchemaFile: true,
-          context: (ctx) => ({
-            ...ctx,
-            driver,
-          }),
-        }
-      },
-    }),
-  ],
+  imports: [GraphqlNodeModule],
   controllers: [Neo4jNodeController],
-  providers: [Neo4jNodeService],
-  exports: [],
+  providers: [Neo4jNodeService, neo4DriversProvider, Neo4jSchemaService],
+  exports: [NEO4J_DRIVERS_PROVIDER, Neo4jSchemaService],
 })
 export class Neo4jNodeModule {}
