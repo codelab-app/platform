bab71d783028bbfcc01574815f210a998caf50bd
artonio <asukhovatkin@gmail.com>
2020-10-23 21:27:31 -0400

    hasura connection wip

---
+++ apps/api/external/src/app/app.module.ts
@@ -0,0 +1,45 @@
+import { Module } from '@nestjs/common'
+import { TypeOrmModule, TypeOrmModuleAsyncOptions } from '@nestjs/typeorm';
+
+
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import {ConfigModule} from "./config/config.module"
+import {ConfigService} from "./config/config.service"
+import {GraphQLModule} from "@nestjs/graphql"
+import {GraphqlService} from "./graphql/graphql.service"
+import { GraphQLSchema } from "graphql";
+
+@Module({
+  imports: 
+  [
+    ConfigModule,
+    TypeOrmModule.forRootAsync({
+    imports: [ConfigModule],
+    inject: [ConfigService],
+    useFactory: async (config: ConfigService) => {
+      return {
+        host: config.dbHost,
+        type: config.dbType,
+        port: config.dbPort,
+        username: config.dbUsername,
+        password: config.dbPassword,
+        database: config.db,
+        entities: [
+        ],
+        extra: {
+          connectionLimit: 5
+        }
+      } as TypeOrmModuleAsyncOptions
+    }
+  }),
+  GraphQLModule.forRootAsync({
+    imports: [ConfigModule],
+    useClass: GraphqlService,
+    inject: [ConfigService]
+  }),
+],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
