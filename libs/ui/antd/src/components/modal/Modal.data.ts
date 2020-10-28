cee4075b2781bd58e6ecbb14d8d718f52bb1e171
Webber Wang <webber@codelab.ai>
2020-10-28 01:00:25 -0700

    chore: merged with master

--- libs/ui/antd/src/components/modal/Modal.data.ts
+++ libs/ui/antd/src/components/modal/Modal.data.ts
@@ -1,60 +1,35 @@
-import { Button } from '../button'
-import { Text } from '../text'
-import { Modal } from '.'
 import { NodeReactI, NodeType } from '@codelab/shared/interface/node'
 import { PropType } from '@codelab/shared/interface/props'
+import { Button, Modal, Text } from '@codelab/ui/antd'
 
-export const modalData: NodeReactI<Modal.Props | Button.Props | Text.Props> = {
-  type: NodeType.React_Fragment,
+export const modalButton: NodeReactI<Button.Props | Text.Props> = {
+  type: NodeType.React_Button,
   props: {
-    ctx: {
-      __type: [PropType.Eval, PropType.Leaf],
-      value:
-        'const [visible, setVisible] = this.React.useState(false); return { visible, setVisible }',
+    type: 'primary',
+    onClick: {
+      __type: [PropType.Eval],
+      value: `return () => this.send({ type: 'OPEN' })`,
     },
   },
-  children: [
-    {
-      type: NodeType.React_Button,
-      props: {
-        type: 'primary',
-        onClick: {
-          __type: [PropType.Eval],
-          value: 'console.log(this); return () => this.ctx.setVisible(true)',
-        },
-      },
-      children: [{ type: NodeType.React_Text, props: { value: 'Open modal' } }],
+  children: [{ type: NodeType.React_Text, props: { value: 'Open modal' } }],
+}
+
+export const modal: NodeReactI<Modal.Props | Text.Props> = {
+  type: NodeType.React_Modal,
+  props: {
+    title: 'Basic Modal',
+    footer: null,
+    onOk: {
+      __type: [PropType.Eval],
+      value: `return () => this.send({ type: 'CLOSE' })`,
     },
-    {
-      type: NodeType.React_Modal,
-      props: {
-        title: 'Basic Modal',
-        onOk: {
-          __type: [PropType.Eval],
-          value: 'return () => this.ctx.setVisible(false)',
-        },
-        onCancel: {
-          __type: [PropType.Eval],
-          value: 'return () => this.ctx.setVisible(false)',
-        },
-        visible: {
-          __type: [PropType.Eval],
-          value: 'return this.ctx.visible',
-        },
-      },
-      children: [
-        {
-          type: NodeType.React_Html_P,
-          children: [
-            {
-              type: NodeType.React_Text,
-              props: {
-                value: 'Some contents...',
-              },
-            },
-          ],
-        },
-      ],
+    onCancel: {
+      __type: [PropType.Eval],
+      value: `return () => {this.send({ type: 'CLOSE' }); this.handlecancel()}`,
     },
-  ],
+    visible: {
+      __type: [PropType.Eval],
+      value: 'return this.state.context.visible',
+    },
+  },
 }
