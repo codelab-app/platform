bbf1509d0c0b6ecbdefab113044ada7e860f4d51
Webber Wang <webber@codelab.ai>
2020-11-01 21:16:48 -0800

    ci: turn on dev filters instead of staging

--- .kubernetes/setup/ambassador.sh
+++ .kubernetes/setup/ambassador.sh
@@ -6,18 +6,23 @@
 # https://www.getambassador.io/docs/latest/topics/install/helm/
 #
 
-helm repo add datawire https://www.getambassador.io
+# helm repo add datawire https://www.getambassador.io
 
-kubectl create namespace ambassador
+# kubectl create namespace ambassador
 
-helm install ambassador --namespace ambassador datawire/ambassador
+# helm upgrade --install ambassador --namespace ambassador datawire/ambassador
 
-edgectl install
+# edgectl install
 
-#  kubectl apply -f https://www.getambassador.io/yaml/aes-crds.yaml
+#
+# Kubectl Method
+#
+# https://www.getambassador.io/docs/latest/topics/install/yaml-install/
 
-#  kubectl wait --for condition=established --timeout=90s crd -lproduct=aes
+kubectl apply -f https://www.getambassador.io/yaml/aes-crds.yaml
 
-#  kubectl apply -f https://www.getambassador.io/yaml/aes.yaml
+kubectl wait --for condition=established --timeout=90s crd -lproduct=aes
 
-#  kubectl -n ambassador wait --for condition=available --timeout=90s deploy -lproduct=aes
+kubectl apply -f https://www.getambassador.io/yaml/aes.yaml
+
+kubectl -n ambassador wait --for condition=available --timeout=90s deploy -lproduct=aes
