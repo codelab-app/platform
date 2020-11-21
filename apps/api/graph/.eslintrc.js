98d6801294fdebe44fa95e9b5996509e98311e15
artonio <asukhovatkin@gmail.com>
2020-11-20 22:18:33 -0500

    feat(apps-api-graph): implemented graph and added tests

--- apps/api/graph/.eslintrc.js
+++ apps/api/graph/.eslintrc.js
@@ -17,5 +17,6 @@ module.exports = {
     'import/no-extraneous-dependencies': 'off',
     '@typescript-eslint/no-useless-constructor': 'off',
     '@typescript-eslint/no-empty-function': 'off',
+    'no-param-reassign': 'off',
   },
 }
---
+++ apps/api/graph/src/providers/graph/graph.ts
@@ -0,0 +1,107 @@
+import cytoscape, { EdgeDefinition, NodeDefinition } from 'cytoscape'
+import { merge } from 'lodash'
+import objectMapper from 'object-mapper'
+import { v4 as uuidv4 } from 'uuid'
+import { EdgeA, VertexA } from '@codelab/shared/interface/graph-v2'
+
+export class Graph {
+  declare vertices: Array<VertexA>
+
+  declare edges: Array<EdgeA>
+
+  constructor(vertices: Array<VertexA> = [], edges: Array<EdgeA> = []) {
+    this.vertices = vertices
+    this.edges = edges
+  }
+
+  moveVertex(source: VertexA, target: VertexA) {}
+
+  addVertex(v: VertexA): void {
+    if (!this.hasVertex(v.id)) {
+      this.vertices.push(v)
+    }
+  }
+
+  addVertices(vertices: Array<VertexA>): void {
+    vertices.forEach((v: VertexA) => {
+      if (!this.hasVertex(v.id)) {
+        this.vertices.push(v)
+      }
+    })
+  }
+
+  addEdge(source: VertexA, target: VertexA): void {
+    if (!this.hasVertex(source.id)) {
+      throw new Error(`Vertex with source id ${source.id} does not exist`)
+    }
+
+    if (!this.hasVertex(target.id)) {
+      throw new Error(`Vertex with target id ${target.id} does not exist`)
+    }
+
+    if (!this.hasEdge(source.id, target.id)) {
+      target.parent = source.id
+      const edge: EdgeA = {
+        id: uuidv4(),
+        start: source.id,
+        end: target.id,
+      }
+
+      this.edges.push(edge)
+    }
+  }
+
+  hasVertex(vertexId: string): boolean {
+    const index = this.vertices.findIndex((v: VertexA) => {
+      return v.id === vertexId
+    })
+
+    return index !== -1
+  }
+
+  hasEdge(sourceId: string, targetId: string): boolean {
+    const index = this.edges.findIndex((e: EdgeA) => {
+      return e.start === sourceId && e.end === targetId
+    })
+
+    return index !== -1
+  }
+
+  public makeCytoscape(graph: Graph): cytoscape.Core {
+    return cytoscape({
+      headless: true,
+      elements: {
+        nodes: this.cyMapVertices(graph.vertices),
+        edges: this.cyMapEdges(graph.edges),
+      },
+    })
+  }
+
+  private cyMapEdges(edges: Array<EdgeA>): Array<EdgeDefinition> {
+    const mapper = {
+      id: 'data.id',
+      start: 'data.source',
+      end: 'data.target',
+    }
+
+    return edges.map((edge) => {
+      return objectMapper<EdgeDefinition>(edge, mapper)
+    })
+  }
+
+  private cyMapVertices(
+    vertices: Array<Partial<VertexA>>,
+  ): Array<NodeDefinition> {
+    const mapper = {
+      id: 'data.id',
+      parent: 'data.parent',
+    }
+
+    return vertices.map((vertex) => {
+      // Spread rest of vertex props
+      return merge(objectMapper<NodeDefinition>(vertex, mapper), {
+        data: { ...vertex },
+      })
+    })
+  }
+}
