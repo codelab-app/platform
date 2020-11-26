f6305028697d3138e90a2317c4e5ef9528d70757
Aleksandr Burtsev <aburcev@gmail.com>
2020-11-26 19:30:04 +0300

    feat(libs-ui-antd): #323 render graph components

--- libs/ui/antd/.storybook/preview.js
+++ libs/ui/antd/.storybook/preview.js
@@ -1,5 +1,7 @@
 import { withKnobs } from '@storybook/addon-knobs'
 import { addDecorator } from '@storybook/react'
 import '../../../../.storybook/preview'
+import { withApollo } from "./decorators/withApollo"
 
 addDecorator(withKnobs)
+addDecorator(withApollo)
--- libs/ui/antd/src/components/button/Button.stories.tsx
+++ libs/ui/antd/src/components/button/Button.stories.tsx
@@ -1,6 +1,7 @@
+/* eslint-disable no-shadow*/
 import React from 'react'
 import { buttonData, buttonEvalData } from './Button.data'
-import { Renderer } from '@codelab/core/renderer'
+import { IGraphData, Renderer } from '@codelab/core/renderer'
 
 export default {
   title: 'Button',
@@ -22,4 +23,25 @@ export const EvalButton = () => {
   const Button = Renderer.components(buttonEvalData)
 
   return <Button />
+
+}
+
+export interface IGraphStroyArgs<T = IGraphData> {
+  fetched: boolean | undefined,
+  data: T | undefined
+}
+
+export const GraphButton = (args: IGraphStroyArgs) => {
+  if (args.fetched) {
+    const GraphButton = Renderer.graphComponents(args.data as IGraphData);
+    return <GraphButton />
+  } else {
+
+    return <div>loading...</div>
+  }
+}
+
+
+GraphButton.parameters = {
+  graphLabel: 'button-graph',
 }
