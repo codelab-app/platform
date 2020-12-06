d9b5de5e9fb1b5a7500f877536198f290bc4bd59
Webber Wang <webber@codelab.ai>
2020-12-06 19:43:03 +0800

    fix: use .env instead of .env.dev

--- libs/ddd/modules/users/core/application/useCases/createUser/CreateUserUseCase.spec.ts
+++ libs/ddd/modules/users/core/application/useCases/createUser/CreateUserUseCase.spec.ts
@@ -1,28 +1,38 @@
+import { CommandBus, CqrsModule } from '@nestjs/cqrs'
+import { Test } from '@nestjs/testing'
+import {
+  handlerProviders,
+  persistenceProviders,
+  useCaseProviders,
+} from '../../../../infrastructure/adapter/UserModule'
+import { CreateUserCommand } from '../../commands/CreateUserCommand'
+
 describe('CreateUserUseCase', () => {
   let UserModule: any
 
-  it('works', () => {
-    expect(true).toBeTruthy()
+  beforeAll(async () => {
+    UserModule = await Test.createTestingModule({
+      imports: [CqrsModule],
+      providers: [
+        ...persistenceProviders,
+        ...useCaseProviders,
+        ...handlerProviders,
+      ],
+    }).compile()
   })
-  // beforeAll(async () => {
-  //   UserModule = await Test.createTestingModule({
-  //     imports: [CqrsModule],
-  //     providers: [...useCaseProviders, ...handlerProviders],
-  //   }).compile()
-  // })
 
-  // it('throws an error when an email is taken', async () => {
-  //   const commandBus: CommandBus = UserModule.select(CqrsModule).get(CommandBus)
+  it('throws an error when an email is taken', async () => {
+    const commandBus: CommandBus = UserModule.select(CqrsModule).get(CommandBus)
 
-  //   const results = await commandBus.execute(
-  //     new CreateUserCommand({
-  //       email: 'admin@codelab.ai',
-  //       password: 'password',
-  //     }),
-  //   )
+    const results = await commandBus.execute(
+      new CreateUserCommand({
+        email: 'admin@codelab.ai',
+        password: 'password',
+      }),
+    )
 
-  //   expect(results).toBeTruthy()
-  // })
+    expect(results).toBeTruthy()
+  })
 
   // it('validates the request', () => {
   //   const email = new UserEmail({ value: 'admin@codelab.ai' })
