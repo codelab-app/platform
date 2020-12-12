0996bea688630bdf7b142d82e71daacf6bd4d800
artonio <asukhovatkin@gmail.com>
2020-12-11 22:10:12 -0500

    feat(apps-api-codelab): added DeleteUserUseCase and EditUserUseCase.ts

---
+++ libs/ddd/modules/users/src/infrastructure/persistence/TypeOrmUserRepositoryAdapter.ts
@@ -0,0 +1,40 @@
+import { DeleteResult, EntityRepository } from 'typeorm'
+import { BaseRepository } from 'typeorm-transactional-cls-hooked'
+import { FindUserBy } from '../../common/CommonTypes'
+import { UserRepositoryPort } from '../../core/adapters/UserRepositoryPort'
+import { User } from '../../core/domain/user'
+import { UserEmail } from '../../core/domain/user-email'
+import { TypeOrmUser } from '@codelab/ddd/backend'
+
+@EntityRepository(TypeOrmUser)
+export class TypeOrmUserRepositoryAdapter
+  extends BaseRepository<TypeOrmUser>
+  implements UserRepositoryPort {
+  async exists(searchBy: FindUserBy): Promise<boolean> {
+    const entity = await this.findOne(searchBy)
+
+    return !!entity
+  }
+
+  async createUser(user: User): Promise<TypeOrmUser> {
+    const u = new TypeOrmUser()
+
+    u.email = user.email.toString()
+    u.password = user.password.value
+
+    return this.save(u)
+  }
+
+  async deleteUser(email: UserEmail): Promise<DeleteResult> {
+    return this.delete({ email: email.toString() })
+  }
+
+  async updateUser(user: User): Promise<User> {
+    const u = new TypeOrmUser()
+
+    u.email = user.email.toString()
+    await this.update({ email: user.email.toString() }, u)
+
+    return Promise.resolve(user)
+  }
+}
