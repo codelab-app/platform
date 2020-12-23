b5e557ac4d756dd33e44ca7ec372d5254316ef72
Aleksandr Burtsev <aburcev@gmail.com>
2020-12-23 17:40:27 +0300

    feat(apps): json-schema-generator. generate schemas based on pick

--- json-schema-generator/generate-props-names.ts
+++ json-schema-generator/generate-props-names.ts
@@ -1,14 +1,12 @@
 import { readdirSync } from 'fs'
 import { resolve } from 'path'
 
-const ANTD_ROOT_PATH = resolve('./node_modules/antd/lib')
+const COMPONENTS_ROOT_PATH = resolve('./libs/alpha/ui/antd/src/components')
 
 const getDirectories = (source: string) =>
   readdirSync(source, { withFileTypes: true })
     .filter((dirent) => dirent.isDirectory())
     .map((dirent) => dirent.name)
-
-const filterUtilsFolders = (dirName: string) => dirName.charAt(0) !== '_'
 
 const convertKebabToCamel = (str: string) =>
   str
@@ -16,10 +14,9 @@ const convertKebabToCamel = (str: string) =>
     .map((w) => w[0].toUpperCase() + w.substr(1))
     .join('')
 
-const addPropsSuffix = (propName: string) => `${propName}Props`
+const addPropsSuffix = (propName: string) => `${propName}.AntdProps`
 
 export const getAntdPropsNames = () =>
-  getDirectories(ANTD_ROOT_PATH)
-    .filter(filterUtilsFolders)
+  getDirectories(COMPONENTS_ROOT_PATH)
     .map(convertKebabToCamel)
     .map(addPropsSuffix)
--- json-schema-generator/json-schema-generator.ts
+++ json-schema-generator/json-schema-generator.ts
@@ -1,6 +1,5 @@
-import { writeFile } from 'fs'
+import { unlink, createWriteStream, writeFile, existsSync, mkdirSync } from 'fs'
 import { join, resolve } from 'path'
-import { pipe } from 'ramda'
 import { getAntdPropsNames } from './generate-props-names'
 import * as TJS from 'typescript-json-schema'
 
@@ -11,9 +10,14 @@ const outputRoot = resolve(
   `./libs/alpha/ui/component/src/form/graph/json-schemas/`,
 )
 
+if (!existsSync(outputRoot)) {
+  mkdirSync(outputRoot)
+}
+
 const settings: TJS.PartialArgs = {
   required: true,
   ref: false,
+  ignoreErrors: true,
 }
 
 const compilerOptions = {
@@ -21,82 +25,56 @@ const compilerOptions = {
 }
 
 const program = TJS.getProgramFromFiles(
-  [resolve('./node_modules/antd/lib/index.d.ts')],
+  [resolve('./libs/alpha/ui/antd/src/components/index.ts')],
   compilerOptions,
 )
 const generator = TJS.buildGenerator(program, settings)
+const allSymbols = generator.getUserSymbols()
 
-const cleanReactDependencies = (definition: TJS.Definition): TJS.Definition => {
-  const { properties, required = [], ...rest } = definition
+const schemasFilePath = join(outputRoot, `schemas.ts`)
 
-  const [nextRequired, nexProperties] = Object.keys(properties).reduce(
-    (acc, curr) => {
-      const prop = properties[curr]
-      const [accRequired, accProperties] = acc
-
-      if ((prop as any)?.$ref?.includes('React')) {
-        // if we filter some props, we should be sure that they will be excluded from 'required'
-        return [
-          accRequired.filter((propName) => propName !== curr),
-          accProperties,
-        ]
-      }
-
-      return [accRequired, { ...accProperties, [curr]: prop }]
-    },
-    [required, {}],
-  )
-
-  return {
-    ...rest,
-    required: [...nextRequired],
-    properties: {
-      ...nexProperties,
-    },
+// clear schemas file
+unlink(schemasFilePath, (err) => {
+  if (err) throw err
+  console.log(`${schemasFilePath} was deleted`)
+})
+writeFile(schemasFilePath, '', (err) => {
+  if (err) {
+    console.log(err)
   }
-}
+})
 
-const cleanStyleDependencies = (definition: TJS.Definition): TJS.Definition => {
-  const { properties, ...rest } = definition
-  const { style, ...restProperties } = properties
-  return {
-    ...rest,
-    properties: {
-      ...restProperties,
-    },
-  }
-}
-
-const componentsWithErrors = [
-  'CarouselProps',
-  'DatePickerProps',
-  'DrawerProps',
-  'DropdownProps',
-  'FormProps',
-  'GridProps',
-  'IconProps',
-  'ImageProps',
-  'LayoutProps'
-]
+const stream = createWriteStream(schemasFilePath, { flags: 'a' })
 
 getAntdPropsNames().forEach((typeName) => {
-  if (componentsWithErrors.includes(typeName)) {
+  if (!allSymbols.includes(typeName)) {
     return
   }
-  console.log(typeName)
-  const propsSchema = pipe(
-    (symbol: string) => generator.getSchemaForSymbol(symbol),
-    cleanReactDependencies,
-    cleanStyleDependencies,
-  )(typeName)
 
-  writeFile(
-    join(outputRoot, `${typeName}Schema.json`),
-    JSON.stringify(propsSchema, null, 2),
-    (err) => {
-      if (err) {
-        console.log(err)
-      }
-    },
+  console.log('JSON-schema generated for: ', typeName)
+  const propsSchema = generator.getSchemaForSymbol(typeName)
+  const exportedTypeName = typeName.replace('.AntdProps', '')
+
+  // writeFile(
+  //   join(outputRoot, `${exportedTypeName}.schemas.ts`),
+  //   `export const ${exportedTypeName} = ${JSON.stringify(
+  //     propsSchema,
+  //     null,
+  //     2,
+  //   )}`,
+  //   (err) => {
+  //     if (err) {
+  //       console.log(err)
+  //     }
+  //   },
+  // )
+  stream.write(
+    `export const ${exportedTypeName} = ${JSON.stringify(
+      propsSchema,
+      null,
+      2,
+    )}\n\n`,
   )
 })
+
+stream.end()
