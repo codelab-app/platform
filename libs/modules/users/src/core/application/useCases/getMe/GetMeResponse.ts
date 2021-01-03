0825ee399d59930e57903c1d4b57cec6bebfda5f
artonio <asukhovatkin@gmail.com>
2021-01-02 19:14:48 -0500

    fix(apps-api-codelab): fix get me response

--- libs/modules/users/src/core/application/useCases/getMe/GetMeResponse.ts
+++ libs/modules/users/src/core/application/useCases/getMe/GetMeResponse.ts
@@ -3,4 +3,4 @@ import { User } from '../../../domain/user'
 import { GetMeErrors } from './GetMeErrors'
 import { Result } from '@codelab/backend'
 
-export type GetMeResponse = Either<GetMeErrors.DemoError, Result<User>>
+export type GetMeResponse = Either<GetMeErrors.UserNotFoundError, Result<User>>
