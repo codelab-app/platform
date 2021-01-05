e94d264dcd8777d607b43b33d8f4197f3ced4e8f
Webber Wang <webber@codelab.ai>
2021-01-05 16:26:26 +0800

    refactor: rename resolver return type

--- libs/modules/user/src/presentation/UserDto.ts
+++ libs/modules/user/src/presentation/UserDto.ts
@@ -1,7 +1,7 @@
 import { Field, ObjectType } from '@nestjs/graphql'
 
-@ObjectType()
-export class UserUseCaseDto {
+@ObjectType('User')
+export class UserDto {
   @Field()
   public declare id: string
 
