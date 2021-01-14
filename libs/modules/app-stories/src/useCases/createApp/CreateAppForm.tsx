a844d1344e54e191faf93ef06dd318dfc4bfb6d3
Anton Mihaylov <antonmmihaylov@gmail.com>
2021-01-14 19:25:42 +0200

    refactor(libs-modules-app-stories): #470 refactored CreateAppFrom to use the new ModalForm

--- libs/modules/app-stories/src/useCases/createApp/CreateAppForm.tsx
+++ libs/modules/app-stories/src/useCases/createApp/CreateAppForm.tsx
@@ -1,56 +1,31 @@
-import { Theme as AntDTheme } from '@rjsf/antd'
-import { withTheme } from '@rjsf/core'
 import { JSONSchema7 } from 'json-schema'
-import React, { useState } from 'react'
-import { CreateAppInput } from '../../../../app/src/core/application/useCases/createApp/CreateAppInput'
+import React from 'react'
+import GeneratedXStateForm, {
+  GeneratedXStateFormProps,
+} from '../../../../../frontend/src/components/generated-form/GeneratedXStateForm'
 import { CreateAppInputSchema } from '../../../../app/src/core/application/useCases/createApp/CreateAppInput.generated'
+import { LoginUserInput } from '../../../../user/src/core/application/useCases/loginUser/LoginUserInput'
 import { useAppMachine } from '../../model'
 
-const Form = withTheme(AntDTheme)
+export type CreateAppFormProps = Omit<
+  GeneratedXStateFormProps<LoginUserInput, any>,
+  'schema' | 'rjsfFormProps' | 'send' | 'createSubmitEvent'
+>
 
-// This, LoginForm and RegisterForm are very similar, if we create more similar forms, maybe
-// it's a good idea to create a generic modal form
-export const CreateAppForm = ({
-  formId,
-  hasSubmitButton,
-  submitBtnRef,
-}: {
-  formId?: string
-  hasSubmitButton?: boolean
-  submitBtnRef?: React.RefObject<HTMLButtonElement>
-}) => {
+export const CreateAppForm = (props: CreateAppFormProps) => {
   const app = useAppMachine()
 
-  const onSubmit = ({ formData }: any) => {
-    app.send({
-      type: 'ON_SUBMIT',
-      data: formData,
-    })
-  }
-
-  // The state is needed, because the rjsf doesn't keep any state. Every time this re-renders, the input values get lost
-  const [formData, setFormData] = useState<CreateAppInput>({
-    title: '',
-  })
-
   return (
-    <Form
-      id={formId}
+    <GeneratedXStateForm<LoginUserInput, any>
       schema={CreateAppInputSchema as JSONSchema7}
-      onSubmit={onSubmit}
-      onChange={(e) => setFormData(e.formData)}
-      formData={formData}
-    >
-      {/* This button exists because by default the Form from rjsf includes a submit button.
-       Since we don't want to use it and we want to submit with the modal button, we need to hide it.
-       So if we add our own button and hide it with display: none, it works */}
-      <button
-        ref={submitBtnRef}
-        type="submit"
-        style={hasSubmitButton ? undefined : { display: 'none' }}
-      >
-        Submit
-      </button>
-    </Form>
+      send={app.send}
+      createSubmitEvent={({ data }) => {
+        return {
+          type: 'ON_SUBMIT',
+          data,
+        }
+      }}
+      {...props}
+    />
   )
 }
