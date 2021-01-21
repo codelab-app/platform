560e97a5662b492839f8b7e87f02f152ed0fbc35
burtsev <aburcev@gmail.com>
2021-01-21 18:53:02 +0300

    feat(libs-tools-grid-decorator): decorator details generator

---
+++ libs/tools/grid-decorator/src/fileMngmtHelpers.ts
@@ -0,0 +1,42 @@
+import * as fs from 'fs'
+import * as path from 'path'
+import { IDecoratorMap } from './DecoratorMapGenerator'
+
+export const writeFile = (
+  decorators: IDecoratorMap,
+  outputPath: string,
+  symbol: string,
+): string => {
+  const fileContents = `export const ${symbol}Decorators = ${JSON.stringify(
+    decorators,
+    null,
+    2,
+  )}`
+
+  console.log(`Saving "${symbol}" to "${outputPath}"...`)
+
+  fs.writeFileSync(outputPath, fileContents)
+
+  return outputPath
+}
+
+export const getSymbolDirectory = (
+  symbol: string,
+  includeFilePatterns: Array<string>,
+) => {
+  const includeFilePattern = includeFilePatterns.find((pattern) => {
+    return pattern.includes(symbol)
+  })
+
+  if (!includeFilePattern) {
+    throw new Error(
+      `${symbol} base directory not found! \nSymbol name must follow [useCase]Input.ts format`,
+    )
+  }
+
+  return path.dirname(includeFilePattern)
+}
+
+export const getOutputFile = (baseDirectory: string, symbol: string) => {
+  return path.resolve(baseDirectory, `${symbol}.decorators.generated.ts`)
+}
