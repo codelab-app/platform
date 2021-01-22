94494d3aea4bce4f14879a9f89a9fa5ab215c6ba
burtsev <aburcev@gmail.com>
2021-01-22 20:09:36 +0300

    feat(libs-modules-demo-grid-form): generated decorator details for DemoGridFormInput

---
+++ libs/modules/demo-grid-form/src/useCases/DemoGridForm/DemoGridFormInput.decorators.generated.ts
@@ -0,0 +1,26 @@
+export const DemoGridFormInputDecorators = {
+  email1: {
+    grid: [
+      {
+        order: 1,
+        span: 12,
+      },
+    ],
+  },
+  password0: {
+    grid: [
+      {
+        order: 0,
+        span: 16,
+      },
+    ],
+  },
+  name2: {
+    grid: [
+      {
+        order: 2,
+        span: 8,
+      },
+    ],
+  },
+}
