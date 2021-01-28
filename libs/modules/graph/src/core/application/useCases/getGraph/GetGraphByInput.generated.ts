99ed7f643e121cc3baa1601dc0d4de16af8146cd
artonio <asukhovatkin@gmail.com>
2021-01-27 23:08:51 -0500

    feat(apps-api-codelab): move node and graph tests

---
+++ libs/modules/graph/src/core/application/useCases/getGraph/GetGraphByInput.generated.ts
@@ -0,0 +1,12 @@
+export const GetGraphByInputSchema = {
+  type: 'object',
+  properties: {
+    appId: {
+      type: 'string',
+    },
+    pageId: {
+      type: 'string',
+    },
+  },
+  $schema: 'http://json-schema.org/draft-07/schema#',
+}
