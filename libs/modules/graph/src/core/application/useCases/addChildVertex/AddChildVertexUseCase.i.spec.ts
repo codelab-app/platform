4c7417e09885e5765db9ddc5b8945d106323050e
Webber Wang <webber@codelab.ai>
2021-01-30 01:58:47 +0800

    test: fix addVertex spec

--- libs/modules/graph/src/core/application/useCases/addChildVertex/AddChildVertexUseCase.i.spec.ts
+++ libs/modules/graph/src/core/application/useCases/addChildVertex/AddChildVertexUseCase.i.spec.ts
@@ -92,7 +92,6 @@ describe('AddChildVertexUseCase', () => {
         )
       })
       .then((res) => res.body.data.createPage)
-    const graphId = page.graphs[0].id
     const parentVertexId = page.graphs[0].vertices[0].id
 
     const addChildVertexWithParent = await request(app.getHttpServer())
@@ -102,7 +101,6 @@ describe('AddChildVertexUseCase', () => {
         variables: {
           input: {
             order: 0,
-            graphId,
             parentVertexId,
             vertex: {
               type: 'React_Text',
@@ -115,17 +113,14 @@ describe('AddChildVertexUseCase', () => {
       })
       .expect(200)
       .expect((res) => {
-        const graph = res.body.data.addChildVertex
+        const vertex = res.body.data.addChildVertex
 
-        expect(graph.label).toEqual('Layout')
-        expect(graph.vertices.length).toEqual(2)
-        expect(graph.edges.length).toEqual(1)
-        expect(graph.edges[0].source).toEqual(parentVertexId)
-        expect(graph.edges[0].order).toEqual(0)
-        expect(graph.vertices[0].type).toEqual('React_Grid_Layout_Container')
-        expect(graph.vertices[1].type).toEqual('React_Text')
-        expect(graph.vertices[1].props).toMatchObject({
-          id: 'a',
+        expect(vertex).toMatchObject({
+          id: vertex.id,
+          type: 'React_Text',
+          props: {
+            id: 'a',
+          },
         })
       })
   })
