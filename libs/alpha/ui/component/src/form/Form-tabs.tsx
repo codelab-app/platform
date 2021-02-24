021f7d12d1fa64a81e6012d687ebe15847254ab6
Webber Wang <webber@codelab.ai>
2021-02-25 02:01:45 +0800

    feat: add new object template for form

--- libs/alpha/ui/component/src/form/Form-tabs.tsx
+++ libs/alpha/ui/component/src/form/Form-tabs.tsx
@@ -7,6 +7,16 @@ import { JsonSchemaForm } from '@codelab/frontend'
 export const schema: JSONSchema7 = {
   type: 'object',
   properties: {
+    address: {
+      type: 'object',
+      title: 'Address - Object Test',
+      properties: {
+        streetName: { type: 'string', title: 'Street Name' },
+        houseNumber: { type: 'string', title: 'House Number' },
+        postalCode: { type: 'string', title: 'Postal Code' },
+        phoneNumber: { type: 'string', title: 'Phone Number' },
+      },
+    },
     email: {
       type: 'string',
     },
@@ -29,12 +39,12 @@ export const tabsFormProps = {
   schema,
   ObjectFieldTemplate: ObjectFieldTabsTemplate,
   uiSchema: {
-    'ui:tabs': [
+    'ui:groups': [
       'firstname',
       'lastname',
       {
         groups: {
-          password: ['password'],
+          password: ['password', 'address'],
           contacts: ['email'],
         },
         'ui:template': 'tabs',
@@ -47,11 +57,47 @@ export const TabsForm = () => {
   return <JsonSchemaForm {...tabsFormProps} />
 }
 
-export const TabsFormV2 = () => {
-  const tabsFormPropsV2 = {
-    ...tabsFormProps,
-    ObjectFieldTemplate: CodelabObjectFieldTemplate,
-  }
+export const tabsFormPropsV2 = {
+  ...tabsFormProps,
+  schema: {
+    type: 'object',
+    properties: {
+      email: {
+        type: 'string',
+      },
+      password: {
+        type: 'string',
+      },
+      address_1: {
+        type: 'string',
+      },
+      address_2: {
+        type: 'string',
+      },
+      other: {
+        type: 'string',
+      },
+      address: {
+        type: 'object',
+        title: 'Address - Object Test',
+        properties: {
+          streetName: { type: 'string', title: 'Street Name' },
+          houseNumber: { type: 'string', title: 'House Number' },
+          postalCode: { type: 'string', title: 'Postal Code' },
+          phoneNumber: { type: 'string', title: 'Phone Number' },
+        },
+      },
+    },
+  } as JSONSchema7,
+  uiSchema: {
+    'ui:tabs': [
+      { title: 'Info', fields: ['email', 'password'] },
+      { title: 'Address', fields: ['address_1', 'address_2', 'address'] },
+    ],
+  },
+  ObjectFieldTemplate: CodelabObjectFieldTemplate,
+}
 
+export const TabsFormV2 = () => {
   return <JsonSchemaForm {...tabsFormPropsV2} />
 }
