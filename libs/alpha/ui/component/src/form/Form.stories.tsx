1eecd991549f2fe89403c4f709615c2c540caedf
Webber Wang <webber@codelab.ai>
2021-02-25 02:01:45 +0800

    feat: add new object template for form

--- libs/alpha/ui/component/src/form/Form.stories.tsx
+++ libs/alpha/ui/component/src/form/Form.stories.tsx
@@ -1,5 +1,6 @@
 import {
   AccordionForm as AccordionFormStory,
+  AccordionFormV2 as AccordionFormV2Story,
   accordionFormProps,
 } from './Form-accordion'
 import {
@@ -11,7 +12,12 @@ import {
   SelectableSearchArrayForm,
   selectableSearchArrayFormProps,
 } from './Form-selectable'
-import { TabsForm as TabsFormStory } from './Form-tabs'
+import {
+  TabsForm as TabsFormStory,
+  TabsFormV2 as TabsFormV2Story,
+  tabsFormProps,
+  tabsFormPropsV2,
+} from './Form-tabs'
 
 export default {
   title: 'Json Schema Forms',
@@ -21,13 +27,16 @@ export default {
       ConditionalForm: conditionalFormProps,
       GridsForm: gridsFormProps,
       SelectableForm: selectableSearchArrayFormProps,
-      TabsForm: {},
+      TabsForm: tabsFormProps,
+      TabsFormV2: tabsFormPropsV2,
     },
   },
 }
 
 export const AccordionForm = AccordionFormStory
+export const AccordionFormV2 = AccordionFormV2Story
 export const ConditionalForm = ConditionalFormStory
 export const GridsForm = GridsFormStory
 export const SelectableForm = SelectableSearchArrayForm
 export const TabsForm = TabsFormStory
+export const TabsFormV2 = TabsFormV2Story
