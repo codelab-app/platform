86d56a6d7e30be197a84a430029da5d0a0cc40b7
Webber Wang <webber@codelab.ai>
2021-02-25 21:13:25 +0800

    feat: add form example for nesting schema

--- libs/alpha/ui/component/src/form/rjsf-templates/CodelabTabsTemplate.tsx
+++ libs/alpha/ui/component/src/form/rjsf-templates/CodelabTabsTemplate.tsx
@@ -1,73 +1,68 @@
-import React from 'react';
-import { Tabs } from 'antd';
+import { Tabs } from 'antd'
+import React from 'react'
 
 export interface ITabs {
-	title: string,
-	fields: string[]
+  title: string
+  fields: Array<string>
 }
 
 export const CodelabTabsTemplate = (props: any) => {
-	const {
-		TitleField,
-		DescriptionField,
-		uiSchema,
-		idSchema,
-		required,
-		disabled,
-		readonly,
-		formData,
-		formContext,
-		properties,
-		schema
-	} = props
+  const {
+    TitleField,
+    DescriptionField,
+    uiSchema,
+    idSchema,
+    required,
+    disabled,
+    readonly,
+    formData,
+    formContext,
+    properties,
+    schema,
+  } = props
 
-	const findContent = (name: string) => {
-		return properties.find((prop: any) => {
-			return prop.name === name
-		})
-	}
+  const findContent = (name: string) => {
+    return properties.find((prop: any) => {
+      return prop.name === name
+    })
+  }
 
-	const layout: ITabs[] = uiSchema['ui:groups']
+  const layout: Array<ITabs> = uiSchema['ui:groups']
 
-	return (
-		<fieldset id={props.idSchema.$id}>
-			{(props.uiSchema["ui:title"] || props.title) && (
-				<TitleField
-					id={`${props.idSchema.$id}__title`}
-					title={props.title || props.uiSchema["ui:title"]}
-					required={props.required}
-					formContext={props.formContext}
-				/>
-			)}
-			{props.description && (
-				<DescriptionField
-					id={`${props.idSchema.$id}__description`}
-					description={props.description}
-					formContext={props.formContext}
-				/>
-			)}
-			<Tabs defaultActiveKey="0" key="tabs">
-				{
-					layout?.map((row, index) => {
-						return (
-							<Tabs.TabPane tab={row.title} key={index}>
-								{
-									row.fields.map((field: string) => {
-										if (schema.properties[field]) {
-											const element = findContent(field)
-											return (
-												<div>
-													{element.content}
-												</div>
-											)
-										}
-									})
-								}
-							</Tabs.TabPane>
-						)
-					})
-				}
-			</Tabs>
-		</fieldset>
-	)
+  return (
+    <fieldset id={props.idSchema.$id}>
+      {(props.uiSchema['ui:title'] || props.title) && (
+        <TitleField
+          id={`${props.idSchema.$id}__title`}
+          title={props.title || props.uiSchema['ui:title']}
+          required={props.required}
+          formContext={props.formContext}
+        />
+      )}
+      {props.description && (
+        <DescriptionField
+          id={`${props.idSchema.$id}__description`}
+          description={props.description}
+          formContext={props.formContext}
+        />
+      )}
+      <Tabs defaultActiveKey="0" key="tabs">
+        {layout?.map((row, index) => {
+          return (
+            <Tabs.TabPane tab={row.title} key={index as any}>
+              {row.fields.map((field: string) => {
+                if (schema.properties[field]) {
+                  const element = findContent(field)
+
+                  return <div>{element.content}</div>
+                }
+
+                return null
+              })}
+            </Tabs.TabPane>
+          )
+        })}
+      </Tabs>
+    </fieldset>
+  )
 }
