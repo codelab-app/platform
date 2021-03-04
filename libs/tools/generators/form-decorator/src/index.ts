92d288284b73291245f339a8a76f2e07bafcbfca
Webber Wang <webber@codelab.ai>
2021-03-04 20:01:01 +0800

    feat: add generator lib

--- libs/tools/generators/form-decorator/src/index.ts
+++ libs/tools/generators/form-decorator/src/index.ts
@@ -1,10 +1,4 @@
-import * as path from 'path'
-import { getJsonSchema } from '@tsed/schema'
-import { User } from '../test/models/ArrayTypes/User'
-import { GridWithTabs } from '../test/models/GridWithTabs'
-import { Shipping } from '../test/models/Shipping/Shipping'
 import { generateSchemas, writeSchemasToFile } from './processors'
-import { getJsonSchemaCustom } from './processors/custom-tsed/getJsonSchemaCustom'
 
 interface GenerateSchemaProps {
   // Function to be generated
@@ -17,9 +11,7 @@ export const generateSchema = (props: GenerateSchemaProps) => {
   writeSchemasToFile(generateSchemas(props.target), props.filePath)
 }
 
-const filePath = path.join(__dirname)
-
-generateSchema({
-  target: GridWithTabs,
-  filePath,
-})
+// generateSchema({
+//   target: GridWithTabs,
+//   filePath: path.join(__dirname),
+// })
