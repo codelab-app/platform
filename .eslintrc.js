a0ecdbc1b66665ebeb8561bc1f8dc99ec11127fb
Webber Wang <webber@codelab.ai>
2021-03-22 17:38:01 +0800

    fix: graphql codegen

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   extends: require.resolve('./dist/libs/tools/eslint-config-codelab'),
   parserOptions: {
     tsconfigRootDir: '.',
+    extraFileExtensions: ['.graphql'],
     project: ['tsconfig.eslint.json'],
   },
   rules: {
