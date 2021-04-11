818ebb4d5b97ed7bdbdbdf61bbec80c7b8184935
Webber Wang <webber@codelab.ai>
2021-04-11 23:49:42 +0800

    fix: app api test

--- apps/api/src/app/app.controller.spec.ts
+++ apps/api/src/app/app.controller.spec.ts
@@ -14,10 +14,10 @@ describe('AppController', () => {
   })
 
   describe('getData', () => {
-    it('should return "Welcome to api!"', () => {
+    it('should return "Welcome to codelab!"', () => {
       const appController = app.get<AppController>(AppController)
       expect(appController.getData()).toEqual({
-        message: 'Welcome to api!',
+        message: 'Welcome to codelab!',
       })
     })
   })
