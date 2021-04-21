f9ceb2c96414c9a76023609e43d7a275e5fa470a
Anton Mihaylov <antonmmihaylov@gmail.com>
2021-04-21 22:42:15 +0300

    fix(apps-web-e2e): #777 fixed build error

--- apps/web-e2e/src/integration/atom.spec.ts
+++ apps/web-e2e/src/integration/atom.spec.ts
@@ -1,5 +1,3 @@
-import { Page } from '@codelab/frontend/shared'
-
 const appId = 'c83654f1-f89f-48a0-8741-acf91e38c888'
 const pageId = '4ccde878-25d3-4b36-a9b8-67fcec986e7b'
 
@@ -7,11 +5,7 @@ describe('Atom', () => {
   before(() => {
     cy.login()
 
-    cy.visit(
-      Page.PAGE_DETAIL.url
-        .replace('[appId]', appId)
-        .replace('[pageId]', pageId),
-    )
+    cy.visit(`/apps/${appId}/pages/${pageId}`)
   })
 
   it('loads', () => {
