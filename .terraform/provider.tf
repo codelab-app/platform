eee1ada63afdd1611b6d5774ebfdcff12397694d
Webber Wang <webber@codelab.ai>
2021-04-25 01:33:34 +0800

    refactor: add iam to module

--- .terraform/provider.tf
+++ .terraform/provider.tf
@@ -2,8 +2,18 @@ provider "aws" {
   region = var.AWS_REGION
 }
 
+terraform {
+  required_version = ">= 0.14"
+}
+
 module "codelab-vpc" {
   source = "./vpc"
+}
+
+module "codelab-iam" {
+  source              = "./iam"
+  PATH_TO_PRIVATE_KEY = var.PATH_TO_PRIVATE_KEY
+  PATH_TO_PUBLIC_KEY  = var.PATH_TO_PUBLIC_KEY
 }
 
 module "codelab-rds" {
