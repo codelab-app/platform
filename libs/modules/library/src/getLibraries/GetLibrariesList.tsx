309c28908e79d041cff97340b36dbd42026175fb
burtsev <aburcev@gmail.com>
2021-04-29 13:13:14 +0300

    test(apps-web-e2e): #750 libraries CRUD

--- libs/modules/library/src/getLibraries/GetLibrariesList.tsx
+++ libs/modules/library/src/getLibraries/GetLibrariesList.tsx
@@ -1,17 +1,19 @@
-import { List, Space } from 'antd'
+import { List, Space, Spin } from 'antd'
 import React from 'react'
 import { useGetLibrariesListQuery } from '@codelab/hasura'
 import { DeleteOutlined, SettingOutlined } from '@ant-design/icons'
 import { EntityType, useCRUDModalForm } from '@codelab/frontend/shared'
 
 export const GetLibrariesList = () => {
-  const { data } = useGetLibrariesListQuery({})
+  const { data, loading } = useGetLibrariesListQuery({})
 
   const { openDeleteModal, openUpdateModal } = useCRUDModalForm(
     EntityType.Library,
   )
 
-  return (
+  return loading ? (
+    <Spin />
+  ) : (
     <List
       size="small"
       dataSource={data?.library}
@@ -19,8 +21,14 @@ export const GetLibrariesList = () => {
         <List.Item onMouseOver={() => null} style={{ paddingLeft: 0 }}>
           <Space style={{ width: '100%' }}>{library.name}</Space>
           <Space>
-            <SettingOutlined onClick={() => openUpdateModal(library.id)} />
-            <DeleteOutlined onClick={() => openDeleteModal(library.id)} />
+            <SettingOutlined
+              title="Settings"
+              onClick={() => openUpdateModal(library.id)}
+            />
+            <DeleteOutlined
+              title="Delete"
+              onClick={() => openDeleteModal(library.id)}
+            />
           </Space>
         </List.Item>
       )}
