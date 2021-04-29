20b2a85f38240ee24294378931be831692e6c502
burtsev <aburcev@gmail.com>
2021-04-29 13:37:17 +0300

    refactor(libs-modules-library): integrate buttons for settins and delete

--- libs/modules/library/src/getLibraries/GetLibrariesList.tsx
+++ libs/modules/library/src/getLibraries/GetLibrariesList.tsx
@@ -1,8 +1,12 @@
 import { List, Space, Spin } from 'antd'
 import React from 'react'
 import { useGetLibrariesListQuery } from '@codelab/hasura'
-import { DeleteOutlined, SettingOutlined } from '@ant-design/icons'
-import { EntityType, useCRUDModalForm } from '@codelab/frontend/shared'
+import {
+  EntityType,
+  ListItemDeleteButton,
+  ListItemSettingsButton,
+  useCRUDModalForm,
+} from '@codelab/frontend/shared'
 
 export const GetLibrariesList = () => {
   const { data, loading } = useGetLibrariesListQuery({})
@@ -21,14 +25,11 @@ export const GetLibrariesList = () => {
         <List.Item onMouseOver={() => null} style={{ paddingLeft: 0 }}>
           <Space style={{ width: '100%' }}>{library.name}</Space>
           <Space>
-            <SettingOutlined
-              title="Settings"
+            <ListItemSettingsButton
               onClick={() => openUpdateModal(library.id)}
             />
-            <DeleteOutlined
-              title="Delete"
-              onClick={() => openDeleteModal(library.id)}
-            />
+
+            <ListItemDeleteButton onClick={() => openDeleteModal(library.id)} />
           </Space>
         </List.Item>
       )}
