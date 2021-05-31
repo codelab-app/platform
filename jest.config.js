f0320e90aad69189da54832d53797d340752ad4f
Webber Wang <webber@codelab.ai>
2021-05-30 19:36:50 -0700

    fix: jest specs error
    fix: jest specs error

--- jest.config.js
+++ jest.config.js
@@ -6,10 +6,13 @@ module.exports = {
     '<rootDir>/libs/modules/lambda-api',
     '<rootDir>/libs/modules/app-api',
     '<rootDir>/libs/modules/user-api',
+    '<rootDir>/libs/modules/page-api',
+    '<rootDir>/libs/modules/page-element-api',
     '<rootDir>/libs/plugins/codelab',
     '<rootDir>/libs/modules/atom-api',
     '<rootDir>/libs/modules/prop-api',
     '<rootDir>/libs/modules/value-type-api',
     '<rootDir>/libs/modules/value-type',
+    '<rootDir>/libs/test',
   ],
 }
