7425f08213bd66d4a632f418c01057964a8ea83a
Webber Wang <webber@codelab.ai>
2021-06-01 16:05:58 -0700

    refactor(libs-backend): use provider instead of service for dgraph

--- libs/backend/src/infrastructure/dgraph/dgraph.module.ts
+++ libs/backend/src/infrastructure/dgraph/dgraph.module.ts
@@ -1,12 +1,13 @@
 import { Global, Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 import { dgraphConfig } from './config/dgraph.config'
-import { DGraphService } from './dgraph.service'
+import { DgraphTokens } from './config/dgraph.tokens'
+import { dgraphClientProvider } from './dgraph.provider'
 
 @Global()
 @Module({
   imports: [ConfigModule.forFeature(dgraphConfig)],
-  providers: [DGraphService],
-  exports: [DGraphService],
+  providers: [dgraphClientProvider],
+  exports: [DgraphTokens.DgraphProvider],
 })
 export class DGraphModule {}
---
+++ libs/backend/src/infrastructure/dgraph/dgraph.provider.ts
@@ -0,0 +1,28 @@
+import { Provider } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import { DgraphClient, DgraphClientStub } from 'dgraph-js-http'
+import { DgraphConfiguration } from './config/dgraph.config'
+import { DgraphTokens } from './config/dgraph.tokens'
+
+export type DgraphProvider = {
+  client: DgraphClient
+  resetDb: ReturnType<DgraphClient['alter']>
+}
+
+export const dgraphClientProvider: Provider<DgraphProvider> = {
+  provide: DgraphTokens.DgraphProvider,
+  useFactory: (configService: ConfigService) => {
+    const config = configService.get<DgraphConfiguration>(
+      DgraphTokens.DgraphConfig.toString(),
+    )
+
+    const clientStub = new DgraphClientStub(config?.endpoint)
+    const dgraphClient = new DgraphClient(clientStub)
+
+    return {
+      client: dgraphClient,
+      resetDb: dgraphClient.alter({ dropAll: true }),
+    }
+  },
+  inject: [ConfigService],
+}
