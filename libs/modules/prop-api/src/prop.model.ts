3e5dd6b2df4e2c1da3225cf3ca9cba4537ae9e02
Anton Mihaylov <antonmmihaylov@gmail.com>
2021-06-19 19:03:08 +0300

    refactor(libs): #884 remove old prop type code

--- libs/modules/prop-api/src/prop.model.ts
+++ libs/modules/prop-api/src/prop.model.ts
@@ -1,4 +1,3 @@
-import { ValueType } from '@codelab/modules/value-type-api'
 import { Field, ID, ObjectType } from '@nestjs/graphql'
 
 @ObjectType()
@@ -9,8 +8,8 @@ export class Prop {
   @Field({ nullable: true })
   declare key: string
 
-  @Field(() => ValueType)
-  declare type: ValueType
+  // @Field(() => ValueType)
+  // declare type: ValueType
 
   @Field({ nullable: true })
   declare description: string
