e7ccfda124fa3905223ebcfb4e95aad4095039df
Anton Mihaylov <antonmmihaylov@gmail.com>
2021-06-20 09:48:48 +0300

    feat(apps): #884 props schema

--- libs/modules/prop-api/src/prop.model.ts
+++ libs/modules/prop-api/src/prop.model.ts
@@ -1,19 +1,87 @@
-import { Field, ID, ObjectType } from '@nestjs/graphql'
+import { Field } from '@codelab/modules/type-api'
+import {
+  createUnionType,
+  Field as GraphqlField,
+  Float,
+  ID,
+  Int,
+  ObjectType,
+} from '@nestjs/graphql'
+
+@ObjectType()
+export class StringValue {
+  @GraphqlField(() => ID)
+  declare id: string
+
+  @GraphqlField()
+  declare value: string
+}
+
+@ObjectType()
+export class IntValue {
+  @GraphqlField(() => ID)
+  declare id: string
+
+  @GraphqlField(() => Int)
+  declare value: number
+}
+
+@ObjectType()
+export class FloatValue {
+  @GraphqlField(() => ID)
+  declare id: string
+
+  @GraphqlField(() => Float)
+  declare value: number
+}
+
+@ObjectType()
+export class BooleanValue {
+  @GraphqlField(() => ID)
+  declare id: string
+
+  @GraphqlField()
+  declare value: boolean
+}
+
+@ObjectType()
+export class ArrayValue {
+  @GraphqlField(() => ID)
+  declare id: string
+
+  @GraphqlField(() => [PropValue])
+  declare values: Array<typeof PropValue>
+}
+
+@ObjectType()
+export class InterfaceValue {
+  @GraphqlField(() => ID)
+  declare id: string
+
+  @GraphqlField(() => [Prop])
+  declare props: Array<typeof Prop>
+}
+
+export const PropValue = createUnionType({
+  name: 'PropValue',
+  types: () => [
+    StringValue,
+    IntValue,
+    FloatValue,
+    BooleanValue,
+    ArrayValue,
+    InterfaceValue,
+  ],
+})
 
 @ObjectType()
 export class Prop {
-  @Field(() => ID)
+  @GraphqlField(() => ID)
   declare id: string
 
-  @Field({ nullable: true })
-  declare key: string
+  @GraphqlField(() => Field)
+  declare field: Field
 
-  // @Field(() => ValueType)
-  // declare type: ValueType
-
-  @Field({ nullable: true })
-  declare description: string
-
-  @Field(() => [Prop], { nullable: true })
-  declare props: [Prop]
+  @GraphqlField(() => PropValue, { nullable: true })
+  declare value?: typeof PropValue
 }
