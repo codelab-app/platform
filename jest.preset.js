d902298c96514ba928c6a80ace961c9b5a308352
Webber Wang <webber@codelab.ai>
2021-07-10 18:55:23 +0800

    fix(libs-ui-d3): jest babel error

--- jest.preset.js
+++ jest.preset.js
@@ -3,19 +3,8 @@ const nxPreset = require('@nrwl/jest/preset')
 module.exports = {
   ...nxPreset,
   setupFiles: [`${__dirname}/jest.setup.js`],
-  setupFilesAfterEnv: [
-    // `${__dirname}/.jest/jest.setup.js`,
-    '@testing-library/jest-dom',
-  ],
+  setupFilesAfterEnv: ['@testing-library/jest-dom'],
   testTimeout: 30000,
-  // testPathIgnorePatterns: ['i.spec.ts'],
-  // testMatch: ['**/+(*.)+(spec|test).+(ts|js)?(x)'],
-  // transform: {
-  //   '^.+\\.(ts|js|html)$': 'ts-jest',
-  // },
-  // resolver: '@nrwl/jest/plugins/resolver',
-  // moduleFileExtensions: ['ts', 'js', 'html'],
-  // coverageReporters: ['html'],
   /**
    * Some NPM modules are written in ES6, and must be transformed with babel. node_modules is ignored by default because there are too many packages to transform, so we only transform the ones we have to.
    */
