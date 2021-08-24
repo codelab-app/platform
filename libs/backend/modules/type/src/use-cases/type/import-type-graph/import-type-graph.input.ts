822664ccbcc620019994c92fc9207d278c43e457
Webber Wang <webber@codelab.ai>
2021-08-24 21:35:37 +0800

    revert: import type graph

--- libs/backend/modules/type/src/use-cases/type/import-type-graph/import-type-graph.input.ts
+++ libs/backend/modules/type/src/use-cases/type/import-type-graph/import-type-graph.input.ts
@@ -1,13 +1,16 @@
-import { ITypeGraphInput } from '@codelab/shared/abstract/core'
 import { Field as GraphqlField, InputType } from '@nestjs/graphql'
-import { ImportTypeEdgeInput } from '../../../domain'
-import { ImportTypeVertex } from '../import-type'
+
+// @InputType()
+// export class ImportTypeGraphInput implements ITypeGraphInput {
+//   @GraphqlField(() => [ImportTypeVertex])
+//   declare vertices: Array<ImportTypeVertex>
+//
+//   @GraphqlField(() => [ImportTypeEdgeInput])
+//   declare edges: ReadonlyArray<ImportTypeEdgeInput>
+// }
 
 @InputType()
-export class ImportTypeGraphInput implements ITypeGraphInput {
-  @GraphqlField(() => [ImportTypeVertex])
-  declare vertices: Array<ImportTypeVertex>
-
-  @GraphqlField(() => [ImportTypeEdgeInput])
-  declare edges: ReadonlyArray<ImportTypeEdgeInput>
+export class ImportTypeGraphInput {
+  @GraphqlField()
+  declare name: string
 }
