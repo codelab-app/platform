fe3c68cb57a69e587611abad306da749eed107c5
Webber Wang <webber@codelab.ai>
2021-09-28 01:11:03 +0800

    ci: dont run postinstall in ci

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -2,7 +2,11 @@
 
 set -x
 
+if [ "$CI" = false ]; then
+
 husky install
 nx run-many --target=build --projects=cli,tools-plugins-codelab
 yarn cli dgraph update-schema --env dev
 yarn cli dgraph update-schema --env test
+
+fi
