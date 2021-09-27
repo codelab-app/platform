a28f84a112d1c89e224d5c8a67079b37e87c9c45
Webber Wang <webber@codelab.ai>
2021-09-28 01:44:14 +0800

    ci: add --ignore-scripts

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -2,11 +2,7 @@
 
 set -x
 
-if [ "$CI" = false ]; then
-
 husky install
 nx run-many --target=build --projects=cli,tools-plugins-codelab
 yarn cli dgraph update-schema --env dev
 yarn cli dgraph update-schema --env test
-
-fi
