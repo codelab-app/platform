979d237a3f5077c98515aefb264a5fe7278f109f
Anton Mihaylov <antonmmihaylov@gmail.com>
2021-11-07 14:25:38 +0200

    refactor: hook module

--- libs/backend/modules/element/src/test/graphql/TestElement.fragment.graphql.gen.ts
+++ libs/backend/modules/element/src/test/graphql/TestElement.fragment.graphql.gen.ts
@@ -1,18 +1,12 @@
 import * as Types from '@codelab/frontend/abstract/codegen'
 
-import {
-  AtomBaseFragment,
-  AtomFragment,
-} from '../../../../../../frontend/modules/atom/src/Atom.fragment.graphql.gen'
+import { AtomBaseFragment } from '../../../../../../frontend/modules/atom/src/Atom.fragment.graphql.gen'
 import {
   HookFragment,
   PropMapBindingFragment,
 } from '../../../../../../frontend/modules/element/src/graphql/Element.fragment.graphql.gen'
 import { gql } from '@apollo/client'
-import {
-  AtomBaseFragmentDoc,
-  AtomFragmentDoc,
-} from '../../../../../../frontend/modules/atom/src/Atom.fragment.graphql.gen'
+import { AtomBaseFragmentDoc } from '../../../../../../frontend/modules/atom/src/Atom.fragment.graphql.gen'
 import {
   HookFragmentDoc,
   PropMapBindingFragmentDoc,
