4eb786fb9e94f6ffc18a6fa279a936f3acd46caf
Anton Mihaylov <antonmmihaylov@gmail.com>
2021-11-09 11:21:21 +0200

    fix: e2e

--- apps/web-e2e/src/integration/props/render-props.spec.ts
+++ apps/web-e2e/src/integration/props/render-props.spec.ts
@@ -110,46 +110,50 @@ describe('render props', () => {
               })
             })
 
-            cy.createPageFromScratch().then((page) => {
-              return cy
-                .getElementGraph({ where: { id: page.rootElementId } })
-                .then((elements) => {
-                  const rootElement = elements?.vertices?.[0]
+            cy.createPageFromScratch()
+              .then((page) => {
+                return cy
+                  .getElementGraph({ where: { id: page.rootElementId } })
+                  .then((elements) => {
+                    const rootElement = elements?.vertices?.[0]
 
-                  return cy
-                    .createElement({
-                      atomId: listAtom.id,
-                      name: listElementName,
-                      parentElementId: String(rootElement?.id),
-                    })
-                    .then((listItemElement: Element) => {
-                      // assign data source to list element
-                      cy.updateElementProps({
-                        elementId: listItemElement.id,
-                        props: JSON.stringify({ dataSource: listDataSource }),
+                    return cy
+                      .createElement({
+                        atomId: listAtom.id,
+                        name: listElementName,
+                        parentElementId: String(rootElement?.id),
                       })
+                      .then((listItemElement: Element) => {
+                        // assign data source to list element
+                        cy.updateElementProps({
+                          elementId: listItemElement.id,
+                          props: JSON.stringify({ dataSource: listDataSource }),
+                        })
+                      })
+                  })
+              })
+              .then(() => {
+                // create test component with text prop = 'React Node"
+                cy.createComponent({ name: reactNodeTextComponentName })
+                  .then(({ id }) => {
+                    return cy.createElement({
+                      parentElementId: id,
+                      atomId: textAtomId,
                     })
-                })
-            })
+                  })
+                  .then((textElement: CreateResponse) => {
+                    // create prop mapping from root to text
+                    // bind prop "value" to element "text"'s text prop key
+                    cy.updateElementProps({
+                      elementId: textElement.id,
+                      props: JSON.stringify(reactNodeTextProp),
+                    })
+                  })
+                  .then(() => cy.goToPageByAliasId())
+              })
           },
         )
 
-        // create test component with text prop = 'React Node"
-        cy.createComponent({ name: reactNodeTextComponentName })
-          .then(({ id }) => {
-            return cy.createElement({
-              parentElementId: id,
-              atomId: textAtomId,
-            })
-          })
-          .then((textElement: CreateResponse) => {
-            // create prop mapping from root to text
-            // bind prop "value" to element "text"'s text prop key
-            cy.updateElementProps({
-              elementId: textElement.id,
-              props: JSON.stringify(reactNodeTextProp),
-            })
-          })
         cy.getSpinner().should('not.exist')
       })
     })
@@ -159,23 +163,21 @@ describe('render props', () => {
     })
 
     it('bind  render props prop correctly', () => {
-      cy.goToPageByAliasId().then(() => {
-        // Go to List component
-        cy.findByText('Root element').click()
+      // Go to List component
+      cy.findByText('Root element').click()
 
-        // For some reason it gets an element right before re-rendering and then causes an error for it being detached
-        // eslint-disable-next-line cypress/no-unnecessary-waiting
-        cy.wait(100)
+      // For some reason it gets an element right before re-rendering and then causes an error for it being detached
+      // eslint-disable-next-line cypress/no-unnecessary-waiting
+      cy.wait(100)
 
-        cy.findByText(listElementName).should('be.visible').click()
+      cy.findByText(listElementName).should('be.visible').click()
 
-        // click on prop panel
-        cy.findByText('Props').click()
+      // click on prop panel
+      cy.findByText('Props').click()
 
-        // Click on select renderItem which is render props type
-        cy.findByLabelText(renderItemFieldName).click()
-        cy.getOptionItem(listItemComponentName).first().click()
-      })
+      // Click on select renderItem which is render props type
+      cy.findByLabelText(renderItemFieldName).click()
+      cy.getOptionItem(listItemComponentName).first().click()
     })
 
     it('bind react node prop correctly', () => {
