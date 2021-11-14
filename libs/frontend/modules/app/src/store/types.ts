c70498b650ce1ecd7b25a0cf3b956f63019dd779
Assim <assimhabe@gmail.com>
2021-11-14 18:37:30 +0100

    refactor: fix import

--- libs/frontend/modules/app/src/store/types.ts
+++ libs/frontend/modules/app/src/store/types.ts
@@ -1,4 +1,4 @@
 import { CRUDModalState } from '@codelab/frontend/view/components'
-import { AppFragment } from '../graphql'
+import { AppFragment } from '../graphql/App.fragment.graphql.gen'
 
 export type AppState = CRUDModalState<AppFragment>
