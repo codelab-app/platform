9fd91e1a08d152d085c4b7d7c58eb78d5b0de8b7
assim <assimhabe@gmail.com>
2021-12-02 19:30:04 +0100

    wip: trying to fix e2e tests

--- libs/frontend/modules/element/src/use-cases/hooks/add-hook-to-element/AddHookToElementForm.tsx
+++ libs/frontend/modules/element/src/use-cases/hooks/add-hook-to-element/AddHookToElementForm.tsx
@@ -1,5 +1,4 @@
 import { InterfaceForm } from '@codelab/frontend/modules/type'
-import { SpinnerWrapper } from '@codelab/frontend/view/components'
 import React from 'react'
 import { AutoFields } from 'uniforms-antd'
 import { addHookToElementSchema } from './addHookToElementSchema'
@@ -10,10 +9,8 @@ export const AddHookToElementForm = ({
   ...props
 }: AddHookToElementFormProps) => {
   return (
-    <SpinnerWrapper isLoading={interfaceLoading}>
-      <InterfaceForm {...props} schema={addHookToElementSchema}>
-        <AutoFields omitFields={['appId']} />
-      </InterfaceForm>
-    </SpinnerWrapper>
+    <InterfaceForm {...props} schema={addHookToElementSchema}>
+      <AutoFields omitFields={['appId']} />
+    </InterfaceForm>
   )
 }
