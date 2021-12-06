f46737af850c6c8b87da4f638e7a0c5335af62ea
Anton Mihaylov <antonmmihaylov@gmail.com>
2021-12-06 15:40:56 +0200

    feat: spread prop mappings

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -6,6 +6,8 @@ import { CodelabPage } from '@codelab/frontend/abstract/props'
 import { useApollo } from '@codelab/frontend/model/infra/apollo'
 import { reduxStoreWrapper } from '@codelab/frontend/model/infra/redux'
 import { css, Global } from '@emotion/react'
+import DateFnsAdapter from '@mui/lab/AdapterDateFns'
+import LocalizationProvider from '@mui/lab/LocalizationProvider'
 import { ConfigProvider } from 'antd'
 import { AppProps } from 'next/app'
 import React from 'react'
@@ -37,31 +39,33 @@ const AppContainer = ({
     <QueryClientProvider client={queryClient}>
       <ApolloProvider client={useApollo(ssrPageProps)}>
         <UserProvider>
-          <ConfigProvider>
-            <GlobalStyles />
-            <Global
-              styles={[
-                css({
-                  '#__next': {
-                    height: '100%',
-                  },
-                }),
-                ...globalTailwindFix,
-              ]}
-            />
-            {Template ? (
-              <Template
-                MainPane={_MainPane}
-                MetaPane={_MetaPane}
-                SidebarNavigation={_SidebarNavigation}
-                Header={_Header}
-              >
+          <LocalizationProvider dateAdapter={DateFnsAdapter}>
+            <ConfigProvider>
+              <GlobalStyles />
+              <Global
+                styles={[
+                  css({
+                    '#__next': {
+                      height: '100%',
+                    },
+                  }),
+                  ...globalTailwindFix,
+                ]}
+              />
+              {Template ? (
+                <Template
+                  MainPane={_MainPane}
+                  MetaPane={_MetaPane}
+                  SidebarNavigation={_SidebarNavigation}
+                  Header={_Header}
+                >
+                  <Component {...ssrPageProps} />
+                </Template>
+              ) : (
                 <Component {...ssrPageProps} />
-              </Template>
-            ) : (
-              <Component {...ssrPageProps} />
-            )}
-          </ConfigProvider>
+              )}
+            </ConfigProvider>
+          </LocalizationProvider>
         </UserProvider>
       </ApolloProvider>
     </QueryClientProvider>
