67449259b5b486c311d969b0cf0bcdf9f8f56c2f
assim <assimhabe@gmail.com>
2021-12-12 21:21:14 +0100

    fix: use correct name

--- libs/frontend/modules/tag/src/use-cases/update-tag/UpdateTagModal.tsx
+++ libs/frontend/modules/tag/src/use-cases/update-tag/UpdateTagModal.tsx
@@ -21,7 +21,7 @@ export const UpdateTagModal = () => {
     },
     visible: actionType === ActionType.Update,
     onCancel: () => reset(),
-    title: <span css={tw`font-semibold`}>Update Component</span>,
+    title: <span css={tw`font-semibold`}>Update Tag</span>,
   }
 
   return (
