f3997aeadb33958552bb0dc4123fb7be84d34876
Webber Wang <webber@codelab.ai>
2021-12-15 15:43:04 -0800

    ci: add assim to slack notifications

--- apps/web/src/home/HomeJumbo.tsx
+++ apps/web/src/home/HomeJumbo.tsx
@@ -36,9 +36,52 @@ export const HomeJumbo = () => {
           </Button>
         </Card>
       </Col>
-      <Col {...twoGridCol}>
-        <Image src="http://via.placeholder.com/1080x1080" preview={false} />
-      </Col>
+
+      <div
+        css={[
+          css`
+            &:hover img {
+              transform: scale(0.92);
+              transition-duration: 0.6s;
+            }
+            &:hover .watch-content {
+              transform: scale(1.08);
+              transition-duration: 0.6s;
+            }
+            & img,
+            & .watch-content {
+              transition-duration: 0.6s;
+            }
+          `,
+          tw`hover:cursor-pointer relative`,
+        ]}
+      >
+        <Image src="/banner-screenshot.png" preview={false} css={tw`mt-8 `} />
+        <div
+          className="watch-content"
+          css={[
+            tw`absolute bg-white z-10 text-2xl flex justify-center`,
+            css`
+              width: 640px;
+              height: 80px;
+              left: calc(50% - 320px);
+              top: calc(50% - 40px);
+            `,
+          ]}
+        >
+          <PlayCircleOutlined css={tw`w-12 text-3xl mt-6`} />
+          <span
+            css={[
+              tw`flex self-center text-2xl`,
+              css`
+                line-height: 80px;
+              `,
+            ]}
+          >
+            Watch how to build a products page with Shopify.
+          </span>
+        </div>
+      </div>
     </Row>
   )
 }
