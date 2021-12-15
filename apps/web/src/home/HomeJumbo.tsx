f9af4a81f0b3154fe6c45e2656b96c046e781b5e
Webber Wang <webber@codelab.ai>
2021-12-15 15:58:47 -0800

    wip: landing page
    wip: landing page
    
    wip: landing page
    
    wip: landing page
    
    wip: update package.json
    
    wip: landing page
    
    wip: landing page

--- apps/web/src/home/HomeJumbo.tsx
+++ apps/web/src/home/HomeJumbo.tsx
@@ -1,40 +1,87 @@
-import { ArrowRightOutlined } from '@ant-design/icons'
-import { padding, twoGridCol } from '@codelab/frontend/style'
-import { Button, Card, Col, Image, Row, Typography } from 'antd'
-import React from 'react'
+import 'bower_components/Morphext/dist/morphext.css'
+import { ArrowRightOutlined, PlayCircleOutlined } from '@ant-design/icons'
+import { Fancybox } from '@codelab/frontend/view/components'
+import { css } from '@emotion/react'
+import { Button, Col, Image, Row, Space, Typography } from 'antd'
+import $ from 'jquery'
+import React, { useEffect, useRef } from 'react'
+import tw from 'twin.macro'
 
-const { Title } = Typography
+const { Title, Text, Paragraph } = Typography
+
+// const Fancybox = dynamic<any>(
+//   () => import('@codelab/frontend/view/components').then((mod) => mod.Fancybox),
+//   { ssr: false },
+// )
 
 export const HomeJumbo = () => {
-  const titleStyle = {
-    fontSize: '2.75rem',
-    lineHeight: 1.5,
-  }
+  const jsRotatingRef = useRef(null)
 
-  const subtitleStyle = {
-    fontSize: '1.2em',
-    lineHeight: 1.5,
-  }
+  useEffect(() => {
+    window.jQuery = $
+    window.Morphtext = require('bower_components/Morphext/dist/morphext.min.js')
+    ;($(jsRotatingRef.current!) as any).Morphext({
+      // The [in] animation type. Refer to Animate.css for a list of available animations.
+      animation: 'animate__animated animate__fadeIn',
+      // An array of phrases to rotate are created based on this separator. Change it if you wish to separate the phrases differently (e.g. So Simple | Very Doge | Much Wow | Such Cool).
+      separator: ',',
+      // The delay between the changing of each phrase in milliseconds.
+      speed: 2200,
+      complete: function () {
+        // Called after the entrance animation is executed.
+      },
+    })
+  })
 
   return (
-    <Row gutter={[padding.sm, padding.sm]} align="middle">
-      <Col {...twoGridCol}>
-        <Card>
-          <Title style={titleStyle}>
-            Rapidly Deliver Production Grade User Interface
-          </Title>
-          <p style={subtitleStyle}>
-            The process of choose a tech stack & managing frontend state can be
-            a challenging task in the ever-so evolving JavaScript ecosystem.
-          </p>
-          <p style={subtitleStyle}>
-            We have abstracted everything you need to build out a robust
-            frontend web application.
-          </p>
-          <Button size="large" icon={<ArrowRightOutlined />} type="primary">
-            Start Building Now
+    <Row css={tw`my-8 justify-center`}>
+      <Col css={tw`flex-col`} style={{ width: '60rem' }}>
+        <Title css={tw`text-center !text-6xl leading-6 !font-extrabold`}>
+          Build using&nbsp;
+          <span css={tw`bg-red-100 inline-block`} style={{ minWidth: '20rem' }}>
+            <span
+              ref={jsRotatingRef}
+              className="animate__animated animate__fadeIn"
+            >
+              Ant Design, Material UI, Semantic UI, HTML tags
+            </span>
+            <br />
+          </span>
+          <br />
+          without template limitations
+        </Title>
+        <Paragraph css={tw`text-center text-2xl px-16 leading-9 font-light`}>
+          Nest components or HTML to construct the DOM tree as you would in
+          code. Configure props provided by UI frameworks so you can save time.
+        </Paragraph>
+        <Space css={tw`w-full justify-center`} align="center" size="large">
+          <Fancybox
+            options={{
+              infinite: false,
+              closeButton: 'outside',
+            }}
+          >
+            <p>
+              <Button
+                size="large"
+                icon={<ArrowRightOutlined />}
+                data-fancybox="gallery"
+                data-src="https://www.youtube.com/watch?v=OrmhGmr0iTA"
+                type="primary"
+              >
+                Watch Tutorial
+              </Button>
+            </p>
+          </Fancybox>
+          <Button
+            size="large"
+            icon={<ArrowRightOutlined />}
+            type="primary"
+            ghost
+          >
+            Log In
           </Button>
-        </Card>
+        </Space>
       </Col>
 
       <div
