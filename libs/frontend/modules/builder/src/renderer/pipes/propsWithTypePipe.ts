31951f090fb7f2d06e81aa9ea1d6cbac4862ca72
assim <assimhabe@gmail.com>
2021-12-19 13:06:16 +0100

    refactor: propsWithTypePipe pipeline

--- libs/frontend/modules/builder/src/renderer/pipes/propsWithTypePipe.ts
+++ libs/frontend/modules/builder/src/renderer/pipes/propsWithTypePipe.ts
@@ -1,33 +1,45 @@
 import { TypeKind } from '@codelab/frontend/abstract/codegen'
 import { mergeProps } from '@codelab/shared/utils'
-import * as _ from 'lodash'
+import { mapValues, pickBy } from 'lodash'
 import { RenderPipeFactory } from './types'
+
+const handledTypeKinds = [
+  TypeKind.AppType,
+  TypeKind.ArrayType,
+  TypeKind.ComponentType,
+  TypeKind.EnumType,
+  TypeKind.ElementType,
+  TypeKind.InterfaceType,
+  TypeKind.LambdaType,
+  TypeKind.MonacoType,
+  TypeKind.PageType,
+  TypeKind.PrimitiveType,
+]
+
+/**
+ * Transforms props with the following format
+ *
+ *     {
+ *        [$propName]: {
+ *          typeKind: $typeKind,
+ *          value: $value,
+ *        },
+ *     }
+ *
+ *     into :
+ *
+ *     {
+ *        [$propName] : $value
+ *     }
+ */
 
 export const propsWithTypePipe: RenderPipeFactory =
   (next) => (element, context, props) => {
-    const { typeKindsById } = context
+    const propsWithType = pickBy(props, (value) =>
+      handledTypeKinds.includes(value?.typeKind),
+    )
 
-    const handledTypeKinds = new Set([
-      TypeKind.PrimitiveType,
-      TypeKind.ArrayType,
-      TypeKind.InterfaceType,
-      TypeKind.EnumType,
-      TypeKind.LambdaType,
-      TypeKind.AppType,
-      TypeKind.PageType,
-      TypeKind.ElementType,
-      TypeKind.ComponentType,
-      TypeKind.MonacoType,
-    ])
-
-    const propsWithType = _.pickBy(props, (value) => {
-      // should have either typekind directly or id as value.type
-      const propTypeKind = value?.typekind || typeKindsById[value?.type]
-
-      return handledTypeKinds.has(propTypeKind)
-    })
-
-    const transformedProps = _.mapValues(propsWithType, (value) => value.value)
+    const transformedProps = mapValues(propsWithType, (value) => value.value)
 
     return next(element, context, mergeProps(props, transformedProps))
   }
