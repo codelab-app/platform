482d8d713e6549c81fd68dadd2764784b56f8cc5
assim <assimhabe@gmail.com>
2021-12-19 11:05:58 +0100

    refactor: move & rename PropsByElementId to RenderPipelinePropsByElementId

--- libs/frontend/modules/builder/src/renderer/pipes/extraElementPropsPipe.spec.ts
+++ libs/frontend/modules/builder/src/renderer/pipes/extraElementPropsPipe.spec.ts
@@ -1,11 +1,5 @@
-<<<<<<< HEAD
 import { extraElementPropsPipe } from './extraElementPropsPipe'
 import { elementToRender, endPipe, EndPipeOutput } from './test'
-=======
-import { IElement } from '@codelab/shared/abstract/core'
-import { extraElementPropsPipe } from './extraElementPropsPipe'
-import { elementToRender } from './test'
->>>>>>> fd007128 (test: extraElementPropsPipe)
 import { RenderContext } from './types'
 
 const extraElementProps: RenderContext['extraElementProps'] = {
@@ -18,7 +12,6 @@ const extraElementProps: RenderContext['extraElementProps'] = {
 const defaultContext = { extraElementProps } as RenderContext
 const initialProps = {}
 
-<<<<<<< HEAD
 describe('ExtraElementPropsPipe', () => {
   it('should add element extra props', () => {
     const { props } = extraElementPropsPipe(endPipe)(
@@ -28,22 +21,5 @@ describe('ExtraElementPropsPipe', () => {
     ) as EndPipeOutput
 
     expect(props).toStrictEqual(extraElementProps[elementToRender.id])
-=======
-const restfulPipe = (
-  element: IElement,
-  context: RenderContext,
-  props: Record<string, unknown>,
-) => props
-
-describe('ExtraElementPropsPipe', () => {
-  it('should add element extra props', () => {
-    const restful = extraElementPropsPipe(restfulPipe)(
-      elementToRender,
-      defaultContext,
-      initialProps,
-    )
-
-    expect(restful).toStrictEqual(extraElementProps[elementToRender.id])
->>>>>>> fd007128 (test: extraElementPropsPipe)
   })
 })
