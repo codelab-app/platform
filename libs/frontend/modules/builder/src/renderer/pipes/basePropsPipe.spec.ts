9fd0ad46001ce645b845479a8e11627923e2be46
assim <assimhabe@gmail.com>
2021-12-19 09:58:42 +0100

    refactor: basePropsPipe spec

--- libs/frontend/modules/builder/src/renderer/pipes/basePropsPipe.spec.ts
+++ libs/frontend/modules/builder/src/renderer/pipes/basePropsPipe.spec.ts
@@ -1,13 +1,23 @@
+import { IElement } from '@codelab/shared/abstract/core'
 import { basePropsPipe } from './basePropsPipe'
 import { elementToRender } from './test'
 import { RenderContext } from './types'
 
+const defaultContext = {} as RenderContext
+const initialProps = {}
+
+const restfulPipe = (
+  element: IElement,
+  context: RenderContext,
+  props: Record<string, unknown>,
+) => props
+
 describe('BasePropsPipe', () => {
   it('should add base props', () => {
-    const restful = basePropsPipe((element, context, props) => props)(
+    const restful = basePropsPipe(restfulPipe)(
       elementToRender,
-      {} as RenderContext,
-      {},
+      defaultContext,
+      initialProps,
     )
 
     expect(restful).toStrictEqual({
