2b495c06208e49c7c1a256fc2b3cac63b7d911c1
assim <assimhabe@gmail.com>
2021-12-22 20:55:38 +0100

    test: propTransformationPipe

---
+++ libs/frontend/modules/builder/src/renderer/pipes/test/hookPipe.spec.ts
@@ -0,0 +1,36 @@
+import { IElement, IHook } from '@codelab/shared/abstract/core'
+import { RenderPipelineProps } from '../../../store'
+import { hookPipe } from '../hookPipe'
+import { RenderContext } from '../types'
+import { elementToRender } from './data'
+
+const testHookResponse: RenderPipelineProps = {
+  testHookResponse: {
+    prop01: 'value01',
+    prop02: 'value02',
+  },
+}
+
+const getHooksResponse = (hooks: Array<IHook>, props: RenderPipelineProps) =>
+  testHookResponse
+
+const defaultContext = { getHooksResponse } as RenderContext
+const initialProps = {}
+
+const restfulPipe = (
+  element: IElement,
+  context: RenderContext,
+  props: Record<string, unknown>,
+) => props
+
+describe('HookPipe', () => {
+  it('should hooks responses to props', () => {
+    const restful = hookPipe(restfulPipe)(
+      elementToRender,
+      defaultContext,
+      initialProps,
+    )
+
+    expect(restful).toStrictEqual(testHookResponse)
+  })
+})
