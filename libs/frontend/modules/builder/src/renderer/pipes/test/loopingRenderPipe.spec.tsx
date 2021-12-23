a842c51ca3a2b1e62aacb31742a1be539dd2135e
assim <assimhabe@gmail.com>
2021-12-23 20:58:35 +0100

    test: writing loopRenderingPipe

---
+++ libs/frontend/modules/builder/src/renderer/pipes/test/loopingRenderPipe.spec.tsx
@@ -0,0 +1,55 @@
+import { IElement } from '@codelab/shared/abstract/core'
+import { ReactElement } from 'react'
+import { loopingRenderPipe } from '../loopRenderingPipe'
+import { RenderContext } from '../types'
+import { elementToRender } from './data'
+
+const defaultContext = {} as RenderContext
+
+const initialProps = {
+  [elementToRender.renderForEachPropKey as string]: [
+    { prop01: 'prop01Value' },
+    { prop02: 'prop02Value' },
+    { prop03: 'prop03Value' },
+    { prop04: 'prop04Value' },
+  ],
+}
+
+const restfulPipe = (
+  element: IElement,
+  context: RenderContext,
+  props: Record<string, unknown>,
+) => props
+
+describe('LoopingRenderPipe', () => {
+  it('should add renderForEachPropKey props', () => {
+    const restful = loopingRenderPipe(restfulPipe)(
+      elementToRender,
+      defaultContext,
+      initialProps,
+    )
+
+    expect((restful as ReactElement).props.children).toStrictEqual([
+      {
+        key: `${elementToRender.id}-0`,
+        prop01: 'prop01Value',
+        ...initialProps,
+      },
+      {
+        key: `${elementToRender.id}-1`,
+        prop02: 'prop02Value',
+        ...initialProps,
+      },
+      {
+        key: `${elementToRender.id}-2`,
+        prop03: 'prop03Value',
+        ...initialProps,
+      },
+      {
+        key: `${elementToRender.id}-3`,
+        prop04: 'prop04Value',
+        ...initialProps,
+      },
+    ])
+  })
+})
