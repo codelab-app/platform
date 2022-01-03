4327e68edc00e37c0c07ee5335779c447df1bdab
Webber Wang <webber@codelab.ai>
2022-01-02 22:20:16 -0800

    feat: wip
    ci: fix slack username notification
    
    wip: import built css instead of less

--- apps/web/src/home/HomeClients.tsx
+++ apps/web/src/home/HomeClients.tsx
@@ -1,5 +1,9 @@
 import { TestimonialSection } from './TestimonialSection'
 
+const Testimonial = () => {
+  return null
+}
+
 export const HomeClients = () => {
   return (
     <section id="home-client" style={{ padding: '2rem 0' }}>
