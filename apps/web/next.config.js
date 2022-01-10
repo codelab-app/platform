22a5f4b376b5588af947668994f0c145e6d0ece5
Webber Wang <webber@codelab.ai>
2022-01-10 17:27:37 +0800

    wip: fixing webpack 5

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -19,107 +19,107 @@ const log = (obj) => {
  */
 module.exports = withPlugins(
   [
-    [
-      withAntdLess,
-      {
-        lessVarsFilePath: './src/styles/antd-theme.less',
-      },
-    ],
+    // [
+    //   withAntdLess,
+    //   {
+    //     lessVarsFilePath: './src/styles/antd-theme.less',
+    //   },
+    // ],
     withBundleAnalyzer,
   ],
   withNx({
     cssModules: false,
     webpack(config, options) {
-      const isServer = options.isServer
-      console.log({ isServer })
+      // const isServer = options.isServer
+      // console.log({ isServer })
 
-      // monaco-editor fix
-      // monkey patching, becareful when upgrading next.js
-      const monacoEditorTestRegex = /[\\/]node_modules[\\/]monaco-editor[\\/]/
-      // ignore monaco from error-loader
-      const rules = config.module.rules.find((rule) => rule?.oneOf).oneOf
+      // // monaco-editor fix
+      // // monkey patching, be careful when upgrading next.js
+      // const monacoEditorTestRegex = /[\\/]node_modules[\\/]monaco-editor[\\/]/
+      // // ignore monaco from error-loader
+      // const rules = config.module.rules.find((rule) => rule?.oneOf).oneOf
 
-      // patch css global error
-      const errorCssGlobal =
-        'Global CSS \x1B[1mcannot\x1B[22m be imported from files other than your \x1B[1mCustom <App>\x1B[22m. Due to the Global nature of stylesheets, and to avoid conflicts, Please move all first-party global CSS imports to \x1B[36mpages/_app.js\x1B[39m. Or convert the import to Component-Level CSS (CSS Modules).\n' +
-        'Read more: https://nextjs.org/docs/messages/css-global'
+      // // patch css global error
+      // const errorCssGlobal =
+      //   'Global CSS \x1B[1mcannot\x1B[22m be imported from files other than your \x1B[1mCustom <App>\x1B[22m. Due to the Global nature of stylesheets, and to avoid conflicts, Please move all first-party global CSS imports to \x1B[36mpages/_app.js\x1B[39m. Or convert the import to Component-Level CSS (CSS Modules).\n' +
+      //   'Read more: https://nextjs.org/docs/messages/css-global'
 
-      const cssGlobalErrorLoaderRule = config.module.rules
-        .find((rule) => rule?.oneOf)
-        .oneOf.find(
-          (rule) =>
-            rule?.use?.loader === 'error-loader' &&
-            rule?.use?.options?.reason === errorCssGlobal,
-        )
+      // const cssGlobalErrorLoaderRule = config.module.rules
+      //   .find((rule) => rule?.oneOf)
+      //   .oneOf.find(
+      //     (rule) =>
+      //       rule?.use?.loader === 'error-loader' &&
+      //       rule?.use?.options?.reason === errorCssGlobal,
+      //   )
 
-      if (cssGlobalErrorLoaderRule) {
-        cssGlobalErrorLoaderRule.issuer = {
-          not: monacoEditorTestRegex,
-        }
-      }
+      // if (cssGlobalErrorLoaderRule) {
+      //   cssGlobalErrorLoaderRule.issuer = {
+      //     not: monacoEditorTestRegex,
+      //   }
+      // }
 
-      // patch css-npm error
-      const errorCssNpm =
-        'Global CSS \x1B[1mcannot\x1B[22m be imported from within \x1B[1mnode_modules\x1B[22m.\n' +
-        'Read more: https://nextjs.org/docs/messages/css-npm'
+      // // patch css-npm error
+      // const errorCssNpm =
+      //   'Global CSS \x1B[1mcannot\x1B[22m be imported from within \x1B[1mnode_modules\x1B[22m.\n' +
+      //   'Read more: https://nextjs.org/docs/messages/css-npm'
 
-      const cssNpmErrorLoaderRule = config.module.rules
-        .find((rule) => rule?.oneOf)
-        .oneOf.find(
-          (rule) =>
-            rule?.use?.loader === 'error-loader' &&
-            rule?.use?.options?.reason === errorCssNpm,
-        )
+      // const cssNpmErrorLoaderRule = config.module.rules
+      //   .find((rule) => rule?.oneOf)
+      //   .oneOf.find(
+      //     (rule) =>
+      //       rule?.use?.loader === 'error-loader' &&
+      //       rule?.use?.options?.reason === errorCssNpm,
+      //   )
 
-      if (cssNpmErrorLoaderRule?.issuer) {
-        cssNpmErrorLoaderRule.issuer.not = monacoEditorTestRegex
-      }
+      // if (cssNpmErrorLoaderRule?.issuer) {
+      //   cssNpmErrorLoaderRule.issuer.not = monacoEditorTestRegex
+      // }
 
-      // allow monaco to load css
-      // use this rule instead of the bellow rule, because the bellow one has issuer.not = /node_modules/
-      // this is an exceptional, and intentional case
-      // next.js doesn't want node_modules to import global.css
-      const cssRuleApp = config.module.rules
-        .find((rule) => rule?.oneOf)
-        .oneOf.find((rule) => {
-          // log(rule)
-          // console.log('----------')
+      // // allow monaco to load css
+      // // use this rule instead of the bellow rule, because the bellow one has issuer.not = /node_modules/
+      // // this is an exceptional, and intentional case
+      // // next.js doesn't want node_modules to import global.css
+      // const cssRuleApp = config.module.rules
+      //   .find((rule) => rule?.oneOf)
+      //   .oneOf.find((rule) => {
+      //     // log(rule)
+      //     // console.log('----------')
 
-          return (
-            String(rule?.issuer?.and)?.includes('_app') &&
-            String(rule?.test) === String(/(?<!\.module)\.css$/)
-          )
-        })
+      //     return (
+      //       String(rule?.issuer?.and)?.includes('_app') &&
+      //       String(rule?.test) === String(/(?<!\.module)\.css$/)
+      //     )
+      //   })
 
-      if (cssRuleApp?.issuer?.and) {
-        cssRuleApp.issuer.or = [
-          /node_modules\/monaco-editor\/esm/,
-          ...cssRuleApp.issuer.and,
-        ]
-        delete cssRuleApp.issuer.and
-      }
+      // if (cssRuleApp?.issuer?.and) {
+      //   cssRuleApp.issuer.or = [
+      //     /node_modules\/monaco-editor\/esm/,
+      //     ...cssRuleApp.issuer.and,
+      //   ]
+      //   delete cssRuleApp.issuer.and
+      // }
 
-      // https://nextjs.org/docs/basic-features/built-in-css-support
-      // Since Next.js 9.5.4, importing a CSS file from node_modules is permitted anywhere in your application.
-      // but not from bower...
-      // this supports importing css from bower
+      // // https://nextjs.org/docs/basic-features/built-in-css-support
+      // // Since Next.js 9.5.4, importing a CSS file from node_modules is permitted anywhere in your application.
+      // // but not from bower...
+      // // this supports importing css from bower
 
-      const cssRuleWeb = config.module.rules
-        .find((rule) => rule?.oneOf)
-        .oneOf.find((rule) => {
-          // log(rule)
-          // console.log('----------')
+      // const cssRuleWeb = config.module.rules
+      //   .find((rule) => rule?.oneOf)
+      //   .oneOf.find((rule) => {
+      //     // log(rule)
+      //     // console.log('----------')
 
-          return (
-            String(rule?.issuer?.or)?.includes('/apps/web') &&
-            String(rule?.test) === String(/(?<!\.module)\.css$/)
-          )
-        })
+      //     return (
+      //       String(rule?.issuer?.or)?.includes('/apps/web') &&
+      //       String(rule?.test) === String(/(?<!\.module)\.css$/)
+      //     )
+      //   })
 
-      if (cssRuleWeb?.include?.and) {
-        cssRuleWeb.include.or = [/bower_components/, ...cssRuleWeb.include.and]
-        delete cssRuleWeb.include.and
-      }
+      // if (cssRuleWeb?.include?.and) {
+      //   cssRuleWeb.include.or = [/bower_components/, ...cssRuleWeb.include.and]
+      //   delete cssRuleWeb.include.and
+      // }
 
       // https://github.com/prettier/prettier/issues/4959#issuecomment-416834237
       config.plugins.push(
