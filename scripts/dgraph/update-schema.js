9fea66a4dd59345bb72edc6ee61c1e4e82ae9d0f
Webber Wang <webber@codelab.ai>
2022-01-11 22:14:04 +0800

    ci: fix script import

--- scripts/dgraph/update-schema.js
+++ scripts/dgraph/update-schema.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
 const shell = require('shelljs')
-const { env } = require('scripts/env-config')
+const { env } = require('../env-config')
 
 shell.exec(`yarn cli dgraph update-schema --env ${env}`)
