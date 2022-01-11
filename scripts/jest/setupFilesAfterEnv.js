b21d22fb707e4552e1de2f727b307cf4c62d1457
Webber Wang <webber@codelab.ai>
2022-01-11 22:09:31 +0800

    refactor: remove scripts lib
    trigger dgraph schema update once before pre-push only

--- scripts/jest/setupFilesAfterEnv.js
+++ scripts/jest/setupFilesAfterEnv.js
@@ -1,11 +1,6 @@
 const shell = require('shelljs')
 const { env } = require('../env-config')
 
-global.beforeAll(() => {
-  console.log('setupFilesAfterEnv beforeAll')
-  // shell.exec(`yarn cli dgraph update-schema --env ${env}`)
-})
-
-global.beforeAll(() => {
+global.beforeAll(async () => {
   // shell.exec(`yarn cli dgraph reset-data --env ${env}`)
 })
