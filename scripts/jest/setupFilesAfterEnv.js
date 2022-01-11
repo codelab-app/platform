dbc5e42424e5d7b9a8969490ae6d11c6dca63984
Webber Wang <webber@codelab.ai>
2022-01-11 21:57:44 +0800

    test: reduce update-schema call

--- scripts/jest/setupFilesAfterEnv.js
+++ scripts/jest/setupFilesAfterEnv.js
@@ -1,6 +1,11 @@
 const shell = require('shelljs')
 const { env } = require('../env-config')
 
-global.beforeAll(async () => {
+global.beforeAll(() => {
+  console.log('setupFilesAfterEnv beforeAll')
+  // shell.exec(`yarn cli dgraph update-schema --env ${env}`)
+})
+
+global.beforeAll(() => {
   // shell.exec(`yarn cli dgraph reset-data --env ${env}`)
 })
