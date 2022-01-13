a5009e0fdb31dc49de2f83df1e10e4c88c78927d
Webber Wang <webber@codelab.ai>
2022-01-13 16:37:40 +0800

    build: update docker tag

--- .docker/startup.sh
+++ .docker/startup.sh
@@ -2,14 +2,9 @@
 
 # Debugging
 
-echo "Starting..."
-
-ls
-ls node_modules
-
 # This script is called by ECS
 
-# node dist/apps/cli/main.js dgraph update-schema --env ci
-# node dist/apps/cli/main.js dgraph reset-data --env ci
+node dist/apps/cli/main.js dgraph update-schema --env ci
+node dist/apps/cli/main.js dgraph reset-data --env ci
 
-# node dist/apps/api/main.js
+node dist/apps/api/main.js
