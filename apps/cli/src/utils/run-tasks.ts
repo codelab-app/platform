983915c81483b32078b8a0f761f92d1919602b88
Webber Wang <webber@codelab.ai>
2022-01-29 19:19:32 +0800

    ci: modify nx build
    ci: modify nx build
    
    ci: modify nx build

--- apps/cli/src/utils/run-tasks.ts
+++ apps/cli/src/utils/run-tasks.ts
@@ -4,128 +4,96 @@ import { Tasks } from './tasks'
 
 const NX_TEST = 'npx env-cmd -f .env.test nx'
 
+const execCommand = (command: string) => {
+  execa.commandSync(command, {
+    stdio: 'inherit',
+  })
+}
+
 export const runTasks = (env: TaskEnv, task: string, args?: string) => {
   // console.log(env, task)
 
   switch (task) {
     case Tasks.Build:
       if (env === TaskEnv.Test) {
-        execa.command(`${NX_TEST} affected:build --configuration=test`, {
-          stdio: 'inherit',
-        })
+        execCommand(`${NX_TEST} affected:build --configuration=test`)
       }
 
       if (env === TaskEnv.Ci) {
-        execa.command(
+        execCommand(
           'npx nx run-many --target=build --projects=api,web,cmd,cli,tools-rtk-query --configuration-ci --verbose',
-          {
-            stdio: 'inherit',
-          },
         )
       }
 
       break
     case Tasks.Lint:
       if (env === TaskEnv.Test) {
-        execa.command(`yarn cross-env TIMING=1 lint-staged --verbose`, {
-          stdio: 'inherit',
-        })
+        execCommand(`yarn cross-env TIMING=1 lint-staged --verbose`)
       }
 
       if (env === TaskEnv.Ci) {
-        execa.command(
+        execCommand(
           `npx nx affected:lint --configuration=ci --quiet && npx prettier --check '**/*,{graphql.yaml.json}'`,
-          {
-            stdio: 'inherit',
-          },
         )
       }
 
       break
     case Tasks.Unit:
       if (env === TaskEnv.Test) {
-        execa.command(
+        execCommand(
           `${NX_TEST} affected:test --testPathPattern="[^i].spec.ts" --memoryLimit=8192 --color`,
-          {
-            stdio: 'inherit',
-          },
         )
       }
 
       if (env === TaskEnv.Ci) {
-        execa.command(
+        execCommand(
           `npx nx affected:test --testPathPattern="[^i].spec.ts" --verbose --color`,
-          {
-            stdio: 'inherit',
-          },
         )
       }
 
       break
     case Tasks.Int:
       if (env === TaskEnv.Test) {
-        execa.command(
+        execCommand(
           `${NX_TEST} affected:test --testPathPattern="i.spec.ts" --parallel=1 --memoryLimit=8192 --runInBand`,
-          {
-            stdio: 'inherit',
-          },
         )
       }
 
       if (env === TaskEnv.Ci) {
-        execa.command(
+        execCommand(
           `npx nx affected:test --testPathPattern="i.spec.ts" --parallel=1 --runInBand --verbose`,
-          {
-            stdio: 'inherit',
-          },
         )
       }
 
       break
     case Tasks.E2e:
       if (env === TaskEnv.Test) {
-        execa.command(
+        execCommand(
           `${NX_TEST} affected:e2e --configuration=test --browser firefox`,
-          {
-            stdio: 'inherit',
-          },
         )
       }
 
       if (env === TaskEnv.Ci) {
-        execa.command(
+        execCommand(
           `yarn affected:e2e --configuration ci --record --browser firefox`,
-          {
-            stdio: 'inherit',
-          },
         )
       }
 
       break
     case Tasks.Commitlint:
       if (env === TaskEnv.Test) {
-        execa.command(`npx --no-install commitlint --edit ${args}`, {
-          stdio: 'inherit',
-        })
+        execCommand(`npx --no-install commitlint --edit ${args}`)
       }
 
       if (env === TaskEnv.Ci) {
-        execa.command(
+        execCommand(
           `yarn affected:e2e --configuration ci --record --browser firefox`,
-          {
-            stdio: 'inherit',
-          },
         )
       }
 
       break
     case Tasks.Circularlint:
-      execa.command(
-        `yarn madge --circular apps libs --extensions ts,tsx,js,jsx`,
-        {
-          stdio: 'inherit',
-        },
-      )
+      execCommand(`yarn madge --circular apps libs --extensions ts,tsx,js,jsx`)
 
       break
     default:
--- jest.config.js
+++ jest.config.js
@@ -2,11 +2,5 @@ const { getJestProjects } = require('@nrwl/jest')
 
 module.exports = {
   projects: getJestProjects(),
-  transformIgnorePatterns: ['^.+\\.js$'],
   // bail: process.env.CI ? true : false,
-  globals: {
-    'ts-jest': {
-      isolatedModules: true,
-    },
-  },
 }
