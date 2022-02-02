8c5463f687565501b2067290ffdd7b25414a535d
Webber Wang <webber@codelab.ai>
2022-02-02 20:16:31 +0800

    wip: add upsert user inside nextjs api

--- apps/web/pages/api/v2/graphql.ts
+++ apps/web/pages/api/v2/graphql.ts
@@ -1,14 +1,18 @@
+import { getSession } from '@auth0/nextjs-auth0'
+import { OGM } from '@neo4j/graphql-ogm'
 import { ApolloServer, gql } from 'apollo-server-micro'
 import fs from 'fs'
 import {
   buildClientSchema,
   getIntrospectionQuery,
   IntrospectionQuery,
+  print,
   printSchema,
 } from 'graphql'
 import { NextApiHandler } from 'next'
 import { getDriver } from '../../../src/neo4j-graphql/getDriver'
 import { getSchema } from '../../../src/neo4j-graphql/getSchema'
+import typeDefs from '../../../src/neo4j-graphql/typeDefs'
 
 const driver = getDriver()
 const neoSchema = getSchema(driver)
@@ -22,6 +26,9 @@ const apolloServer = new ApolloServer({
     return {}
   },
 })
+
+const ogm = new OGM({ typeDefs: print(typeDefs), driver })
+const User = ogm.model('User')
 
 const startServer = apolloServer.start().then(async () => {
   /**
@@ -43,6 +50,38 @@ const startServer = apolloServer.start().then(async () => {
 })
 
 const handler: NextApiHandler = async (req, res) => {
+  // Get Next.js Auth0 session
+  const session = await getSession(req, res)
+  const accessToken = session?.accessToken
+
+  /**
+   * Check for upsert only when user exists
+   */
+  if (session?.user) {
+    const user = session.user
+
+    const [existing] = await User.find({
+      where: {
+        auth0Id: user.sub,
+      },
+    })
+
+    if (existing) {
+      console.log(`User with email ${user.email} already exists!`)
+    } else {
+      const { users } = await User.create({
+        input: [
+          {
+            auth0Id: user.sub,
+            email: user.email,
+          },
+        ],
+      })
+
+      console.log('Created', users)
+    }
+  }
+
   res.setHeader('Access-Control-Allow-Credentials', 'true')
   res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader('Access-Control-Allow-Headers', '*')
