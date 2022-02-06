7160a91a15718a8aa9d177adbf0942ac84ab8758
assim habeek <assimhabe@gmail.com>
2022-02-06 12:57:15 +0100

    feat: update element

--- libs/frontend/modules/element/src/graphql/Element.fragment.v2.graphql.gen.ts
+++ libs/frontend/modules/element/src/graphql/Element.fragment.v2.graphql.gen.ts
@@ -1,152 +1,100 @@
-import * as Types from '@codelab/shared/abstract/codegen-v2'
+import * as Types from '@codelab/shared/abstract/codegen-v2';
 
-import { gql } from '@apollo/client'
-export type ElementBaseFragment = {
-  __typename: 'Element'
-  id: string
-  name?: string | null | undefined
-  css?: string | null | undefined
-  props?: string | null | undefined
-  hooks?: Array<string> | null | undefined
-  renderForEachPropKey?: string | null | undefined
-  renderIfPropKey?: string | null | undefined
-  propTransformationJs?: string | null | undefined
-  instanceOfComponent?: { id: string } | null | undefined
-  parentElement?:
-    | { id: string; name?: string | null | undefined }
-    | null
-    | undefined
-  atom?: { id: string; name: string; type: Types.AtomType } | null | undefined
-  componentTag?: { id: string; name: string } | null | undefined
-  propMapBindings?:
-    | Array<{ id: string; sourceKey: string; targetKey: string }>
-    | null
-    | undefined
-}
+import { gql } from '@apollo/client';
+export type ElementBaseFragment = { __typename: 'Element', id: string, name?: string | null | undefined, css?: string | null | undefined, props?: string | null | undefined, hooks?: Array<string> | null | undefined, renderForEachPropKey?: string | null | undefined, renderIfPropKey?: string | null | undefined, propTransformationJs?: string | null | undefined, instanceOfComponent?: { id: string } | null | undefined, parentElement?: { id: string, name?: string | null | undefined } | null | undefined, atom?: { id: string, name: string, type: Types.AtomType } | null | undefined, componentTag?: { id: string, name: string } | null | undefined, propMapBindings?: Array<{ id: string, sourceKey: string, targetKey: string }> | null | undefined };
 
-export type ElementEdgeFragment = {
-  source: string
-  target: string
-  order: number
-}
+export type ElementEdgeFragment = { source: string, target: string, order: number };
 
-export type ElementGraphFragment = {
-  vertices?: Array<ElementBaseFragment> | null | undefined
-  edges?: Array<ElementEdgeFragment> | null | undefined
-}
+export type ElementGraphFragment = { vertices?: Array<ElementBaseFragment> | null | undefined, edges?: Array<ElementEdgeFragment> | null | undefined };
 
-export type ElementFragment = {
-  __typename: 'Element'
-  id: string
-  name?: string | null | undefined
-  css?: string | null | undefined
-  props?: string | null | undefined
-  hooks?: Array<string> | null | undefined
-  renderForEachPropKey?: string | null | undefined
-  renderIfPropKey?: string | null | undefined
-  propTransformationJs?: string | null | undefined
-  instanceOfComponent?: { id: string } | null | undefined
-  parentElement?:
-    | { id: string; name?: string | null | undefined }
-    | null
-    | undefined
-  atom?: { id: string; name: string; type: Types.AtomType } | null | undefined
-  graph?: ElementGraphFragment | null | undefined
-  componentTag?: { id: string; name: string } | null | undefined
-  propMapBindings?:
-    | Array<{ id: string; sourceKey: string; targetKey: string }>
-    | null
-    | undefined
-}
+export type ElementFragment = { __typename: 'Element', id: string, name?: string | null | undefined, css?: string | null | undefined, props?: string | null | undefined, hooks?: Array<string> | null | undefined, renderForEachPropKey?: string | null | undefined, renderIfPropKey?: string | null | undefined, propTransformationJs?: string | null | undefined, instanceOfComponent?: { id: string } | null | undefined, parentElement?: { id: string, name?: string | null | undefined } | null | undefined, atom?: { id: string, name: string, type: Types.AtomType } | null | undefined, graph?: ElementGraphFragment | null | undefined, componentTag?: { id: string, name: string } | null | undefined, propMapBindings?: Array<{ id: string, sourceKey: string, targetKey: string }> | null | undefined };
 
 export const ElementBaseFragmentDoc = gql`
-  fragment ElementBase on Element {
-    __typename
+    fragment ElementBase on Element {
+  __typename
+  id
+  name
+  css
+  instanceOfComponent {
+    id
+  }
+  parentElement {
     id
     name
-    css
-    instanceOfComponent {
-      id
-    }
-    parentElement {
-      id
-      name
-    }
-    atom {
-      id
-      name
-      type
-    }
-    componentTag {
-      id
-      name
-    }
-    props
-    hooks
-    renderForEachPropKey
-    renderIfPropKey
-    propMapBindings {
-      id
-      sourceKey
-      targetKey
-    }
-    propTransformationJs
   }
-`
+  atom {
+    id
+    name
+    type
+  }
+  componentTag {
+    id
+    name
+  }
+  props
+  hooks
+  renderForEachPropKey
+  renderIfPropKey
+  propMapBindings {
+    id
+    sourceKey
+    targetKey
+  }
+  propTransformationJs
+}
+    `;
 export const ElementEdgeFragmentDoc = gql`
-  fragment ElementEdge on ElementEdge {
-    source
-    target
-    order
-  }
-`
+    fragment ElementEdge on ElementEdge {
+  source
+  target
+  order
+}
+    `;
 export const ElementGraphFragmentDoc = gql`
-  fragment ElementGraph on IElementGraph {
-    vertices {
-      ...ElementBase
-    }
-    edges {
-      ...ElementEdge
-    }
+    fragment ElementGraph on IElementGraph {
+  vertices {
+    ...ElementBase
   }
-  ${ElementBaseFragmentDoc}
-  ${ElementEdgeFragmentDoc}
-`
+  edges {
+    ...ElementEdge
+  }
+}
+    ${ElementBaseFragmentDoc}
+${ElementEdgeFragmentDoc}`;
 export const ElementFragmentDoc = gql`
-  fragment Element on Element {
-    __typename
+    fragment Element on Element {
+  __typename
+  id
+  name
+  css
+  instanceOfComponent {
+    id
+  }
+  parentElement {
     id
     name
-    css
-    instanceOfComponent {
-      id
-    }
-    parentElement {
-      id
-      name
-    }
-    atom {
-      id
-      name
-      type
-    }
-    graph {
-      ...ElementGraph
-    }
-    componentTag {
-      id
-      name
-    }
-    props
-    hooks
-    renderForEachPropKey
-    renderIfPropKey
-    propMapBindings {
-      id
-      sourceKey
-      targetKey
-    }
-    propTransformationJs
   }
-  ${ElementGraphFragmentDoc}
-`
+  atom {
+    id
+    name
+    type
+  }
+  graph {
+    ...ElementGraph
+  }
+  componentTag {
+    id
+    name
+  }
+  props
+  hooks
+  renderForEachPropKey
+  renderIfPropKey
+  propMapBindings {
+    id
+    sourceKey
+    targetKey
+  }
+  propTransformationJs
+}
+    ${ElementGraphFragmentDoc}`;
