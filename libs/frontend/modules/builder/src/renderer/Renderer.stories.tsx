3ef2eae3686ded16b53b5c1ae23badcd7a6d9d44
Webber Wang <webber@codelab.ai>
2022-02-13 21:14:58 +0800

    build: fix storybook with lib .babelrc

---
+++ libs/frontend/modules/builder/src/renderer/Renderer.stories.tsx
@@ -0,0 +1,10 @@
+import { Renderer } from './Renderer'
+
+export default {
+  /* 👇 The title prop is optional.
+   * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
+   * to learn how to generate automatic titles
+   */
+  title: 'Renderer',
+  component: Renderer,
+}
