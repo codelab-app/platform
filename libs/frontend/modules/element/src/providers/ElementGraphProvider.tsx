9c071975202e38e33bb3eb15f5aef8f54c02b4d6
assim habeek <assimhabe@gmail.com>
2022-02-13 12:24:54 +0100

    feat: convert element to component

--- libs/frontend/modules/element/src/providers/ElementGraphProvider.tsx
+++ libs/frontend/modules/element/src/providers/ElementGraphProvider.tsx
@@ -34,8 +34,6 @@ export const ElementGraphProvider = ({
     variables: { input: { rootId: elementId } },
   })
 
-  console.log(data)
-
   const { setCurrentGraphRoot } = useElementDispatch()
 
   useEffect(() => {
