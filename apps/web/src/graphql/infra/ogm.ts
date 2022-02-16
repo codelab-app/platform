0406282e21c9d03a01dc56d30e5b5d21d866c226
Anton Mihaylov <antonmmihaylov@gmail.com>
2022-02-16 19:07:04 +0200

    chore: remove old files

--- apps/web/src/graphql/infra/ogm.ts
+++ apps/web/src/graphql/infra/ogm.ts
@@ -3,7 +3,8 @@ import { ModelMap } from '../ogm-types.gen'
 import typeDefs from '../schema/typeDefs'
 import { getDriver } from './driver'
 
-// Keep a single OGM instance if possible
+// Since we're running in a serverless environment, we don't want to instantiate the ogm
+// and all models on every request. By using functions and singleton instances we can instantiate only what we need
 let ogm: OGM<ModelMap>
 
 export const getOgm = () => (ogm ??= new OGM({ typeDefs, driver: getDriver() }))
