c59ac4382db0734fbc129cc249c55132c87348d4
assim habeek <assimhabe@gmail.com>
2022-04-03 14:23:10 +0100

    feat: attach to renderer

--- libs/frontend/modules/store/src/store/action.model.ts
+++ libs/frontend/modules/store/src/store/action.model.ts
@@ -1,20 +1,17 @@
-import { detach, idProp, Model, model, prop, Ref, rootRef } from 'mobx-keystone'
+import { detach, idProp, Model, model, prop, rootRef } from 'mobx-keystone'
 import { ActionFragment } from '../graphql/Action.fragment.v2.1.graphql.gen'
-import { Store, storeRef } from './store.model'
 
 @model('codelab/Action')
 export class Action extends Model({
   id: idProp,
   name: prop<string>(),
   body: prop<string>(),
-  store: prop<Ref<Store>>(),
 }) {
   static fromFragment(action: ActionFragment) {
     return new Action({
       body: action.body,
       name: action.name,
       id: action.id,
-      store: storeRef(action.store.id),
     })
   }
 }
