334a33171d58751462fa3c3bc732ad065b7d2b0d
Ilia <scientists.twin@gmail.com>
2022-04-14 10:41:04 +0400

    feat: app authorization completed

---
+++ apps/web/src/graphql/schema/app.schema.ts
@@ -0,0 +1,25 @@
+import { gql } from 'apollo-server-micro'
+
+export const appSchema = gql`
+  type App {
+    id: ID! @id
+    owner: [User!]! @relationship(type: "OWNED_BY", direction: OUT)
+    name: String!
+    pages: [Page!]! @relationship(type: "PAGES", direction: IN)
+    rootProviderElement: Element! @relationship(type: "ROOT", direction: IN)
+    store: Store @relationship(type: "STORE_OF_APP", direction: IN)
+  }
+
+  extend type App
+    @auth(
+      rules: [
+        {
+          operations: [READ, UPDATE, CREATE]
+          roles: ["User", "Guest"]
+          where: { owner: { auth0Id: "$jwt.sub" } }
+          bind: { owner: { auth0Id: "$jwt.sub" } }
+        }
+        { operations: [READ, UPDATE, CREATE], roles: ["Admin"] }
+      ]
+    )
+`
