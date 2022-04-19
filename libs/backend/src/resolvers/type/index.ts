501a83b4febff68c3171689d1ac2193dc85a7308
Webber Wang <webber@codelab.ai>
2022-04-18 20:50:38 -0700

    refactor: remove custom field resolvers

--- libs/backend/src/resolvers/type/index.ts
+++
@@ -1,8 +0,0 @@
-import { IResolvers } from '@graphql-tools/utils'
-
-export const typeResolver: IResolvers = {
-  Mutation: {
-    // deleteFieldEdge: withRxTransaction(deleteFieldEdge),
-    // upsertFieldEdge: withRxTransaction(upsertFieldEdge),
-  },
-}
