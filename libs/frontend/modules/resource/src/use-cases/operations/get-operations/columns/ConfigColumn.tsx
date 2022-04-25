20856af5c2be702dd0a4a6a0a631f6830883bcf8
assim habeek <assimhabe@gmail.com>
2022-04-25 02:34:37 +0100

    fix: build error

--- libs/frontend/modules/resource/src/use-cases/operations/get-operations/columns/ConfigColumn.tsx
+++ libs/frontend/modules/resource/src/use-cases/operations/get-operations/columns/ConfigColumn.tsx
@@ -11,7 +11,9 @@ export const ConfigColumn = observer<ConfigColumnProps>(({ operation }) => {
   return (
     <Descriptions column={1} size="small">
       {keys(operation.config).map((x) => (
-        <Descriptions.Item label={x}>{operation.config[x]}</Descriptions.Item>
+        <Descriptions.Item label={x}>
+          {operation.config[x as keyof Operation['config']] as string}
+        </Descriptions.Item>
       ))}
     </Descriptions>
   )
