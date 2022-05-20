88f8696d3daf3fcba4039952c5818147b2b9cc19
assim habeek <assimhabe@gmail.com>
2022-05-20 14:49:01 +0100

    refactor: traversal functions

---
+++ libs/shared/utils/src/traversal/deepReplaceObjectValues.ts
@@ -0,0 +1,18 @@
+import { isObject, mapValues } from 'lodash'
+import { IInput, RefSet, TransformFn } from './abstract'
+import { traverseDeep } from './traverseDeep'
+
+const traverseObjectValues = (
+  obj: IInput,
+  replace: TransformFn,
+  _refs: RefSet,
+) =>
+  mapValues(obj, (value, key) =>
+    // isObjects includes array too
+    isObject(value)
+      ? traverseDeep(value, traverseObjectValues, replace, _refs)
+      : replace(value, key, obj),
+  )
+
+export const deepReplaceObjectValues = (obj: IInput, replacer: TransformFn) =>
+  traverseDeep(obj, traverseObjectValues, replacer)
