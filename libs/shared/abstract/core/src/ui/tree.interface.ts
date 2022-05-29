14b82b792e242d53578315e85473cc51051c7127
assim habeek <assimhabe@gmail.com>
2022-05-29 15:46:26 +0100

    fix: build issue

--- libs/shared/abstract/core/src/ui/tree.interface.ts
+++ libs/shared/abstract/core/src/ui/tree.interface.ts
@@ -1,10 +1,6 @@
 import { DataNode } from 'antd/lib/tree'
 import * as React from 'react'
-import {
-  COMPONENT_NODE_TYPE,
-  ELEMENT_NODE_TYPE,
-  STORE_NODE_TYPE,
-} from '../base/node.interface'
+import { COMPONENT_NODE_TYPE, ELEMENT_NODE_TYPE } from '../base/node.interface'
 
 export interface IBuilderDataNode extends DataNode {
   /**
@@ -12,7 +8,7 @@ export interface IBuilderDataNode extends DataNode {
    *
    * - Context menus (different for element vs component)
    */
-  type: COMPONENT_NODE_TYPE | ELEMENT_NODE_TYPE | STORE_NODE_TYPE | null
+  type: COMPONENT_NODE_TYPE | ELEMENT_NODE_TYPE | null
   // This is the id
   key: string | number
   title?: React.ReactNode
