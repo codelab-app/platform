b70f068d7ca4a41a5754a97b3b07e1cd6622b7ca
Webber Wang <webber@codelab.ai>
2022-06-15 20:33:11 -0700

    wip: working slug that renders individual pages for production

---
+++ libs/shared/abstract/core/src/domain/app/app.fragment.graphql.gen.ts
@@ -0,0 +1,86 @@
+import * as Types from '@codelab/shared/abstract/codegen'
+
+import { PageFragment } from '../page/page.fragment.graphql.gen'
+import { GraphQLClient } from 'graphql-request'
+import * as Dom from 'graphql-request/dist/types.dom'
+import { gql } from 'graphql-tag'
+import { PageFragmentDoc } from '../page/page.fragment.graphql.gen'
+export type AppPreviewFragment = {
+  id: string
+  name: string
+  slug: string
+  owner: { id: string }
+  pages: Array<{ id: string }>
+  rootElement: { id: string }
+  store: { id: string }
+}
+
+export type AppFragment = {
+  id: string
+  name: string
+  slug: string
+  owner: { id: string }
+  pages: Array<PageFragment>
+  rootElement: { id: string }
+  store: { id: string }
+}
+
+export const AppPreviewFragmentDoc = gql`
+  fragment AppPreview on App {
+    id
+    name
+    slug
+    owner {
+      id
+    }
+    pages {
+      id
+    }
+    rootElement {
+      id
+    }
+    store {
+      id
+    }
+  }
+`
+export const AppFragmentDoc = gql`
+  fragment App on App {
+    id
+    name
+    slug
+    owner {
+      id
+    }
+    pages {
+      ...Page
+    }
+    rootElement {
+      id
+    }
+    store {
+      id
+    }
+  }
+  ${PageFragmentDoc}
+`
+
+export type SdkFunctionWrapper = <T>(
+  action: (requestHeaders?: Record<string, string>) => Promise<T>,
+  operationName: string,
+  operationType?: string,
+) => Promise<T>
+
+const defaultWrapper: SdkFunctionWrapper = (
+  action,
+  _operationName,
+  _operationType,
+) => action()
+
+export function getSdk(
+  client: GraphQLClient,
+  withWrapper: SdkFunctionWrapper = defaultWrapper,
+) {
+  return {}
+}
+export type Sdk = ReturnType<typeof getSdk>
