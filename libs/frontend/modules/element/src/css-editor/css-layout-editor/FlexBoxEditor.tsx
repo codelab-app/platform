f75cce8987a918511ed3a891a61336089a10c801
Mohammed S. Yaseen <moha.98.1900@gmail.com>
2022-07-21 18:45:15 +0300

    feat(libs-frontend-modules): #1645 add SizeEditor
    feat: rename FlexBoxEditor to DisplayEditor and add display option
    wip: add collapse and hide each
    section uder its corresponding header

--- libs/frontend/modules/element/src/css-editor/css-layout-editor/FlexBoxEditor.tsx
+++ libs/frontend/modules/element/src/css-editor/css-layout-editor/FlexBoxEditor.tsx
@@ -5,26 +5,21 @@ import { observer } from 'mobx-react-lite'
 import React from 'react'
 import { makeMenu, updateGuiCssProperty } from '../utils'
 
-type FlexBoxEditorProps = {
+type DisplayEditorProps = {
   element: IElement
   guiCssObj: cssMap
 }
 
 export const FlexBoxEditor = observer(
-  ({ element, guiCssObj }: FlexBoxEditorProps) => {
+  ({ element, guiCssObj }: DisplayEditorProps) => {
+    const displayMenu = makeMenu(
+      ['block', 'flex'],
+      updateGuiCssProperty(element, 'display'),
+    )
+
     const flexDirectionMenu = makeMenu(
       ['none', 'row', 'row-reverse', 'column', 'column-reverse'],
-      (val) => {
-        if (val === 'none') {
-          element.deleteFromGuiCss(['display'])
-        } else {
-          element.appendToGuiCss({
-            display: 'flex',
-          })
-        }
-
-        updateGuiCssProperty(element, 'flex-direction')(val)
-      },
+      updateGuiCssProperty(element, 'flex-direction'),
     )
 
     const alignItemsMenu = makeMenu(
@@ -44,52 +39,64 @@ export const FlexBoxEditor = observer(
 
     return (
       <>
-        <Dropdown overlay={flexDirectionMenu}>
+        <Dropdown overlay={displayMenu}>
           <Button>
             <Space>
-              flex-direction:{' '}
-              <span>{guiCssObj['flex-direction'] ?? 'none'}</span>
+              display: <span>{guiCssObj['display'] ?? 'none'}</span>
               <DownOutlined />
             </Space>
           </Button>
         </Dropdown>
-        <Dropdown overlay={alignItemsMenu}>
-          <Button>
+        {guiCssObj['display'] !== 'flex' ? null : (
+          <>
+            <Dropdown overlay={flexDirectionMenu}>
+              <Button>
+                <Space>
+                  flex-direction:{' '}
+                  <span>{guiCssObj['flex-direction'] ?? 'none'}</span>
+                  <DownOutlined />
+                </Space>
+              </Button>
+            </Dropdown>
+            <Dropdown overlay={alignItemsMenu}>
+              <Button>
+                <Space>
+                  align-items: <span>{guiCssObj['align-items'] ?? 'none'}</span>
+                  <DownOutlined />
+                </Space>
+              </Button>
+            </Dropdown>
+            <Dropdown overlay={justifyContentMenu}>
+              <Button>
+                <Space>
+                  justify-content:{' '}
+                  <span>{guiCssObj['justify-content'] ?? 'none'}</span>
+                  <DownOutlined />
+                </Space>
+              </Button>
+            </Dropdown>
+            <Dropdown overlay={flexWrapMenu}>
+              <Button>
+                <Space>
+                  flex-wrap: <span>{guiCssObj['flex-wrap'] ?? 'none'}</span>
+                  <DownOutlined />
+                </Space>
+              </Button>
+            </Dropdown>
             <Space>
-              align-items: <span>{guiCssObj['align-items'] ?? 'none'}</span>
-              <DownOutlined />
+              <span>flex-grow: </span>
+              <InputNumber
+                defaultValue={parseFloat(guiCssObj['flex-grow'] ?? '0')}
+                max={1}
+                min={0}
+                onChange={(val) =>
+                  updateGuiCssProperty(element, 'flex-grow')(`${val}`)
+                }
+                step={0.1}
+              />
             </Space>
-          </Button>
-        </Dropdown>
-        <Dropdown overlay={justifyContentMenu}>
-          <Button>
-            <Space>
-              justify-content:{' '}
-              <span>{guiCssObj['justify-content'] ?? 'none'}</span>
-              <DownOutlined />
-            </Space>
-          </Button>
-        </Dropdown>
-        <Dropdown overlay={flexWrapMenu}>
-          <Button>
-            <Space>
-              flex-wrap: <span>{guiCssObj['flex-wrap'] ?? 'none'}</span>
-              <DownOutlined />
-            </Space>
-          </Button>
-        </Dropdown>
-        <Space>
-          <span>flex-grow: </span>
-          <InputNumber
-            defaultValue={parseFloat(guiCssObj['flex-grow'] ?? '0')}
-            max={1}
-            min={0}
-            onChange={(val) =>
-              updateGuiCssProperty(element, 'flex-grow')(`${val}`)
-            }
-            step={0.1}
-          />
-        </Space>
+          </>
+        )}
       </>
     )
   },
