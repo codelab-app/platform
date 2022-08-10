293bbbc32dc023e535c948801ec12e7107584577
assim habeek <assimhabe@gmail.com>
2022-08-10 14:48:33 +0100

    fix: action issues

---
+++ libs/frontend/modules/store/src/store/createActionFn.ts
@@ -0,0 +1,48 @@
+import { IActionKind, IAnyAction } from '@codelab/shared/abstract/core'
+import { set } from 'lodash'
+
+/**
+ *
+ * @param action action
+ * @param context global state
+ */
+export const createActionFn = (action: IAnyAction, context: any) => {
+  switch (action.type) {
+    case IActionKind.CustomAction:
+      // eslint-disable-next-line no-new-func
+      return new Function(action.code).bind(context)
+
+    case IActionKind.ResourceAction:
+      return (...args: Array<any>) =>
+        action
+          .run()
+          .then((data) => {
+            set(context, `${action.name}.response`, data)
+
+            if (action.successAction.current) {
+              return getActionRunner(
+                action.successAction.current,
+                context,
+              )(...args)
+            }
+
+            return () => null
+          })
+          .catch((error) => {
+            set(context, `${action.name}.error`, error)
+
+            if (action.errorAction.current) {
+              return getActionRunner(
+                action.errorAction.current,
+                context,
+              )(...args)
+            }
+
+            return () => null
+          })
+
+    case IActionKind.PipelineAction:
+      return (...args: Array<any>) =>
+        action.actionsSorted.map((a) => getActionRunner(a, context)(...args))
+  }
+}
