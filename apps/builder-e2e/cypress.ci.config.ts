f82dee725c9c018e946b03aa9500254c655c7582
Webber Wang <webber@codelab.ai>
2022-08-31 06:29:59 -0700

    chore: upgrade cypress to 10

--- apps/builder-e2e/cypress.ci.config.ts
+++ apps/builder-e2e/cypress.ci.config.ts
@@ -2,14 +2,14 @@ import { nxE2EPreset } from '@nrwl/cypress/plugins/cypress-preset'
 import { defineConfig } from 'cypress'
 import setupNodeEvents from './src/plugins/index'
 
-const cypressJsonConfig = {
+const cypressJsonConfig: Cypress.ConfigOptions = {
   fileServerFolder: '.',
   fixturesFolder: './src/fixtures',
   video: true,
   videosFolder: './src/videos',
   screenshotsFolder: './src/screenshots',
   chromeWebSecurity: false,
-  ignoreTestFiles: ['*.ignore.ts', '*.required.ts'],
+  // ignoreTestFiles: ['*.ignore.ts', '*.required.ts'],
   defaultCommandTimeout: 30000,
   responseTimeout: 60000,
   execTimeout: 60000,
@@ -17,15 +17,15 @@ const cypressJsonConfig = {
   experimentalSessionAndOrigin: false,
   projectId: '86p5h3',
   env: {},
-  testFiles: '**/*.spec.*',
+  // testFiles: '**/*.spec.*',
   numTestsKeptInMemory: 1,
   retries: {
     runMode: 1,
     openMode: 0,
   },
   viewportHeight: 1000,
-  specPattern: 'src/e2e/**/*.cy.{js,jsx,ts,tsx}',
-  supportFile: 'src/support/e2e.ts',
+  // specPattern: 'src/e2e/**/*.cy.{js,jsx,ts,tsx}',
+  // supportFile: 'src/support/e2e.ts',
 }
 
 export default defineConfig({
