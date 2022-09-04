76e0c1b7660fa947e7e260cc2ede73e21465e867
Webber Wang <webber@codelab.ai>
2022-09-04 14:52:30 +0800

    revert: jest to 27

--- jest.config.ts
+++ jest.config.ts
@@ -1,5 +1,10 @@
 const { getJestProjects } = require('@nrwl/jest')
 
+/**
+ * Issue with jest 28 due to ES6 import, ignoring transform doesn't work either.
+ *
+ * Sticking with jest 27
+ */
 module.exports = {
   projects: getJestProjects(),
   // bail: process.env.CI ? true : false,
