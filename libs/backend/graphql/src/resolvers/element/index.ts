4aba7d8dc9d8866435db089df17a617b2692b53e
Webber Wang <webber@codelab.ai>
2022-09-04 12:59:03 +0800

    fix: element resolver

--- libs/backend/graphql/src/resolvers/element/index.ts
+++ libs/backend/graphql/src/resolvers/element/index.ts
@@ -1,10 +1,11 @@
+import { withRxReadTransaction } from '@codelab/backend/adapter/neo4j'
 import { IResolvers } from '@graphql-tools/utils'
 import { elementGraph } from './element.resolver'
 
 export const elementResolver: IResolvers = {
   Mutation: {},
   Query: {
-    elementGraph,
+    elementGraph: withRxReadTransaction(elementGraph),
   },
 }
 
