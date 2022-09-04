2190f9cb847c46f4c309009c4e8d15996b479a09
Webber Wang <webber@codelab.ai>
2022-09-04 15:17:18 +0800

    wip: try out jest 28 with babel resolution removed

--- libs/test/jest.config.ts
+++ libs/test/jest.config.ts
@@ -3,7 +3,7 @@ export default {
   displayName: 'test',
   preset: '../../jest.preset.ts',
   transform: {
-    '^.+\\.[tj]sx?$': 'babel-jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nrwl/react/babel'] }],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../coverage/libs/test',
