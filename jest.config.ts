4bf366e1bd5036f637580ff2fbfa7f75a387c583
Webber Wang <webber@codelab.ai>
2022-09-04 16:14:50 +0800

    test: fix jest babel config

--- jest.config.ts
+++ jest.config.ts
@@ -4,6 +4,14 @@ const { getJestProjects } = require('@nrwl/jest')
  * Issue with jest 28 due to ES6 import, ignoring transform doesn't work either.
  *
  * Sticking with jest 27
+ *
+ * Must use...
+ *
+ * transform: {
+ *     '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nrwl/react/babel'] }],
+ *   },
+ *
+ * `babel-jest` doesn't pickup `.babelrc` for some reason
  */
 module.exports = {
   projects: getJestProjects(),
