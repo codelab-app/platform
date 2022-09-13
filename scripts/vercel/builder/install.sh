9656f91c54c2d0e43bb5ea48efea20b09ec23e04
Webber Wang <webber@codelab.ai>
2022-09-13 21:28:56 +0800

    ci: log folder size in vercel

--- scripts/vercel/builder/install.sh
+++ scripts/vercel/builder/install.sh
@@ -5,4 +5,5 @@
 
 # Set our own custom env variable to skip post-install
 npx cross-env CI=false SKIP_POST_INSTALL=true yarn install --immutable
-rm -rf node_modules/.cache/nx
+du -sh * | sort -h
+#rm -rf node_modules/.cache/nx
