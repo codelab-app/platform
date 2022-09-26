d1a326a18454599e771e9f9ba4f4952efd317473
assim habeek <assimhabe@gmail.com>
2022-09-26 12:35:45 +0100

    revert: adding observer to CodeMirrorInput

--- libs/frontend/view/components/codeMirror/CodeMirrorInput.tsx
+++ libs/frontend/view/components/codeMirror/CodeMirrorInput.tsx
@@ -5,7 +5,6 @@ import { EditorView, ViewUpdate } from '@codemirror/view'
 import { SerializedStyles } from '@emotion/react'
 import { ReactCodeMirrorProps, useCodeMirror } from '@uiw/react-codemirror'
 import { merge } from 'lodash'
-import { observer } from 'mobx-react-lite'
 import React, { useEffect, useRef, useState } from 'react'
 import { CodeMirrorModal, CodeMirrorModalProps } from './CodeMirrorModal'
 import { containerStyles, editorStyles, ExpandButton } from './styles'
@@ -21,86 +20,84 @@ export interface CodeMirrorInputProps
   title?: Nullish<string>
 }
 
-export const CodeMirrorInput = observer(
-  ({
-    value = '',
-    onChange,
-    onSave,
-    expandable,
-    title,
-    overrideStyles,
-    ...props
-  }: CodeMirrorInputProps) => {
-    const editor = useRef<HTMLDivElement | null>(null)
-    const [isExpand, expand] = useState(false)
+export const CodeMirrorInput = ({
+  value = '',
+  onChange,
+  onSave,
+  expandable,
+  title,
+  overrideStyles,
+  ...props
+}: CodeMirrorInputProps) => {
+  const editor = useRef<HTMLDivElement | null>(null)
+  const [isExpand, expand] = useState(false)
 
-    const toggleCompletion = (start: boolean, view: EditorView) =>
-      start ? startCompletion(view) : closeCompletion(view)
+  const toggleCompletion = (start: boolean, view: EditorView) =>
+    start ? startCompletion(view) : closeCompletion(view)
 
-    const onUpdate = (viewUpdate: ViewUpdate) => {
-      if (viewUpdate.focusChanged) {
-        toggleCompletion(viewUpdate.view.hasFocus, viewUpdate.view)
-      }
+  const onUpdate = (viewUpdate: ViewUpdate) => {
+    if (viewUpdate.focusChanged) {
+      toggleCompletion(viewUpdate.view.hasFocus, viewUpdate.view)
     }
+  }
 
-    const setupFactory: CodeMirrorModalProps['setupFactory'] = (
-      editorRef,
-      overWriteOpts?,
-    ) =>
-      merge(
-        {
-          ...props,
-          container: editorRef.current,
-          basicSetup: false,
-          value,
-          onUpdate,
-          onChange: (v: string, view: ViewUpdate) => {
-            onChange(v)
-          },
+  const setupFactory: CodeMirrorModalProps['setupFactory'] = (
+    editorRef,
+    overWriteOpts?,
+  ) =>
+    merge(
+      {
+        ...props,
+        container: editorRef.current,
+        basicSetup: false,
+        value,
+        onUpdate,
+        onChange: (v: string, view: ViewUpdate) => {
+          onChange(v)
         },
-        overWriteOpts,
-      )
-
-    const { setContainer } = useCodeMirror(setupFactory(editor))
-
-    useEffect(() => {
-      if (editor.current) {
-        setContainer(editor.current)
-      }
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [])
-
-    const toggleExpand = () => {
-      expand((curIsExpand) => !curIsExpand)
-    }
-
-    return (
-      <div css={[containerStyles, overrideStyles]}>
-        {/* The Editor */}
-        <div css={editorStyles} ref={editor} />
-
-        {/* The Expanded Editor */}
-        {expandable && (
-          <React.Fragment>
-            <ExpandButton
-              className="CodeMirrorInput--btnExpand"
-              icon={<ExpandAltOutlined width="12px" />}
-              onClick={toggleExpand}
-              size="small"
-              type="primary"
-            />
-            <CodeMirrorModal
-              closeModal={toggleExpand}
-              onChange={onChange}
-              onSave={onSave}
-              setupFactory={setupFactory}
-              title={title}
-              value={value}
-              visible={isExpand}
-            />
-          </React.Fragment>
-        )}
-      </div>
+      },
+      overWriteOpts,
     )
-  },
-)
+
+  const { setContainer } = useCodeMirror(setupFactory(editor))
+
+  useEffect(() => {
+    if (editor.current) {
+      setContainer(editor.current)
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  const toggleExpand = () => {
+    expand((curIsExpand) => !curIsExpand)
+  }
+
+  return (
+    <div css={[containerStyles, overrideStyles]}>
+      {/* The Editor */}
+      <div css={editorStyles} ref={editor} />
+
+      {/* The Expanded Editor */}
+      {expandable && (
+        <React.Fragment>
+          <ExpandButton
+            className="CodeMirrorInput--btnExpand"
+            icon={<ExpandAltOutlined width="12px" />}
+            onClick={toggleExpand}
+            size="small"
+            type="primary"
+          />
+          <CodeMirrorModal
+            closeModal={toggleExpand}
+            onChange={onChange}
+            onSave={onSave}
+            setupFactory={setupFactory}
+            title={title}
+            value={value}
+            visible={isExpand}
+          />
+        </React.Fragment>
+      )}
+    </div>
+  )
+}
