e721e844b14c76d7d0c703b5a07b7b6fe5711279
Mohammed S. Yaseen <moha.98.1900@gmail.com>
2022-09-28 11:08:03 +0300

    feat: add validation rules visualization in fields table

--- libs/frontend/modules/type/src/use-cases/fields/get-fields/FieldsTable.tsx
+++ libs/frontend/modules/type/src/use-cases/fields/get-fields/FieldsTable.tsx
@@ -1,7 +1,11 @@
 import { DeleteFilled, EditFilled } from '@ant-design/icons'
-import { IInterfaceType, ITypeService } from '@codelab/shared/abstract/core'
+import {
+  IInterfaceType,
+  ITypeService,
+  IValidationRules,
+} from '@codelab/shared/abstract/core'
 import { Nullish } from '@codelab/shared/abstract/types'
-import { Button, Space, Table, TableColumnProps } from 'antd'
+import { Button, Divider, Space, Table, TableColumnProps, Tag } from 'antd'
 import { Observer, observer } from 'mobx-react-lite'
 import React from 'react'
 import tw from 'twin.macro'
@@ -13,12 +17,30 @@ export type FieldsTableProps = {
   hideActions?: boolean
 } & { typeService: ITypeService }
 
+interface ValidationRuleTag {
+  key: string
+  value: string | number | boolean
+}
+
+const getValidationRuleTagsArray = (validationRules: IValidationRules) => {
+  const rules: Array<ValidationRuleTag> = []
+
+  Object.entries(validationRules).forEach(([_, ruleCategory]) => {
+    Object.entries(ruleCategory).forEach(([key, value]) => {
+      rules.push({ key, value: value as any })
+    })
+  })
+
+  return rules
+}
+
 interface CellData {
   id: string
   name: Nullish<string>
   description: Nullish<string>
   key: string
   typeKind?: string
+  validationRules?: Array<ValidationRuleTag>
 }
 
 const headerCellProps = () => ({ style: tw`font-semibold text-gray-900` })
@@ -49,6 +71,33 @@ export const FieldsTable = observer<FieldsTableProps>(
         dataIndex: 'typeKind',
         key: 'typeKind',
         onHeaderCell: headerCellProps,
+      },
+      {
+        title: 'Validation Rules',
+        dataIndex: 'ruleName',
+        key: 'ruleName',
+        onHeaderCell: headerCellProps,
+        render: (_, { validationRules }) =>
+          validationRules &&
+          validationRules.map((rule) => {
+            const color = 'geekblue'
+
+            return typeof rule.value === 'boolean' ? (
+              rule.value && (
+                <Tag color={color} key={rule.key}>
+                  <Space>{rule.key}</Space>
+                </Tag>
+              )
+            ) : (
+              <Tag color={color} key={rule.key}>
+                <Space>
+                  {rule.key}
+                  <Divider type="vertical" />
+                  {rule.value}
+                </Space>
+              </Tag>
+            )
+          }),
       },
       {
         title: 'Action',
@@ -105,6 +154,7 @@ export const FieldsTable = observer<FieldsTableProps>(
       key: f.key,
       typeKind: f.type.maybeCurrent ? f.type.maybeCurrent.kind : '',
       description: f.description || '',
+      validationRules: getValidationRuleTagsArray(f.validationRules),
     }))
 
     return (
