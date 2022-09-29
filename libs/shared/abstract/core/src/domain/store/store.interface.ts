db513f6ebc55bef6d2d5b3a7477a18c33280421f
assim habeek <assimhabe@gmail.com>
2022-09-29 14:21:11 +0100

    refactor: remove private members from interface

--- libs/shared/abstract/core/src/domain/store/store.interface.ts
+++ libs/shared/abstract/core/src/domain/store/store.interface.ts
@@ -1,19 +1,15 @@
 import { ICacheService } from '../../service'
 import { IAnyAction } from '../action'
 import { IPropData } from '../prop'
-import { IInterfaceType } from '../type'
 import { IStoreDTO } from './store.dto.interface'
 
 export interface IStore extends ICacheService<IStoreDTO, IStore> {
   id: string
   name: string
   apiId: string
-  _api: IInterfaceType
   actions: Array<IAnyAction>
-  _actionsRunners: IPropData
 
   state: IPropData
-  _state: IPropData
   updateState(initialState: IPropData): void
 }
 
