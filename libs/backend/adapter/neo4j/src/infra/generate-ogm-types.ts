84a7ff53354bbd18c5d6466b9421a9e1fe2f2f5d
Webber Wang <webber@codelab.app>
2022-10-01 06:52:07 -0700

    refactor: add prettier to OGM codegen

--- libs/backend/adapter/neo4j/src/infra/generate-ogm-types.ts
+++ libs/backend/adapter/neo4j/src/infra/generate-ogm-types.ts
@@ -1,6 +1,8 @@
 import { generate } from '@neo4j/graphql-ogm'
 import { ESLint } from 'eslint'
+import * as fs from 'fs'
 import * as path from 'path'
+import * as prettier from 'prettier'
 import { getOgm } from './ogm'
 
 export const generateOgmTypes = async () => {
@@ -10,20 +12,37 @@ export const generateOgmTypes = async () => {
     'src/ogm-types.gen.ts',
   )
 
-  return await generate({
+  const output = await generate({
     ogm: await getOgm(),
     outFile,
+    noWrite: true,
   })
-    .then(() => console.info('OGM type generated!'))
+    .then((data) => {
+      console.info('OGM type generated!')
+
+      return data
+    })
     .catch((e) =>
       console.error(`[generateOgmTypes] ${JSON.stringify(e, null, 2)}`),
     )
 
+  // Get prettier config
+  const options = await prettier.resolveConfig(outFile)
+
+  // Format
+  const formatted = prettier.format(`${output}`, {
+    ...options,
+    filepath: outFile,
+  })
+
   /**
    * Save to abstract folder as well for exporting just the interfaces
    */
-  // fs.writeFileSync(outFile, JSON.stringify(data))
+  fs.writeFileSync(outFile, formatted)
 
+  /**
+   * Don't use ESLint since it's much slower
+   */
   // const results = await getEslint().lintFiles(outFile)
   // await ESLint.outputFixes(results)
 
