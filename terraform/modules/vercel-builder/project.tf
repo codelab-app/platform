ddd59425b7d6ae0aedef52edeecf413d1f07a17e
Webber Wang <webber@codelab.app>
2022-10-21 05:00:48 -0700

    wip: add vercel env to project
    wip: add vercel env to project
    
    wip: add vercel env to project

--- terraform/modules/vercel-builder/project.tf
+++ terraform/modules/vercel-builder/project.tf
@@ -28,37 +28,37 @@ resource "vercel_project" "builder" {
 
   environment = [
     {
-      target = ["production"]
+      target = ["production", "preview"]
       key = "NEXT_PUBLIC_BUILDER_URL"
       value = var.NEXT_PUBLIC_BUILDER_URL
     },
     # Auth0
     {
-      target = ["production"]
+      target = ["production", "preview"]
       key = "AUTH0_AUDIENCE"
       value = var.AUTH0_AUDIENCE
     },
     {
-      target = ["production"]
+      target = ["production", "preview"]
       key = "AUTH0_ISSUER_BASE_URL"
       value = var.AUTH0_ISSUER_BASE_URL
     },
     {
-      target = ["production"]
+      target = ["production", "preview"]
       key = "AUTH0_SECRET"
       # This isn't working
       #      value = data.auth0_client.web_client.client_secret
       value = var.AUTH0_SECRET
     },
     {
-      target = ["production"]
+      target = ["production", "preview"]
       key = "AUTH0_CLIENT_SECRET"
       # This isn't working
 #      value = data.auth0_client.web_client.client_secret
       value = var.auth0_client_secret
     },
     {
-      target = ["production"]
+      target = ["production", "preview"]
       key = "AUTH0_CLIENT_ID"
       # This isn't working
 #      value = data.auth0_client.web_client.id
@@ -66,28 +66,28 @@ resource "vercel_project" "builder" {
     },
     # Neo4j
     {
-      target = ["production"]
+      target = ["production", "preview"]
       key = "NEO4J_USER"
       value = var.NEO4J_USER
     },
     {
-      target = ["production"]
+      target = ["production", "preview"]
       key = "NEO4J_URI"
       value = var.NEO4J_URI
     },
     {
-      target = ["production"]
+      target = ["production", "preview"]
       key = "NEO4J_PASSWORD"
       value = var.NEO4J_PASSWORD
     },
     # Vercel
     {
-      target = ["production"]
+      target = ["production", "preview"]
       key = "VERCEL_API_TOKEN"
       value = var.VERCEL_API_TOKEN
     },
     {
-      target = ["production"]
+      target = ["production", "preview"]
       key = "VERCEL_PROJECT_ID"
       # Cannot have self-referencing ID
       # https://github.com/hashicorp/terraform/issues/3267
@@ -96,7 +96,7 @@ resource "vercel_project" "builder" {
       value = var.VERCEL_BUILDER_PROJECT_ID
     },
     {
-      target = ["production"]
+      target = ["production", "preview"]
       key = "VERCEL_TEAM_ID"
       value = var.VERCEL_TEAM_ID
     },
