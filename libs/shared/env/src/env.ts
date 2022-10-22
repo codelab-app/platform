95b5746b0da353566ef23a341ddbe04877b16461
Webber Wang <webber@codelab.app>
2022-10-21 19:55:49 -0700

    build: add ci back to vercel step
    build: add ci back to vercel step
    
    build: add ci back to vercel step

--- libs/shared/env/src/env.ts
+++ libs/shared/env/src/env.ts
@@ -2,4 +2,8 @@ export const isServer = typeof window === 'undefined'
 
 export const isProduction = process.env['NODE_ENV'] === 'production'
 
-export const isCi = process.env['CI'] === 'true'
+export const isCi =
+  // Vercel uses '1'
+  process.env['CI'] === '1' ||
+  // Others may use 'true'
+  process.env['CI'] === 'true'
