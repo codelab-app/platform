e25b5b5cc8390cd1fd65341ee9026c64509f6e2e
Webber Wang <webber@codelab.app>
2022-10-25 13:16:10 +0800

    wip: upgrade auth0

--- apps/builder/pages/api/auth/[...auth0].ts
+++ apps/builder/pages/api/auth/[...auth0].ts
@@ -1,28 +1,26 @@
-import { handleLogin } from '@auth0/nextjs-auth0'
 import { auth0Instance } from '@codelab/shared/adapter/auth0'
-import { Config } from '@codelab/shared/config'
 
 export default auth0Instance.handleAuth({
   // https://github.com/auth0/nextjs-auth0/issues/383
   // https://github.com/auth0/nextjs-auth0/blob/main/examples/README.md
-  login: async (req, res) => {
-    console.log(Config().auth0.baseUrl)
-
-    return auth0Instance.handleLogin(req, res, {
-      returnTo: '/apps',
-    })
-  },
-  logout: async (req, res) => {
-    return auth0Instance.handleLogin(req, res, {
-      returnTo: '/',
-    })
-  },
-  callback: async (req, res) => {
-    return auth0Instance.handleCallback(req, res, {
-      redirectUri: '/apps',
-    })
-  },
-  profile: async (req, res) => {
-    return auth0Instance.handleProfile(req, res, {})
-  },
+  // login: async (req, res) => {
+  //   console.log(Config().auth0.baseUrl)
+  //
+  //   return auth0Instance.handleLogin(req, res, {
+  //     returnTo: '/apps',
+  //   })
+  // },
+  // logout: async (req, res) => {
+  //   return auth0Instance.handleLogin(req, res, {
+  //     returnTo: '/',
+  //   })
+  // },
+  // callback: async (req, res) => {
+  //   return auth0Instance.handleCallback(req, res, {
+  //     redirectUri: '/apps',
+  //   })
+  // },
+  // profile: async (req, res) => {
+  //   return auth0Instance.handleProfile(req, res, {})
+  // },
 })
