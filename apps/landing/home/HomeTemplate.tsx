5597f52209e45ab70fd081b56f220f2a59f8c3a6
Tony Phan <tinh.w3@gmail.com>
2022-12-23 09:44:46 +0700

    fix: use dynamic import solution instead of using hook

--- apps/landing/home/HomeTemplate.tsx
+++ apps/landing/home/HomeTemplate.tsx
@@ -1,6 +1,6 @@
 import { useMobileOrTabletMediaQuery } from '@codelab/frontend/shared/style'
 import type { PropsWithChildren } from 'react'
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { useRecoilValue } from 'recoil'
 import tw from 'twin.macro'
 import { Footer } from '../sections/footer/Footer'
@@ -29,18 +29,14 @@ const Content = ({ children }: PropsWithChildren) => {
   return <section>{children}</section>
 }
 
-export const HomeTemplate = ({ children }: HomeTemplateProps) => {
+const HomeTemplate = ({ children }: HomeTemplateProps) => {
   const isMobileOrTablet = useMobileOrTabletMediaQuery()
-  // While rendering your application, there was a difference between the React tree that was pre-rendered (SSR/SSG) and the React tree that rendered during the first render in the Browser.
-  // The first render is called Hydration which is a feature of React.
-  // This can cause the React tree to be out of sync with the DOM and result in unexpected content/attributes being present.
-  // Possible Ways to Fix It - https://github.com/vercel/next.js/discussions/35773#discussioncomment-2485078
-  const [hasMounted, setHasMounted] = useState(false)
-  useEffect(() => {
-    setHasMounted(true)
-  }, [])
 
-  return hasMounted ? (
+  if (typeof window === 'undefined') {
+    return <></>
+  }
+
+  return (
     <Layout>
       <Header>
         <CodelabMenuContainer>
@@ -50,7 +46,7 @@ export const HomeTemplate = ({ children }: HomeTemplateProps) => {
       <Content>{children}</Content>
       <Footer></Footer>
     </Layout>
-  ) : null
+  )
 }
 
 // const LayoutTest = ({ children }: PropsWithChildren<any>) => {
@@ -74,3 +70,5 @@ export const HomeTemplate = ({ children }: HomeTemplateProps) => {
 //     <Footer></Footer>
 //   </LayoutTest>
 // )
+
+export default HomeTemplate
