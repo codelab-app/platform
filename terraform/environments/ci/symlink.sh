a68242b1294e538926f0b262399f2d1ad9eaee36
Webber Wang <webber@codelab.app>
2023-01-07 14:17:32 +0800

    ci: add env to ci for supabase

--- terraform/environments/ci/symlink.sh
+++ terraform/environments/ci/symlink.sh
@@ -6,3 +6,4 @@ rm ./*.symlink.tf
 
 ln -s ../shared/module-auth0.tf ./module-auth0.symlink.tf
 ln -s ../shared/vars-auth0.tf ./vars-auth0.symlink.tf
+ln -s ../shared/vars-supabase.tf ./vars-supabase.symlink.tf
