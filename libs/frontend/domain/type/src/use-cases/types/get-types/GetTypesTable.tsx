b3d1fac2d0caee4c0fbfe51f5b048002efdbf5cb
Mohammed S. Yaseen <moha.98.1900@gmail.com>
2023-01-09 09:55:00 +0300

    feat: remove the support for navigating to selected type

--- libs/frontend/domain/type/src/use-cases/types/get-types/GetTypesTable.tsx
+++ libs/frontend/domain/type/src/use-cases/types/get-types/GetTypesTable.tsx
@@ -3,24 +3,18 @@ import type {
   IFieldService,
   ITypeService,
 } from '@codelab/frontend/abstract/core'
-import { PageType } from '@codelab/frontend/abstract/types'
 import { Spin, Table } from 'antd'
 import { observer } from 'mobx-react-lite'
-import { useRouter } from 'next/router'
 import React, { useCallback, useEffect, useState } from 'react'
-import scrollIntoView from 'scroll-into-view'
 import { TypeDetailsTable } from './tables/TypeDetailsTable'
 import { useTypesTable } from './useTypesTable'
 import { useTypesTableData } from './useTypesTableData'
 
-const SCROLL_ROW_CLASS_NAME = 'scroll-row'
-
 export const GetTypesTable = observer<{
-  typeId?: string
   typeService: ITypeService
   fieldService: IFieldService
-}>(({ typeId, typeService, fieldService }) => {
-  const { types, typesList } = typeService
+}>(({ typeService, fieldService }) => {
+  const { typesList } = typeService
 
   const {
     isLoadingAllTypes,
@@ -31,26 +25,12 @@ export const GetTypesTable = observer<{
 
   const [curPage, setCurPage] = useState(1)
   const [curPageSize, setCurPageSize] = useState(25)
-  const [rowClassReady, setRowClassReady] = useState(false)
-  const router = useRouter()
 
   const { columns, rowSelection, pagination } = useTypesTable({
     typeService,
     isLoadingTypeDependencies: isLoadingAllTypes,
     fieldService,
   })
-
-  const findPageOfCurrentType = () => {
-    const currentType = types.get(typeId ?? '')
-
-    if (!currentType) {
-      return
-    }
-
-    return Math.ceil(
-      (typesList.findIndex((t) => t.id === currentType.id) + 1) / curPageSize,
-    )
-  }
 
   const handlePageChange = useCallback(
     (page: number, pageSize: number) => {
@@ -62,46 +42,12 @@ export const GetTypesTable = observer<{
   )
 
   useEffect(() => {
-    if (!typesList.length && typeId) {
-      return
-    }
-
-    let offset = (curPage - 1) * curPageSize
-
-    if (typeId) {
-      const page = findPageOfCurrentType()
-
-      if (page) {
-        handlePageChange(page, curPageSize)
-        offset = (page - 1) * curPageSize
-
-        /**
-         * Removing the current type id from the url because there is no use for it anymore
-         */
-        router.push(PageType.Type).catch((e) => console.error(e))
-      }
-    }
-
+    const offset = (curPage - 1) * curPageSize
     void getBaseTypes({
       offset,
       limit: curPageSize,
     })
   }, [curPage, curPageSize, getBaseTypes])
-
-  /**
-   * Scroll to the current type to make sure it is visible
-   */
-  useEffect(() => {
-    const scrollRow = document.querySelector(`.${SCROLL_ROW_CLASS_NAME}`)
-
-    if (scrollRow) {
-      scrollIntoView(scrollRow as HTMLElement, {
-        align: {
-          top: 0,
-        },
-      })
-    }
-  }, [rowClassReady])
 
   return (
     <Table<IAnyType>
@@ -113,7 +59,6 @@ export const GetTypesTable = observer<{
             await getTypeDescendants(record.id)
           }
         },
-        defaultExpandedRowKeys: [typeId ?? ''],
         expandedRowRender: (type) =>
           isLoadingAllTypes || isLoadingTypeDescendants ? (
             <Spin />
@@ -131,15 +76,6 @@ export const GetTypesTable = observer<{
         current: curPage,
         pageSize: curPageSize,
         onChange: handlePageChange,
-      }}
-      rowClassName={(record) => {
-        if (record.id === typeId) {
-          setRowClassReady(true)
-
-          return SCROLL_ROW_CLASS_NAME
-        }
-
-        return ''
       }}
       rowKey={(type) => type.id}
       rowSelection={rowSelection}
