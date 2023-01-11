dba440f64754b6aacf5fd58092bc60c15c8f54fb
Webber Wang <webber@codelab.app>
2023-01-11 15:41:19 +0800

    wip: modifying layout

--- libs/frontend/view/templates/SidebarNavigation/SidebarNavigation.tsx
+++ libs/frontend/view/templates/SidebarNavigation/SidebarNavigation.tsx
@@ -5,7 +5,9 @@ import React from 'react'
 import tw from 'twin.macro'
 import { sidebarWidth } from '../Dashboard'
 
-interface SidebarNavigationProps {
+export interface SidebarNavigationProps {
+  // Home icon
+  homeItem: MenuProps['items']
   // Default menu items
   primaryItems?: MenuProps['items']
   // Menu items at the bottom
@@ -30,30 +32,29 @@ export const SidebarNavigation = ({
 
   return (
     <div
-      css={tw`flex flex-col justify-between h-full border-solid border-gray-200 border-r box-border`}
+      css={tw`flex flex-col justify-between h-full border-gray-200 border-r box-border`}
       // Set the width on the outermost container
       style={{
         width: `${sidebarWidth}px`,
       }}
     >
-      <div css={tw`box-border`}>
-        <Menu
-          css={tw`border-none box-border`}
-          defaultOpenKeys={[]}
-          // defaultSelectedKeys={[PageType.AppList]}
-          items={primaryItems}
-          mode="inline"
-          selectedKeys={transformSelectedKeys([router.pathname])}
-          style={
-            {
-              // width: `${sidebarWidth}px`,
-              // height: fullHeight ? '100%' : undefined,
-              // maxHeight: '100vh',
-            }
-          }
-        />
-      </div>
       <Menu
+        css={tw`border-none box-border`}
+        defaultOpenKeys={[]}
+        // defaultSelectedKeys={[PageType.AppList]}
+        items={primaryItems}
+        mode="inline"
+        selectedKeys={transformSelectedKeys([router.pathname])}
+        style={
+          {
+            // width: `${sidebarWidth}px`,
+            // height: fullHeight ? '100%' : undefined,
+            // maxHeight: '100vh',
+          }
+        }
+      />
+      <Menu
+        css={tw`border-none box-border`}
         defaultOpenKeys={[]}
         // defaultSelectedKeys={[PageType.AppList]}
         items={secondaryItems}
