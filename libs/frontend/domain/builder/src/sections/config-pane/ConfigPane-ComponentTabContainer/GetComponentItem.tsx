429c6ddf494e99122dd67fb896da20894d598e53
jethrocabaluna <jethrocabaluna310@gmail.com>
2023-01-19 00:48:48 +0800

    fix: default prop values when creating an atom

--- libs/frontend/domain/builder/src/sections/config-pane/ConfigPane-ComponentTabContainer/GetComponentItem.tsx
+++ libs/frontend/domain/builder/src/sections/config-pane/ConfigPane-ComponentTabContainer/GetComponentItem.tsx
@@ -1,8 +1,15 @@
-import type { IBuilderComponent } from '@codelab/frontend/abstract/core'
+import type {
+  IBuilderComponent,
+  IFieldDefaultValue,
+  IInterfaceType,
+} from '@codelab/frontend/abstract/core'
 import { BuilderDndType } from '@codelab/frontend/abstract/core'
+import { useStore } from '@codelab/frontend/presenter/container'
 import { createSlug } from '@codelab/frontend/shared/utils'
+import type { Maybe, Nullish } from '@codelab/shared/abstract/types'
 import { antDesignIconPrefix } from '@codelab/shared/data'
 import { Card } from 'antd'
+import { isNil } from 'ramda'
 import React, { useMemo } from 'react'
 import tw from 'twin.macro'
 import { useCreateElementDraggable } from '../../../dnd/useCreateElementDraggable'
@@ -11,14 +18,41 @@ interface DraggableGetComponentItemProps {
   component: IBuilderComponent
 }
 
+/**
+ * generates a JSON containing api fields that has a default value
+ * that will be saved as props for the new element created
+ */
+const makeDefaultProps = (typeApi: Maybe<IInterfaceType>) => {
+  const fields = typeApi?.fields ?? []
+
+  const defaultProps = fields.reduce<
+    Record<string, Nullish<IFieldDefaultValue>>
+  >((acc, field) => {
+    if (!isNil(field.defaultValues)) {
+      acc[field.key] = field.defaultValues
+    }
+
+    return acc
+  }, {})
+
+  return JSON.stringify(defaultProps)
+}
+
 export const DraggableGetComponentItem = ({
   component,
 }: DraggableGetComponentItemProps) => {
+  const { typeService } = useStore()
+
   const createElementInput = useMemo(() => {
+    // Atoms are interface type
+    // by the time this is called, it should be already available because of the query GetRenderedPageAndCommonAppData
+    const typeApi = typeService.type(component.api.id) as Maybe<IInterfaceType>
+
     return {
       name: component.name,
       atomId: component.id,
       slug: createSlug(component.name),
+      propsData: makeDefaultProps(typeApi),
     }
   }, [component])
 
