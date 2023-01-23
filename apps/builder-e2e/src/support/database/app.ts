d2e2f5823cbb97635e8d4ee9ba32fa03faa4c651
assim habeek <assimhabe@gmail.com>
2023-01-23 12:59:20 +0100

    fix: builder e2e specs

--- apps/builder-e2e/src/support/database/app.ts
+++ apps/builder-e2e/src/support/database/app.ts
@@ -1,4 +1,11 @@
 import type { IAppDTO } from '@codelab/frontend/abstract/core'
+import {
+  APP_PAGE_NAME,
+  APP_PAGE_SLUG,
+  DEFAULT_GET_SERVER_SIDE_PROPS,
+  ROOT_ELEMENT_NAME,
+} from '@codelab/frontend/abstract/core'
+import { createSlug } from '@codelab/frontend/shared/utils'
 import type { AppCreateInput } from '@codelab/shared/abstract/codegen'
 import { ITypeKind } from '@codelab/shared/abstract/core'
 import { connectOwner } from '@codelab/shared/data'
@@ -8,12 +15,40 @@ import { v4 } from 'uuid'
 
 export const createAppInput = (userId: string): AppCreateInput => {
   const appId = v4()
+  const pageId = v4()
+  const rootId = v4()
+
+  const providerPage = {
+    id: pageId,
+    name: APP_PAGE_NAME,
+    slug: `${appId}-${APP_PAGE_SLUG}`,
+    getServerSideProps: DEFAULT_GET_SERVER_SIDE_PROPS,
+    app: {
+      connect: { where: { node: { id: appId } } },
+    },
+    rootElement: {
+      create: {
+        node: {
+          id: rootId,
+          name: ROOT_ELEMENT_NAME,
+          slug: createSlug(ROOT_ELEMENT_NAME, pageId),
+        },
+      },
+    },
+    isProvider: true,
+    pageContainerElement: {
+      connect: { where: { node: { id: rootId } } },
+    },
+  }
 
   return {
     id: appId,
     name: `Test app ${appId}`,
     slug: `test-${appId}`,
     owner: connectOwner(userId),
+    pages: {
+      create: [{ node: providerPage }],
+    },
     store: {
       create: {
         node: {
