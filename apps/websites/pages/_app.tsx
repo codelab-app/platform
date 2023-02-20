f03ad2c48888e7bc8987f55e2c09d4d07c5b14a1
Vladyslav Polishchuk <gm.vpolishchuk@gmail.com>
2023-02-20 20:43:15 +0100

    refactor: cleanup not needed code for websites project

--- apps/websites/pages/_app.tsx
+++ apps/websites/pages/_app.tsx
@@ -4,53 +4,25 @@ import 'react-quill/dist/quill.snow.css'
 // This stylesheet is used to override some of the default Quill editor's styles.
 import '../src/styles/quill.snow.override.css'
 import 'react-grid-layout/css/styles.css'
-import 'react-resizable/css/styles.css'
 import type { IAppProps, IPageProps } from '@codelab/frontend/abstract/core'
-import type { CodelabPage } from '@codelab/frontend/abstract/types'
 import { initializeStore } from '@codelab/frontend/model/infra/mobx'
 import { StoreProvider } from '@codelab/frontend/presenter/container'
-import { css, Global } from '@emotion/react'
-import { LocalizationProvider } from '@mui/x-date-pickers'
-import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns'
-import { ConfigProvider } from 'antd'
-import type { PropsWithChildren } from 'react'
 import React, { useMemo } from 'react'
 import { RecoilRoot } from 'recoil'
 import { GlobalStyles } from 'twin.macro'
-import { globalTailwindFix } from '../src/styles/GlobalTailwindFix'
-import { slickCssFix } from '../src/styles/slick/Slick'
 
 const App = ({ pageProps, Component }: IAppProps<IPageProps>) => {
   const store = useMemo(() => initializeStore(pageProps), [])
 
-  const { Layout = ({ children }: PropsWithChildren) => <>{children}</> } =
-    Component as CodelabPage<unknown>
-
   return (
     <StoreProvider value={store}>
       <RecoilRoot>
-        <LocalizationProvider dateAdapter={AdapterDateFns}>
-          <ConfigProvider>
-            <GlobalStyles />
-            <Global
-              styles={[
-                css({
-                  '#__next': {
-                    height: '100%',
-                  },
-                }),
-                slickCssFix,
-                ...globalTailwindFix,
-              ]}
-            />
-            <Layout>
-              <Component
-                // eslint-disable-next-line react/jsx-props-no-spreading
-                {...pageProps}
-              />
-            </Layout>
-          </ConfigProvider>
-        </LocalizationProvider>
+        <GlobalStyles />
+
+        <Component
+          // eslint-disable-next-line react/jsx-props-no-spreading
+          {...pageProps}
+        />
       </RecoilRoot>
     </StoreProvider>
   )
