92f2bb0e99b61e74cfe17f391ef5b19f2a183025
jethrocabaluna <jethrocabaluna310@gmail.com>
2023-02-28 19:59:04 +0800

    refactor: computed name based on render type

--- libs/frontend/domain/element/src/components/auto-computed-element-name/AutoComputedElementNameField.tsx
+++ libs/frontend/domain/element/src/components/auto-computed-element-name/AutoComputedElementNameField.tsx
@@ -1,40 +1,81 @@
-import type { IComputeElementNameService } from '@codelab/frontend/abstract/core'
+import type { RenderType } from '@codelab/frontend/abstract/core'
+import { RenderTypeEnum } from '@codelab/frontend/abstract/core'
+import { useStore } from '@codelab/frontend/presenter/container'
+import type { Maybe } from '@codelab/shared/abstract/types'
+import { compoundCaseToTitleCase } from '@codelab/shared/utils'
 import type { InputProps } from 'antd'
 import { observer } from 'mobx-react-lite'
-import React, { useEffect } from 'react'
+import React, { useEffect, useRef } from 'react'
 import type { FieldProps } from 'uniforms'
-import { connectField } from 'uniforms'
+import { connectField, useField } from 'uniforms'
 import { TextField } from 'uniforms-antd'
+import { makeAutoIncrementedName } from '../../utils'
 
 type AutoComputedElementNameProps = FieldProps<
   string,
   Omit<InputProps, 'onReset'>
 > & {
   onChange: (value: string) => void
-  computeElementNameService: IComputeElementNameService
 }
 
 /**
- * This component helps providing the computeElementNameService with
- * the user input as well as connect the parent form with the final
- * result from the computeElementNameService.
+ * This component helps providing a computed name for an element
+ * based on the renderType selected and the user input
  */
 const AutoComputedElementName = observer<AutoComputedElementNameProps>(
   (props) => {
-    const { name, onChange, computeElementNameService, value } = props
+    const { name, onChange, value } = props
 
-    useEffect(() => {
-      computeElementNameService.setPickedName(value)
-    }, [value])
+    const [renderTypeField] = useField<{ value?: Partial<RenderType> }>(
+      'renderType',
+      {},
+    )
+
+    const { atomService, componentService, builderService } = useStore()
+    // Used to check if the previous selected atom/component name
+    // is different from the current value to determine if the user
+    // altered the auto-generated name
+    const currentRenderTypeName = useRef<string>()
+
+    const changedRenderTypeHandler = async (
+      renderType?: Partial<RenderType>,
+    ) => {
+      let renderTypeName: Maybe<string>
+
+      if (!renderType || !renderType.id) {
+        return
+      }
+
+      if (renderType.model === RenderTypeEnum.Atom) {
+        renderTypeName = (await atomService.getOne(renderType.id))?.name
+      }
+
+      if (renderType.model === RenderTypeEnum.Component) {
+        renderTypeName = (await componentService.getOne(renderType.id))?.name
+      }
+
+      renderTypeName = renderTypeName
+        ? makeAutoIncrementedName(
+            builderService.activeElementTree?.elementsList.map(
+              (element) => element.name,
+            ) || [],
+            compoundCaseToTitleCase(renderTypeName),
+          )
+        : undefined
+
+      if (!value || value === currentRenderTypeName.current) {
+        onChange(renderTypeName ?? '')
+      }
+
+      currentRenderTypeName.current = renderTypeName
+    }
 
     useEffect(() => {
       // When renderType changes, we need to programatically
       // change the name field based on the selected renderTypeName
       // but only if user did not changed the name
-      if (!computeElementNameService.hasCustomName) {
-        onChange(computeElementNameService.pickedName)
-      }
-    }, [computeElementNameService.pickedRenderTypeName])
+      void changedRenderTypeHandler(renderTypeField.value)
+    }, [renderTypeField.value])
 
     return <TextField name={name} />
   },
