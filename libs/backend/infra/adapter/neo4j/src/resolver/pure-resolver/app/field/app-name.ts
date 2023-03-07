6c728bac48df5d014a807d205a144ea761b66688
Webber Wang <webber@codelab.app>
2023-03-07 14:15:32 -0800

    fix: _compoundName for app and page

--- libs/backend/infra/adapter/neo4j/src/resolver/pure-resolver/app/field/app-name.ts
+++ libs/backend/infra/adapter/neo4j/src/resolver/pure-resolver/app/field/app-name.ts
@@ -2,13 +2,10 @@ import type { App } from '@codelab/shared/abstract/codegen'
 import type { IFieldResolver } from '@graphql-tools/utils'
 
 /**
- * `_compoundName` contains format `appId-name`, which allows page name to be unique across users.
+ * `_compoundName` contains format `userId-name`, which allows page name to be unique across users.
  *
  * We can compute name by replacing the ID
  */
-export const name: IFieldResolver<
-  Pick<App, 'id' | '_compoundName'>,
-  unknown
-> = (props) => {
-  return props._compoundName.replace(`${props.id}-`, '')
+export const name: IFieldResolver<App, unknown> = (app) => {
+  return app._compoundName.replace(`${app.owner.auth0Id}-`, '')
 }
