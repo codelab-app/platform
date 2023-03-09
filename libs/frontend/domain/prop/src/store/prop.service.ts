2d77e6865087506327d0771328e8f12e0651903c
Webber Wang <webber@codelab.app>
2023-03-08 20:34:35 -0800

    refactor: remove patchElement with propService update

--- libs/frontend/domain/prop/src/store/prop.service.ts
+++ libs/frontend/domain/prop/src/store/prop.service.ts
@@ -1,20 +1,31 @@
-import type { IProp, IPropService } from '@codelab/frontend/abstract/core'
+import type {
+  ICreatePropData,
+  IProp,
+  IPropService,
+  IUpdatePropData,
+} from '@codelab/frontend/abstract/core'
 import { IPropDTO } from '@codelab/frontend/abstract/core'
 import {
+  _async,
+  _await,
   createContext,
   idProp,
   Model,
   model,
   modelAction,
+  modelFlow,
   objectMap,
   prop,
+  transaction,
 } from 'mobx-keystone'
 import { Prop } from './prop.model'
+import { PropRepository } from './prop.repository'
 
 @model('@codelab/PropService')
 export class PropService
   extends Model({
     id: idProp,
+    propRepository: prop(() => new PropRepository({})),
     props: prop(() => objectMap<IProp>()),
   })
   implements IPropService
@@ -25,12 +36,47 @@ export class PropService
 
   @modelAction
   add({ id, data }: IPropDTO) {
-    const propModel = Prop.create({ data, id })
+    const props = Prop.create({ data, id })
 
-    this.props.set(propModel.id, propModel)
+    this.props.set(props.id, props)
 
-    return propModel
+    return props
   }
+
+  @modelFlow
+  @transaction
+  create = _async(function* (
+    this: PropService,
+    { id, data, api }: ICreatePropData,
+  ) {
+    const props = Prop.create({ api, data, id })
+
+    yield* _await(this.propRepository.add(props))
+
+    return props
+  })
+
+  @modelFlow
+  @transaction
+  delete = _async(function* (this: PropService, props: IProp) {
+    this.props.delete(props.id)
+
+    yield* _await(this.propRepository.delete([props]))
+
+    return props
+  })
+
+  @modelFlow
+  @transaction
+  update = _async(function* (this: PropService, { id, data }: IUpdatePropData) {
+    const props = this.props.get(id)!
+
+    props.writeCache({ data })
+
+    yield* _await(this.propRepository.update(props))
+
+    return props
+  })
 }
 
 export const propServiceContext = createContext<IPropService>()
