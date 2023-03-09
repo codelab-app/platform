4d23ca31ae8fe1b895cc2a0d55fcb69b607d48a6
assim habeek <assimhabe@gmail.com>
2023-03-09 11:54:00 +0100

    revert: restore action server interface

--- libs/frontend/abstract/core/src/domain/action/action.service.interface.ts
+++ libs/frontend/abstract/core/src/domain/action/action.service.interface.ts
@@ -1,10 +1,36 @@
-import type { IRepository } from '../../service'
+import type {
+  ActionFragment,
+  ApiActionOptions,
+} from '@codelab/shared/abstract/codegen'
+import type { Maybe } from '@codelab/shared/abstract/types'
+import type { Ref } from 'mobx-keystone'
+import type {
+  ICRUDModalService,
+  ICRUDService,
+  IQueryService,
+} from '../../service'
+import type {
+  IActionDTO,
+  IBaseActionDTO,
+  ICreateActionData,
+  IUpdateActionData,
+} from './action.dto.interface'
 import type { IAction } from './action.interface'
 import type { IActionWhere } from './action.where.interface'
-import type { ActionFragment } from './fragments'
 
-export type IStoreRepository = IRepository<
-  IAction,
-  ActionFragment,
-  IActionWhere
->
+export interface IActionFactory {
+  fromActionFragment(fragment: ActionFragment): IActionDTO
+}
+
+export interface IActionService
+  extends ICRUDService<IAction, ICreateActionData, IUpdateActionData>,
+    Omit<IQueryService<IAction, IActionWhere, ApiActionOptions>, 'getAll'>,
+    ICRUDModalService<Ref<IAction>, { action: Maybe<IAction> }> {
+  actionsList: Array<IAction>
+  action(id: string): Maybe<IAction>
+  add(action: IBaseActionDTO): IAction
+  setSelectedActions(actions: Array<Ref<IAction>>): void
+  // Replace due to union interface neo4j issue
+  getAll(storeId?: string): Promise<Array<IAction>>
+  actionFactory: IActionFactory
+}
