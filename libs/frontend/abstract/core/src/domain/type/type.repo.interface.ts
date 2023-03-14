654cca6138f93c6cc91322adf3b4f4723ac1bdcf
Vladyslav Polishchuk <74900868+vladyslav-polishchuk@users.noreply.github.com>
2023-03-14 21:04:50 +0100

    fix: projects build (#2367)

--- libs/frontend/abstract/core/src/domain/type/type.repo.interface.ts
+++ libs/frontend/abstract/core/src/domain/type/type.repo.interface.ts
@@ -3,23 +3,19 @@ import type {
   TypeFragment,
 } from '@codelab/shared/abstract/codegen'
 import type { IRepository } from '../../service'
+import type { BaseType_BaseType_Fragment } from './fragments/base-type.fragment.graphql.gen'
 import type { ITypeDTO } from './type.dto.interface'
 import type { IType } from './types'
-import type { BaseType_BaseType_Fragment } from './fragments/base-type.fragment.graphql.gen'
 
 export interface BaseTypesOptions {
-  offset?: number
   limit?: number
+  offset?: number
   where?: {
     name: string
   }
 }
 
-export type ITypeRepository = IRepository<
-  IType,
-  ITypeDTO,
-  BaseTypeWhere
-> & {
+export type ITypeRepository = IRepository<IType, ITypeDTO, BaseTypeWhere> & {
   findDescendants(parentIds: Array<string>): Promise<Array<TypeFragment>>
   findBaseTypes(options: BaseTypesOptions): Promise<{
     items: Array<BaseType_BaseType_Fragment>
