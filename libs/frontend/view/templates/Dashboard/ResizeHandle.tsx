d9d370df0ae0cc424d4d18b01bca00d8bb73795b
JLL32 <eabduljalil@outlook.com>
2023-03-22 18:18:06 +0000

    fix: remove resize handler width on hover

--- libs/frontend/view/templates/Dashboard/ResizeHandle.tsx
+++ libs/frontend/view/templates/Dashboard/ResizeHandle.tsx
@@ -5,7 +5,7 @@ import tw from 'twin.macro'
 const ResizeHandle = () => {
   return (
     <PanelResizeHandle
-      css={tw`w-[3px] hover:w-4 hover:bg-gray-300 active:w-4 active:bg-gray-300  h-full bg-gray-200`}
+      css={tw`w-1 hover:bg-blue-400 active:bg-blue-400 h-full`}
     />
   )
 }
