f485dfd23f6f2b3bdac19bc600bf5c0a1356684f
Mohammed S. Yaseen <moha.98.1900@gmail.com>
2023-03-28 14:26:16 +0300

    fix: create tag modal flashes incorrect validation error

--- libs/frontend/domain/tag/src/use-cases/create-tag/CreateTagModal.tsx
+++ libs/frontend/domain/tag/src/use-cases/create-tag/CreateTagModal.tsx
@@ -2,29 +2,37 @@ import type { ICreateTagData } from '@codelab/frontend/abstract/core'
 import { useStore } from '@codelab/frontend/presenter/container'
 import { createNotificationHandler } from '@codelab/frontend/shared/utils'
 import { ModalForm } from '@codelab/frontend/view/components'
+import type { LabeledValue } from 'antd/es/select'
 import { observer } from 'mobx-react-lite'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { AutoFields, SelectField } from 'uniforms-antd'
 import { v4 } from 'uuid'
 import { createTagSchema } from './create.tag.schema'
 
 export const CreateTagModal = observer(() => {
   const { tagService, userService } = useStore()
+  const [options, setOptions] = useState<Array<LabeledValue>>([])
+  const isOpen = tagService.createModal.isOpen
 
   const onSubmit = (input: ICreateTagData) => {
     return tagService.create(input)
   }
 
-  const options = tagService.tagsSelectOptions
+  /**
+   * Get the options only when modal is opened (or closed)
+   * otherwise of tagService.tagsSelectOptions is directly
+   * used mobx will rerender the entire component on submit
+   * as we update local cache first.
+   */
+  useEffect(() => {
+    setOptions(tagService.tagsSelectOptions)
+  }, [tagService, isOpen])
+
   const defaultOption = tagService.selectedOption
   const closeModal = () => tagService.createModal.close()
 
   return (
-    <ModalForm.Modal
-      okText="Create Tag"
-      onCancel={closeModal}
-      open={tagService.createModal.isOpen}
-    >
+    <ModalForm.Modal okText="Create Tag" onCancel={closeModal} open={isOpen}>
       <ModalForm.Form
         model={{
           id: v4(),
