09da49092da4ad8416f7769c9d810635f2ad9a6d
Mohammed S. Yaseen <moha.98.1900@gmail.com>
2023-03-29 11:09:42 +0300

    refactor: replace hook with custom SelectTag component

--- libs/frontend/domain/tag/src/use-cases/create-tag/CreateTagModal.tsx
+++ libs/frontend/domain/tag/src/use-cases/create-tag/CreateTagModal.tsx
@@ -2,31 +2,19 @@ import type { ICreateTagData } from '@codelab/frontend/abstract/core'
 import { useStore } from '@codelab/frontend/presenter/container'
 import { createNotificationHandler } from '@codelab/frontend/shared/utils'
 import { ModalForm } from '@codelab/frontend/view/components'
-import type { LabeledValue } from 'antd/es/select'
 import { observer } from 'mobx-react-lite'
-import React, { useEffect, useState } from 'react'
-import { AutoFields, SelectField } from 'uniforms-antd'
+import React from 'react'
+import { AutoField, AutoFields } from 'uniforms-antd'
 import { v4 } from 'uuid'
 import { createTagSchema } from './create.tag.schema'
 
 export const CreateTagModal = observer(() => {
   const { tagService, userService } = useStore()
-  const [options, setOptions] = useState<Array<LabeledValue>>([])
   const isOpen = tagService.createModal.isOpen
 
   const onSubmit = (input: ICreateTagData) => {
     return tagService.create(input)
   }
-
-  /**
-   * Get the options only when modal is opened (or closed)
-   * otherwise of tagService.tagsSelectOptions is directly
-   * used mobx will rerender the entire component on submit
-   * as we update local cache first.
-   */
-  useEffect(() => {
-    setOptions(tagService.tagsSelectOptions)
-  }, [tagService, isOpen])
 
   const defaultOption = tagService.selectedOption
   const closeModal = () => tagService.createModal.close()
@@ -48,13 +36,7 @@ export const CreateTagModal = observer(() => {
       >
         <AutoFields omitFields={['parent']} />
         {/* <DisplayIfNotRoot> */}
-        <SelectField
-          label="Parent Tag"
-          name="parent.id"
-          optionFilterProp="label"
-          options={options}
-          showSearch
-        />
+        <AutoField label="Parent Tag" name="parent.id" />
         {/* </DisplayIfNotRoot> */}
       </ModalForm.Form>
     </ModalForm.Modal>
