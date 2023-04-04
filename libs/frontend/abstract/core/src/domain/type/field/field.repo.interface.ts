ac4efadee31ebd23e5e6040575a59a790d0289a3
jethrocabaluna <jethrocabaluna310@gmail.com>
2023-04-04 16:50:11 +0800

    fix: build

--- libs/frontend/abstract/core/src/domain/type/field/field.repo.interface.ts
+++ libs/frontend/abstract/core/src/domain/type/field/field.repo.interface.ts
@@ -1,11 +1,9 @@
-import type {
-  FieldFragment,
-  FieldWhere,
-} from '@codelab/shared/abstract/codegen'
+import type { FieldWhere } from '@codelab/shared/abstract/codegen'
+import type { IEntity } from '@codelab/shared/abstract/types'
 import type { IRepository } from '../../../service'
 import type { IField } from './field.interface'
 
 export type IFieldRepository = Omit<
-  IRepository<IField, FieldFragment, FieldWhere>,
+  IRepository<IField, IEntity, FieldWhere>,
   'find'
 >
--- libs/frontend/abstract/core/src/domain/type/type.repo.interface.ts
+++ libs/frontend/abstract/core/src/domain/type/type.repo.interface.ts
@@ -3,17 +3,14 @@ import type {
   GetBaseTypesOptions,
   TypeFragment,
 } from '@codelab/shared/abstract/codegen'
+import type { IEntity } from '@codelab/shared/abstract/types'
 import type { IRepository } from '../../service'
 import type { BaseType_BaseType_Fragment } from './fragments/base-type.fragment.graphql.gen'
 import type { IType } from './types'
 
 export type BaseTypesOptions = GetBaseTypesOptions
 
-export type ITypeRepository = IRepository<
-  IType,
-  TypeFragment,
-  BaseTypeWhere
-> & {
+export type ITypeRepository = IRepository<IType, IEntity, BaseTypeWhere> & {
   findDescendants(parentIds: Array<string>): Promise<Array<TypeFragment>>
   findBaseTypes(options: BaseTypesOptions): Promise<{
     items: Array<BaseType_BaseType_Fragment>
