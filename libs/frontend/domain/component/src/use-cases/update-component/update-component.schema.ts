de7a16617d3f5c04facaae7fa6db75ec02540f7e
assim habeek <assimhabe@gmail.com>
2023-04-04 15:08:05 +0100

    revert: remove added changes by rebase

--- libs/frontend/domain/component/src/use-cases/update-component/update-component.schema.ts
+++ libs/frontend/domain/component/src/use-cases/update-component/update-component.schema.ts
@@ -11,6 +11,7 @@ export const updateComponentSchema: JSONSchemaType<IUpdateComponentData> = {
   properties: {
     ...idSchema,
     childrenContainerElement: {
+      label: '',
       properties: {
         id: {
           label: 'Container for component children',
