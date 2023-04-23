affd0ee59d3fc6b9353357a0a06b220944e022ee
Amr Mohamed <amrmuhmedd@gmail.com>
2023-04-23 15:13:45 +0200

    fix: refactor sort function

--- libs/frontend/domain/type/src/shared/sortFields.ts
+++ libs/frontend/domain/type/src/shared/sortFields.ts
@@ -1,26 +1,19 @@
 import { type IField } from '@codelab/frontend/abstract/core'
+import sortBy from 'lodash/sortBy'
 
 export const sortFieldsArray = (fields: Array<IField>) => {
-  const compareNodes = (field1: IField, field2: IField) => {
-    let node1 = field1
-    let node2 = field2
-    const pathA = [field1.id]
-    const pathB = [field2.id]
+  const nodes = new Map(fields.map((field) => [field.id, field]))
 
-    while (node1.prevSibling) {
-      node1 = fields.find((node) => node.id === node1.prevSibling?.id) as IField
-      pathA.unshift(node1.id)
+  const getPath = (field: IField) => {
+    const path = [field.id]
+
+    while (field.prevSibling) {
+      field = nodes.get(field.prevSibling.id) as IField
+      path.unshift(field.id)
     }
 
-    while (node2.prevSibling) {
-      node2 = fields.find((node) => node.id === node2.prevSibling?.id) as IField
-      pathB.unshift(node2.id)
-    }
-
-    const result = pathA.join().localeCompare(pathB.join())
-
-    return result !== 0 ? result : field1.id.localeCompare(field2.id)
+    return path.join()
   }
 
-  return fields.sort(compareNodes)
+  return sortBy(fields, [(field) => getPath(field), 'id'])
 }
