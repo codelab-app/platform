e6fc1b8089af6ad35f673608eb833ed4f7d3b8e5
Amr Mohamed <amrmuhmedd@gmail.com>
2023-04-28 06:10:15 +0300

    fix: solve lint issue

--- libs/backend/application/app/src/shared/removeDuplicatesFromApp.ts
+++ libs/backend/application/app/src/shared/removeDuplicatesFromApp.ts
@@ -1,7 +1,8 @@
-import { IAppExport } from '@codelab/backend/abstract/core'
+import type { IAppExport } from '@codelab/backend/abstract/core'
 
 export const removeDuplicates = (app: IAppExport) => {
   const uniqueIds = new Set()
+
   const pages = app.pages.map((page) => ({
     ...page,
     components: page.components.filter((comp) => {
@@ -9,10 +10,12 @@ export const removeDuplicates = (app: IAppExport) => {
         return false
       } else {
         uniqueIds.add(comp.id)
+
         return true
       }
     }),
   }))
+
   return {
     ...app,
     pages,
