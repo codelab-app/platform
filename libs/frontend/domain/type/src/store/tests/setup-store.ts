002bbd811392cd674b42c2fee14cd4826a609d51
assim habeek <assimhabe@gmail.com>
2023-05-02 05:57:21 +0100

    revert: restore store removed by mistake

--- libs/frontend/domain/type/src/store/tests/setup-store.ts
+++ libs/frontend/domain/type/src/store/tests/setup-store.ts
@@ -1,9 +1,13 @@
+import type { IType } from '@codelab/frontend/abstract/core'
 import { fieldRef, typeRef } from '@codelab/frontend/abstract/core'
 import { PrimitiveTypeKind } from '@codelab/shared/abstract/codegen'
 import { ITypeKind } from '@codelab/shared/abstract/core'
 import { objectMap } from 'mobx-keystone'
 import { v4 } from 'uuid'
+import { FieldService } from '../field.service'
 import { Field, InterfaceType, PrimitiveType, UnionType } from '../models'
+import { TypeService } from '../type.service'
+import { TestRootStore } from './test-root-store'
 
 export const stringType = new PrimitiveType({
   id: v4(),
@@ -70,4 +74,21 @@ export const interfaceWithUnionField = new InterfaceType({
   owner: {
     auth0Id: '',
   },
+})
+
+export const rootStore = new TestRootStore({
+  fieldService: new FieldService({
+    fields: objectMap([
+      [stringField.id, stringField],
+      [unionField.id, unionField],
+    ]),
+  }),
+  typeService: new TypeService({
+    types: objectMap<IType>([
+      [unionType.id, unionType],
+      [interfaceWithUnionField.id, interfaceWithUnionField],
+      [intType.id, intType],
+      [stringType.id, stringType],
+    ]),
+  }),
 })
