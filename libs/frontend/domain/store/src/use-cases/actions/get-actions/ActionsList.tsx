6852bdfd3eadf817a0596752a32a650e413ac6b8
assim habeek <assimhabe@gmail.com>
2023-05-10 12:59:33 +0100

    fix: build issues

--- libs/frontend/domain/store/src/use-cases/actions/get-actions/ActionsList.tsx
+++ libs/frontend/domain/store/src/use-cases/actions/get-actions/ActionsList.tsx
@@ -6,7 +6,7 @@ import { ActionItem } from './ActionItem'
 
 export const ActionsList = observer<{ store: IStore }>(({ store }) => (
   <List
-    dataSource={store.actions}
+    dataSource={store.actions.map((action) => action.current)}
     renderItem={(action) => <ActionItem action={action} key={action.id} />}
     size="small"
   />
