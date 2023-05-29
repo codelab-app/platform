af2cad8a09a46b1d9e45a44e3db5d6f670d4a3e9
melhamin <mo.elhamin@gmail.com>
2023-05-30 01:16:24 +0300

    wip: fix app import/export

--- libs/backend/application/app/src/use-case/export-apps.ts
+++ libs/backend/application/app/src/use-case/export-apps.ts
@@ -1,13 +1,14 @@
 import type { AppWhere } from '@codelab/backend/abstract/codegen'
 import type { IAppExport } from '@codelab/backend/abstract/core'
-import { AppRepository, getApp } from '@codelab/backend/domain/app'
+import { AppRepository, getAppPages } from '@codelab/backend/domain/app'
 
 export const exportApps = async (where: AppWhere) => {
   const appRepository = new AppRepository()
   const apps = await appRepository.find(where)
 
   return apps.reduce(async (appsData, app) => {
-    const { app: appExport } = await getApp(app)
+    const pagesData = await getAppPages(app)
+    const appExport = { ...app, pages: pagesData }
 
     return [...(await appsData), appExport]
   }, Promise.resolve<Array<IAppExport>>([]))
