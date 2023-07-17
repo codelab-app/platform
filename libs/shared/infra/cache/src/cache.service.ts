2b61745b926cc99228ab3cab496b1b205f8ee833
Webber Wang <webber@codelab.app>
2023-07-17 14:31:39 +0800

    wip: add vercel kv

--- libs/shared/infra/cache/src/cache.service.ts
+++ libs/shared/infra/cache/src/cache.service.ts
@@ -1,5 +1,7 @@
+import { getEnv } from '@codelab/shared/config'
 import { flattenWithPrefix, withTracing } from '@codelab/shared/infra/otel'
-import Redis from 'ioredis'
+import type { VercelKV } from '@vercel/kv'
+import { createClient } from '@vercel/kv'
 
 export enum CacheInstance {
   Backend = 'Backend',
@@ -10,19 +12,19 @@ interface ICacheService {
   clearAllCache(): Promise<void>
 }
 
-export class CacheService implements ICacheService {
+export class CacheService {
   private static instances: Record<CacheInstance, CacheService | undefined> = {
     [CacheInstance.Backend]: undefined,
     [CacheInstance.Frontend]: undefined,
   }
 
-  readonly cache: Redis
+  // readonly cache: VercelKV
 
   private constructor() {
-    this.cache = new Redis({ host: '127.0.0.1', port: 6379 })
-    this.cache.on('error', (error) => {
-      console.error(`Redis error: ${error}`)
-    })
+    // this.cache = createClient({
+    //   token: getEnv().vercelKV.restApiToken,
+    //   url: getEnv().vercelKV.restApiUrl,
+    // })
   }
 
   public static getInstance(name: CacheInstance): CacheService {
@@ -35,52 +37,15 @@ export class CacheService implements ICacheService {
     return instance
   }
 
-  public async clearAllCache() {
-    await this.cache.flushall()
-
-    return
-  }
-
   public async clearCache(model?: string, where?: object) {
-    await withTracing('CacheService.clearCache()', async (span) => {
-      if (!model) {
-        // If model is not defined, clear all cache
-        return await this.cache.flushall()
-      }
-
-      let pattern = `${model}:`
-
-      if (where) {
-        // If 'where' is defined, sort entries by keys
-        const sortedWhereEntries = Object.entries(where).sort(
-          ([aKey], [bKey]) => aKey.localeCompare(bKey),
-        )
-
-        // Append key:value pairs to pattern
-        sortedWhereEntries.forEach(([key, value], index, array) => {
-          pattern += `${key}:${value}`
-
-          if (index < array.length - 1) {
-            pattern += ':'
-          }
-        })
-      }
-
-      let cursor = '0'
-      const pipeline = this.cache.pipeline()
-
-      do {
-        const res = await this.cache.scan(cursor, 'MATCH', `${pattern}*`)
-
-        cursor = res[0]
-        res[1].forEach((key: string) => pipeline.del(key))
-      } while (cursor !== '0')
-
-      const whereAttributes = flattenWithPrefix(where ?? {}, 'where')
-      span.setAttributes({ model })
-      span.setAttributes(whereAttributes)
-
-      return await pipeline.exec()
-    })()
+    // await withTracing('CacheService.clearCache()', async (span) => {
+    //   if (!model) {
+    //     // If model is not defined, clear all cache
+    //     return await this.cache.flushall()
+    //   }
+    //   const whereAttributes = flattenWithPrefix(where ?? {}, 'where')
+    //   span.setAttributes({ model })
+    //   span.setAttributes(whereAttributes)
+    // })()
   }
 }
