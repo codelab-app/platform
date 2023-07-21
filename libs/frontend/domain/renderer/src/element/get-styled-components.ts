e31c75474de97550bf3945900c8b2f76367d3166
melhamin <mo.elhamin@gmail.com>
2023-07-21 17:41:29 +0300

    fix: elements not rendered conditionally

--- libs/frontend/domain/renderer/src/element/get-styled-components.ts
+++ libs/frontend/domain/renderer/src/element/get-styled-components.ts
@@ -20,7 +20,20 @@ export const getStyledComponent = (
     `
   }
 
-  return ReactComponent
+  // styled() uses hooks internally. Therefore, we need to always wrap the component
+  // with styled, otherwise the number of hooks will be different between the
+  // prev and next render, which will cause errors.
+  // This can happen if an element is rendered conditionally (renderIf)
+  const Wrapper = (props: React.PropsWithChildren) => {
+    // Don't pass props to React.Fragment
+    if (ReactComponent === React.Fragment) {
+      return React.createElement(ReactComponent, null, props.children)
+    }
+
+    return React.createElement(ReactComponent, props, props.children)
+  }
+
+  return styled(Wrapper)``
 }
 
 export const jsonStringToCss = (json: string | null | undefined) => {
