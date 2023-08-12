83063704d8ae36e16e0210c13afd4077661ff539
Vladyslav Polishchuk <gm.vpolishchuk@gmail.com>
2023-08-12 12:55:39 +0300

    fix: pages sorting by kind and name

---
+++ libs/frontend/shared/utils/src/sort-pages-by-kind-and-name.ts
@@ -0,0 +1,43 @@
+import type { BuilderPageFragment } from '@codelab/shared/abstract/codegen'
+import { IPageKind } from '@codelab/shared/abstract/core'
+
+/**
+ * Sort pages by kind and name using the following rules:
+ * - `_app` always comes first
+ * - then all regular pages sorted by `name`
+ * - `404` always comes last but one
+ * - `500` always comes last
+ */
+export const sortPagesByKindAndName = (pages: Array<BuilderPageFragment>) => {
+  return pages.sort((a, b) => {
+    // '_app' should always be first
+    if (a.kind === IPageKind.Provider) {
+      return -1
+    }
+
+    if (b.kind === IPageKind.Provider) {
+      return 1
+    }
+
+    // '404' should always be last but one
+    if (a.kind === IPageKind.NotFound) {
+      return b.kind === IPageKind.InternalServerError ? -1 : 1
+    }
+
+    if (b.kind === IPageKind.NotFound) {
+      return a.kind === IPageKind.InternalServerError ? 1 : -1
+    }
+
+    // '500' should always be last
+    if (a.kind === IPageKind.InternalServerError) {
+      return 1
+    }
+
+    if (b.kind === IPageKind.InternalServerError) {
+      return -1
+    }
+
+    // All other pages should be sorted by `name`
+    return a.name.localeCompare(b.name)
+  })
+}
