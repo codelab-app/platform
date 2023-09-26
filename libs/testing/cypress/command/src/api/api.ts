3a24b9524f40819c6cfae3cb00f144d5ea1d602b
Webber Wang <webber@codelab.app>
2023-09-26 14:00:33 +0800

    ci: add logging to jwt guard
    ci: fixing 401 error
    
    ci: fixing 401 error
    
    revert: nx
    
    fix: working cypress auth0
    
    fix: working cypress auth0
    
    fix: working cypress auth0

--- libs/testing/cypress/command/src/api/api.ts
+++ libs/testing/cypress/command/src/api/api.ts
@@ -1,9 +1,17 @@
 // import { getEnv } from '@codelab/shared/config'
 
 export const postApiRequest = <T>(url: string) => {
-  return cy.request<T>({
-    method: 'POST',
-    timeout: 10000,
-    url,
+  return cy.getAllCookies().then((cookie) => {
+    cy.log('cookie', cookie)
+
+    return cy.request<T>({
+      // headers: {
+      // cookie,
+      // Authorization: `Bearer ${accessToken}`,
+      // },
+      method: 'POST',
+      timeout: 10000,
+      url,
+    })
   })
 }
