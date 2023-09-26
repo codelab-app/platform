c0e9a2b7c65938a3b150cf14a27a2e0b1e928021
Webber Wang <webber@codelab.app>
2023-09-26 15:14:25 +0800

    wip: remove logs

--- libs/backend/infra/adapter/graphql/src/auth.middleware.ts
+++ libs/backend/infra/adapter/graphql/src/auth.middleware.ts
@@ -14,8 +14,6 @@ export const authMiddleware: NextApiHandler = async (req, res) => {
 
     const accessToken = session?.accessToken
 
-    console.log('accessToken', accessToken)
-
     /**
      * Instead of appending headers to the frontend GraphQL client, we could access session here in serverless then append at the middleware level
      */
@@ -27,8 +25,6 @@ export const authMiddleware: NextApiHandler = async (req, res) => {
      * Attach ID token so we have more information
      */
     const idToken = session?.idToken
-
-    console.log('idToken', idToken)
 
     if (idToken) {
       req.headers['X-ID-TOKEN'] = idToken
