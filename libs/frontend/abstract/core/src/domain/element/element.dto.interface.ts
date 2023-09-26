bf6249cb8e68cf7866443359f21c2c2286b34409
Webber Wang <webber@codelab.app>
2023-09-26 15:11:33 +0800

    fix: merge errors

--- libs/frontend/abstract/core/src/domain/element/element.dto.interface.ts
+++ libs/frontend/abstract/core/src/domain/element/element.dto.interface.ts
@@ -1,46 +1,53 @@
-import type { IPropDTO, RenderType } from '@codelab/shared/abstract/core'
+import {
+  IElementDTO,
+  IPropDTO,
+  type IElementRenderType,
+} from '@codelab/shared/abstract/core'
 import type { IEntity, Nullable, Nullish } from '@codelab/shared/abstract/types'
+import { Static, Type } from '@sinclair/typebox'
+import { Overwrite } from 'utility-types'
 
-export interface ICreateElementData {
-  id: string
-  name: string
-  parentElement?: Nullable<IEntity>
-  postRenderAction?: Nullable<IEntity>
-  preRenderAction?: Nullable<IEntity>
-  prevSibling?: Nullable<IEntity>
-  props?: Nullable<Pick<IPropDTO, 'data'>>
-  /**
-   * We should connect to `atom` or `component` in future
-   */
-  renderType?: Nullable<RenderType>
-  style?: Nullable<string>
-}
+export const ICreateElementData = Type.Composite([
+  Type.Pick(IElementDTO, [
+    'id',
+    'name',
+    'parentElement',
+    'postRenderAction',
+    'preRenderAction',
+    'prevSibling',
+    'renderType',
+    'style',
+    'closestContainerNode',
+  ]),
+  Type.Object({
+    props: Type.Optional(Type.Pick(IPropDTO, ['data'])),
+  }),
+])
 
-export type IUpdateElementData = Pick<
+export type ICreateElementData = Static<typeof ICreateElementData>
+
+export type ICreateElementSchema = Overwrite<
   ICreateElementData,
-  'name' | 'postRenderAction' | 'preRenderAction' | 'renderType' | 'style'
-> &
-  Pick<ICreateElementData, 'id'> & {
-    childMapperComponent?: Nullish<IEntity>
-    childMapperPreviousSibling?: Nullish<IEntity>
-    childMapperPropKey?: Nullish<string>
-    renderForEachPropKey?: Nullable<string>
-    renderIfExpression?: Nullable<string>
-  }
+  { renderType: IElementRenderType | null }
+>
+
+export const IUpdateElementData = Type.Pick(IElementDTO, [
+  'childMapperComponent',
+  'childMapperPreviousSibling',
+  'childMapperPropKey',
+  'id',
+  'name',
+  'postRenderAction',
+  'preRenderAction',
+  'renderForEachPropKey',
+  'renderIfExpression',
+  'renderType',
+  'style',
+])
+
+export type IUpdateElementData = Static<typeof IUpdateElementData>
 
 /**
  * Some properties have their own forms, the base form only uses a subset of fields
  */
-export type IUpdateBaseElementData = Pick<
-  IUpdateElementData,
-  | 'childMapperComponent'
-  | 'childMapperPreviousSibling'
-  | 'childMapperPropKey'
-  | 'id'
-  | 'name'
-  | 'postRenderAction'
-  | 'preRenderAction'
-  | 'renderForEachPropKey'
-  | 'renderIfExpression'
-  | 'renderType'
->
+export type IUpdateBaseElementData = Omit<IUpdateElementData, 'style'>
