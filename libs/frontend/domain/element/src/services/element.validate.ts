b7de25482bf85186f20a09a88e28c1ba3b385632
Webber Wang <webber@codelab.app>
2023-10-04 13:03:59 +0800

    test: fix create element spec

--- libs/frontend/domain/element/src/services/element.validate.ts
+++ libs/frontend/domain/element/src/services/element.validate.ts
@@ -3,8 +3,13 @@ import type { IElementModel } from '@codelab/frontend/abstract/domain'
 export const elementValidate = (element: IElementModel) => {
   const hasParentComponent = Boolean(element.parentComponent)
   const hasPage = Boolean(element.page)
+  const isRoot = Boolean(element.isRoot)
 
-  if ([hasParentComponent, hasPage].filter(Boolean).length !== 1) {
+  if (isRoot && [hasParentComponent, hasPage].filter(Boolean).length !== 1) {
+    console.error({
+      hasPage,
+      hasParentComponent,
+    })
     throw new Error('Element must have exactly one container')
   }
 }
