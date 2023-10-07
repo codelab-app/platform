0e231ffd267798e4f78ab3667139b68cff87903a
Webber Wang <webber@codelab.app>
2023-10-08 00:33:53 +0800

    ci: resolve root path

--- libs/backend/shared/util/src/file/resolve-workspace-root.ts
+++ libs/backend/shared/util/src/file/resolve-workspace-root.ts
@@ -1,5 +1,9 @@
-import { sync } from 'find-up'
-import path, { dirname } from 'path'
+import path from 'path'
 
-export const resolveWorkspaceRoot = (basePath: string) =>
-  path.resolve(dirname(sync('package.json')!), basePath)
+export const resolveWorkspaceRoot = (basePath: string) => {
+  if (process.env['NX_CYPRESS_TARGET_CONFIGURATION']) {
+    return path.resolve(process.cwd(), '../../..', basePath)
+  }
+
+  return path.resolve(process.cwd(), basePath)
+}
