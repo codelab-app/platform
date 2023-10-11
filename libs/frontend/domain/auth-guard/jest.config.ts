3ce3c5ec47907f1be9d71789ded36daea7e468cf
assim habeek <assimhabe@gmail.com>
2023-10-11 15:25:29 +0100

    wip: generate auth guards frontend domain package

---
+++ libs/frontend/domain/auth-guard/jest.config.ts
@@ -0,0 +1,36 @@
+/* eslint-disable */
+export default {
+  displayName: 'frontend-domain-auth-guard',
+
+  globals: {},
+  transform: {
+    '^.+.[tj]sx?$': [
+      '@swc/jest',
+      {
+        jsc: {
+          parser: { syntax: 'typescript', tsx: true, decorators: true },
+          transform: {
+            decoratorMetadata: true,
+            react: { runtime: 'automatic' },
+          },
+        },
+      },
+    ],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../../../coverage/libs/frontend/domain/auth-guard',
+  preset: '../../../../jest.preset.js',
+  reporters: [
+    'default',
+    [
+      'jest-junit',
+      {
+        outputName: 'frontend-domain-auth-guard.xml',
+        reportTestSuiteErrors: true,
+        titleTemplate: '{classname} > {title}',
+        ancestorSeparator: ' › ',
+        classNameTemplate: '{displayName} > {filename}',
+      },
+    ],
+  ],
+}
