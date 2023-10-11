80de8d6f333e275691d1d0fdc81b2d568c345f6f
Webber Wang <webber@codelab.app>
2023-10-11 11:45:11 +0800

    wip: fixing field type loading on builder

--- libs/shared/abstract/codegen/src/types.api.graphql.gen.ts
+++ libs/shared/abstract/codegen/src/types.api.graphql.gen.ts
@@ -21384,6 +21384,15 @@ export type GetAppDevelopmentQuery = {
   apps: Array<{ __typename?: 'App' } & AppDevelopmentFragment>
   atoms: Array<{ __typename?: 'Atom' } & AtomDevelopmentFragment>
   resources: Array<{ __typename?: 'Resource' } & ResourceFragment>
+  primitiveTypes: Array<
+    { __typename?: 'PrimitiveType' } & PrimitiveTypeFragment
+  >
+  reactNodeTypes: Array<
+    { __typename?: 'ReactNodeType' } & ReactNodeTypeFragment
+  >
+  renderPropTypes: Array<
+    { __typename?: 'RenderPropType' } & RenderPropTypeFragment
+  >
 }
 
 export type GetAppProductionQueryVariables = Exact<{
