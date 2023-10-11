9385df71c4a37cc357dc268660868faad5e8872c
assim habeek <assimhabe@gmail.com>
2023-10-11 13:39:58 +0100

    fix: graphql client doesn't accept relative urls on server

--- libs/frontend/infra/graphql/src/client.ts
+++ libs/frontend/infra/graphql/src/client.ts
@@ -1,9 +1,19 @@
 import { getEnv } from '@codelab/shared/config'
+import { isServer } from '@codelab/shared/utils'
 import { GraphQLClient } from 'graphql-request'
 import type { GraphQLClientResponse } from 'graphql-request/build/cjs/types'
 
 export const client = new GraphQLClient(getEnv().endpoint.graphqlApiProxyUrl, {
   errorPolicy: 'all',
+  requestMiddleware: (request) => {
+    // fetch doesn't accept relative urls on server
+    if (isServer) {
+      const hostname = getEnv().endpoint.platformHost
+      request.url = `${hostname}${request.url}`
+    }
+
+    return request
+  },
   responseMiddleware: (response: Error | GraphQLClientResponse<unknown>) => {
     if (response instanceof Error) {
       console.error(response)
