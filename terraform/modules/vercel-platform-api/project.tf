2ea3efba7500d930456cb902ff92afc70112a13d
Vladyslav Polishchuk <gm.vpolishchuk@gmail.com>
2023-10-28 09:36:10 +0200

    fix: terraform env variable name

--- terraform/modules/vercel-platform-api/project.tf
+++ terraform/modules/vercel-platform-api/project.tf
@@ -19,7 +19,7 @@ resource "vercel_project" "platform_api" {
   environment = [
     {
       target = ["production", "preview"]
-      key    = "PLATFORM_API_HOST"
+      key    = "NEXT_PUBLIC_PLATFORM_API_HOST"
       value  = var.next_public_platform_api_host
     },
     # Auth0
