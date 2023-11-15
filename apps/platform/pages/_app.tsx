523dd810e9db4662a51ccbd12ea415f36c64751f
Vladyslav Polishchuk <gm.vpolishchuk@gmail.com>
2023-11-15 15:07:02 +0100

    wip: try removing code in _app and _document

--- apps/platform/pages/_app.tsx
+++ apps/platform/pages/_app.tsx
@@ -1,79 +1,24 @@
-// import '../src/wdyr'
 import '../styles/global.css'
 import { UserProvider } from '@auth0/nextjs-auth0/client'
 import type { IAppProps, IPageProps } from '@codelab/frontend/abstract/domain'
 import type { CodelabPage } from '@codelab/frontend/abstract/types'
-import { StoreProvider } from '@codelab/frontend/application/shared/store'
-import { initializeStore } from '@codelab/frontend/infra/mobx'
-import { withActiveSpan } from '@codelab/frontend/infra/otel'
-import { CuiProvider } from '@codelab/frontend/presentation/codelab-ui'
-import { useTwindConfig } from '@codelab/frontend/shared/utils'
-import {
-  withTracerActiveSpanSync,
-  withTracerSpan,
-} from '@codelab/shared/infra/otel'
-import { trace } from '@opentelemetry/api'
-import { App as AntdApp, ConfigProvider } from 'antd'
-import { useRouter } from 'next/router'
-import React, { useMemo } from 'react'
-import config from '../twind.config'
+import { App as AntdApp } from 'antd'
+import React from 'react'
 
-const sleep = (ms: number) => {
-  return new Promise((resolve) => {
-    setTimeout(resolve, ms)
-  })
-}
-
-const App = ({ Component, pageProps: { user } }: IAppProps<IPageProps>) => {
-  const router = useRouter()
-
-  const store = useMemo(() => {
-    console.log('initializeStore', user)
-
-    if (!user) {
-      return null
-    }
-
-    return withTracerSpan('initializeStore', () =>
-      initializeStore({ routerQuery: router.query, user }),
-    )
-  }, [])
-
+const App = ({ Component }: IAppProps<IPageProps>) => {
   const { Layout = ({ children }) => <>{children}</> } =
     Component as CodelabPage<object, object, object>
 
-  // useTwindConfig(config)
-
   return (
-    <StoreProvider value={store}>
-      {/* <Analytics /> */}
-      <UserProvider>
-        <CuiProvider>
-          <ConfigProvider
-            theme={{
-              components: {
-                Layout: {
-                  headerBg: '#ffffff',
-                },
-              },
-              token: {
-                // fontFamily: `'Nunito', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
-                // 'Helvetica Neue', Arial, 'Noto Sans', sans-serif, 'Apple Color Emoji',
-                // 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'`,
-              },
-            }}
-          >
-            <Layout>
-              {(props) => (
-                <AntdApp>
-                  <Component {...props} />
-                </AntdApp>
-              )}
-            </Layout>
-          </ConfigProvider>
-        </CuiProvider>
-      </UserProvider>
-    </StoreProvider>
+    <UserProvider>
+      <Layout>
+        {(props) => (
+          <AntdApp>
+            <Component {...props} />
+          </AntdApp>
+        )}
+      </Layout>
+    </UserProvider>
   )
 }
 
