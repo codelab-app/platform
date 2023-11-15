362503ea14c1f9baf700c7c3e593f319410a8867
Webber Wang <webber@codelab.app>
2023-11-14 23:07:32 -0800

    wip: test out opentel on vercel

--- apps/platform/instrumentation.ts
+++ apps/platform/instrumentation.ts
@@ -2,4 +2,10 @@ import { registerOTel } from '@vercel/otel'
 
 export const register = async () => {
   registerOTel('platform')
+
+  console.log('next_runtime', process.env.NEXT_RUNTIME)
+
+  if (process.env.NEXT_RUNTIME === 'nodejs') {
+    // await import('./instrumentation.node')
+  }
 }
--- apps/platform/pages/_app.tsx
+++ apps/platform/pages/_app.tsx
@@ -7,21 +7,38 @@ import { StoreProvider } from '@codelab/frontend/application/shared/store'
 import { initializeStore } from '@codelab/frontend/infra/mobx'
 import { CuiProvider } from '@codelab/frontend/presentation/codelab-ui'
 import { useTwindConfig } from '@codelab/frontend/shared/utils'
+import { trace } from '@opentelemetry/api'
 import { App as AntdApp, ConfigProvider } from 'antd'
 import { useRouter } from 'next/router'
 import React, { useMemo } from 'react'
 import config from '../twind.config'
 
+const sleep = (ms: number) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms)
+  })
+}
+
 const App = ({ Component, pageProps: { user } }: IAppProps<IPageProps>) => {
   const router = useRouter()
 
   const store = useMemo(() => {
+    console.log('initializeStore', user)
+
     if (!user) {
       return null
     }
 
-    return initializeStore({ routerQuery: router.query, user })
-  }, [user])
+    return trace
+      .getTracer('default')
+      .startActiveSpan('initializeStore', (span) => {
+        try {
+          return initializeStore({ routerQuery: router.query, user })
+        } finally {
+          span.end()
+        }
+      })
+  }, [])
 
   const { Layout = ({ children }) => <>{children}</> } =
     Component as CodelabPage<object, object, object>
