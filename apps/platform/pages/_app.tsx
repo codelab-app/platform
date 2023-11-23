7c09feb5ad6f9bec9b7d7a4f7991925c22805fcd
melhamin <mo.elhamin@gmail.com>
2023-11-23 20:52:18 +0300

    fix: builder layout issue

--- apps/platform/pages/_app.tsx
+++ apps/platform/pages/_app.tsx
@@ -49,7 +49,7 @@ const App = ({ Component, pageProps: { user } }: IAppProps<IPageProps>) => {
           >
             <Layout>
               {(props) => (
-                <AntdApp>
+                <AntdApp className="h-full w-full">
                   <Component {...props} />
                 </AntdApp>
               )}
