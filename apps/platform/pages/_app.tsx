b48318606567641728a914c3740a4cc9b4808d7c
melhamin <mo.elhamin@gmail.com>
2023-11-23 21:28:11 +0300

    fix: store is not initialized

--- apps/platform/pages/_app.tsx
+++ apps/platform/pages/_app.tsx
@@ -2,23 +2,39 @@ import '../styles/global.css'
 import { UserProvider } from '@auth0/nextjs-auth0/client'
 import type { IAppProps, IPageProps } from '@codelab/frontend/abstract/domain'
 import type { CodelabPage } from '@codelab/frontend/abstract/types'
+import { StoreProvider } from '@codelab/frontend/application/shared/store'
+import { initializeStore } from '@codelab/frontend/infra/mobx'
 import { App as AntdApp } from 'antd'
-import React from 'react'
+import { useRouter } from 'next/router'
+import React, { useMemo } from 'react'
 
-const App = ({ Component }: IAppProps<IPageProps>) => {
+const App = ({ Component, pageProps }: IAppProps<IPageProps>) => {
+  const router = useRouter()
+  const { user } = pageProps
+
+  const store = useMemo(() => {
+    if (!user) {
+      return null
+    }
+
+    return initializeStore({ routerQuery: router.query, user })
+  }, [user])
+
   const { Layout = ({ children }) => <>{children}</> } =
     Component as CodelabPage<object, object, object>
 
   return (
-    <UserProvider>
-      <Layout>
-        {(props) => (
-          <AntdApp>
-            <Component {...props} />
-          </AntdApp>
-        )}
-      </Layout>
-    </UserProvider>
+    <StoreProvider value={store}>
+      <UserProvider>
+        <Layout>
+          {(props) => (
+            <AntdApp>
+              <Component {...props} />
+            </AntdApp>
+          )}
+        </Layout>
+      </UserProvider>
+    </StoreProvider>
   )
 }
 
