fed601cf46e1e2c9271657f778508ce884607521
Vladyslav Polishchuk <gm.vpolishchuk@gmail.com>
2023-12-04 21:12:20 +0100

    wip: comment puppeteer

--- libs/backend/application/admin/src/use-case/scrape/scrape-ant-design-data.ts
+++ libs/backend/application/admin/src/use-case/scrape/scrape-ant-design-data.ts
@@ -1,9 +1,7 @@
 import 'jquery'
 import type { AntDesignApi } from '@codelab/backend/abstract/core'
-import { saveFormattedFile } from '@codelab/backend/shared/util'
-import path from 'path'
 import type { Browser } from 'puppeteer'
-import puppeteer from 'puppeteer'
+// import puppeteer from 'puppeteer'
 
 declare const $: JQueryStatic
 
@@ -61,44 +59,34 @@ const getComponentApiData = async (
 }
 
 export const scrapeAntDesignData = async () => {
-  const browser = await puppeteer.launch({ headless: 'new' })
-  const overviewPage = await browser.newPage()
-
-  await overviewPage.goto(`${BASE_URL}overview`, { waitUntil: 'networkidle2' })
-
-  // Inject jQuery into the page
-  await overviewPage.addScriptTag({
-    path: 'node_modules/jquery/dist/jquery.min.js',
-  })
-
-  overviewPage.on('console', (msg) => console.log('PAGE LOG:', msg.text()))
-
-  const components = await overviewPage.evaluate(() => {
-    const sidebarLinks = Array.from(
-      $('section.main-menu-inner .ant-menu-item a'),
-    )
-
-    return sidebarLinks
-      .map((link) => $(link).attr('href')?.replace('/components/', '') || '')
-      .filter((link) => link !== 'overview')
-    // .slice(0, 3)
-  })
-
-  for (const [index, component] of components.entries()) {
-    console.log(
-      `Fetching component data: ${component} (${index + 1}/${
-        components.length
-      })...`,
-    )
-
-    const apiData = await getComponentApiData(browser, component)
-
-    saveFormattedFile(path.join(outputDirectory, `${component}.json`), apiData)
-  }
-
-  await browser.close()
-
-  console.log(
-    'Scraping completed. The data has been saved to the "data/antd-v5" directory.',
-  )
+  // const browser = await puppeteer.launch({ headless: 'new' })
+  // const overviewPage = await browser.newPage()
+  // await overviewPage.goto(`${BASE_URL}overview`, { waitUntil: 'networkidle2' })
+  // // Inject jQuery into the page
+  // await overviewPage.addScriptTag({
+  //   path: 'node_modules/jquery/dist/jquery.min.js',
+  // })
+  // overviewPage.on('console', (msg) => console.log('PAGE LOG:', msg.text()))
+  // const components = await overviewPage.evaluate(() => {
+  //   const sidebarLinks = Array.from(
+  //     $('section.main-menu-inner .ant-menu-item a'),
+  //   )
+  //   return sidebarLinks
+  //     .map((link) => $(link).attr('href')?.replace('/components/', '') || '')
+  //     .filter((link) => link !== 'overview')
+  //   // .slice(0, 3)
+  // })
+  // for (const [index, component] of components.entries()) {
+  //   console.log(
+  //     `Fetching component data: ${component} (${index + 1}/${
+  //       components.length
+  //     })...`,
+  //   )
+  //   const apiData = await getComponentApiData(browser, component)
+  //   saveFormattedFile(path.join(outputDirectory, `${component}.json`), apiData)
+  // }
+  // await browser.close()
+  // console.log(
+  //   'Scraping completed. The data has been saved to the "data/antd-v5" directory.',
+  // )
 }
