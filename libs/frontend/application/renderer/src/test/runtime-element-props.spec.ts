a942c23b7d934001355b2086a4261b4c4e2e170f
assim habeek <assimhabe@gmail.com>
2024-01-07 09:43:34 +0100

    test: add more specs for renderer

--- libs/frontend/application/renderer/src/test/runtime-element-props.spec.ts
+++ libs/frontend/application/renderer/src/test/runtime-element-props.spec.ts
@@ -1,5 +1,9 @@
 import { DATA_ELEMENT_ID } from '@codelab/frontend/abstract/domain'
-import { IPrimitiveTypeKind, ITypeKind } from '@codelab/shared/abstract/core'
+import {
+  IElementRenderTypeKind,
+  IPrimitiveTypeKind,
+  ITypeKind,
+} from '@codelab/shared/abstract/core'
 import { unregisterRootStore } from 'mobx-keystone'
 import { setupRuntimeElement } from './setup'
 import { rootApplicationStore } from './setup/root.test.store'
@@ -13,125 +17,124 @@ describe('Runtime Element props', () => {
     testbed = new TestBed()
   })
 
-  describe('RuntimeProps.props', () => {
-    it('should contain system props', () => {
-      const { element, runtimeElement } = setupRuntimeElement(testbed)
-      const runtimeProps = runtimeElement?.runtimeProps
+  it('should contain system props', () => {
+    const { element, runtimeElement } = setupRuntimeElement(testbed)
+    const runtimeProps = runtimeElement?.runtimeProps
 
-      expect(runtimeProps?.props).toMatchObject({
-        [DATA_ELEMENT_ID]: element.id,
-        key: element.id,
-        ref: expect.any(Function),
-      })
-    })
-
-    it('should contain element props', () => {
-      const { element, runtimeElement } = setupRuntimeElement(testbed)
-      const runtimeProps = runtimeElement?.runtimeProps
-
-      element.props.set('randomProp', 'RandomPropValue')
-
-      expect(runtimeProps?.props).toMatchObject({
-        randomProp: 'RandomPropValue',
-      })
-    })
-
-    it('should contain default props', () => {
-      const { typeService } = rootApplicationStore
-      const { element, runtimeElement } = setupRuntimeElement(testbed)
-      const runtimeProps = runtimeElement?.runtimeProps
-      const atom = element.renderType.current
-      const fieldKey = 'fieldKey'
-      const fieldDefaultValue = '"field-value"'
-
-      const field = testbed.addField({
-        api: atom.api.current,
-        defaultValues: fieldDefaultValue,
-        fieldType: typeService.typeDomainService.typesList.find(
-          (type) =>
-            type.kind === ITypeKind.PrimitiveType &&
-            type.primitiveKind === IPrimitiveTypeKind.String,
-        ),
-        key: fieldKey,
-      })
-
-      atom.api.current.writeCache({ fields: [field] })
-
-      expect(runtimeProps?.props).toMatchObject({
-        [fieldKey]: JSON.parse(fieldDefaultValue),
-      })
+    expect(runtimeProps?.props).toMatchObject({
+      [DATA_ELEMENT_ID]: element.id,
+      key: element.id,
+      ref: expect.any(Function),
     })
   })
 
-  describe('RuntimeProps.evaluatedProps', () => {
-    it('should evaluate state field expression', () => {
-      const { typeService } = rootApplicationStore
-      const { element, runtimeElement } = setupRuntimeElement(testbed)
-      const runtimeProps = runtimeElement?.runtimeProps
-      const fieldKey = 'fieldKey'
-      const fieldDefaultValue = 'some-value'
-      const propKey = 'propKey'
-      const storeApi = element.store.current.api.current
+  it('should contain element props', () => {
+    const { element, runtimeElement } = setupRuntimeElement(testbed)
+    const runtimeProps = runtimeElement?.runtimeProps
 
-      const field = testbed.addField({
-        api: storeApi,
-        defaultValues: JSON.stringify(fieldDefaultValue),
-        fieldType: typeService.typeDomainService.typesList.find(
-          (type) =>
-            type.kind === ITypeKind.PrimitiveType &&
-            type.primitiveKind === IPrimitiveTypeKind.String,
-        ),
-        key: fieldKey,
-      })
+    element.props.set('randomProp', 'RandomPropValue')
 
-      storeApi.writeCache({ fields: [field] })
+    expect(runtimeProps?.props).toMatchObject({
+      randomProp: 'RandomPropValue',
+    })
+  })
 
-      element.props.set(propKey, `{{state.${fieldKey}}}`)
+  it('should contain default props', () => {
+    const { typeService } = rootApplicationStore
+    const { element, runtimeElement } = setupRuntimeElement(testbed)
+    const runtimeProps = runtimeElement?.runtimeProps
+    const atom = element.renderType.current
+    const fieldKey = 'fieldKey'
+    const fieldDefaultValue = '"field-value"'
 
-      expect(runtimeProps?.evaluatedProps).toMatchObject({
-        [propKey]: fieldDefaultValue,
-      })
-
-      field.writeCache({ defaultValues: JSON.stringify('another-value') })
-
-      expect(runtimeProps?.evaluatedProps).toMatchObject({
-        [propKey]: 'another-value',
-      })
+    const field = testbed.addField({
+      api: atom.api.current,
+      defaultValues: fieldDefaultValue,
+      fieldType: typeService.typeDomainService.typesList.find(
+        (type) =>
+          type.kind === ITypeKind.PrimitiveType &&
+          type.primitiveKind === IPrimitiveTypeKind.String,
+      ),
+      key: fieldKey,
     })
 
-    it('should evaluate action expression', () => {
-      const { element, runtimeElement } = setupRuntimeElement(testbed)
-      const runtimeProps = runtimeElement?.runtimeProps
-      const actionName = 'sum'
-      const propKey = 'propKey'
+    atom.api.current.writeCache({ fields: [field] })
 
-      testbed.addCodeAction({
-        code: `function run(a,b){
+    expect(runtimeProps?.props).toMatchObject({
+      [fieldKey]: JSON.parse(fieldDefaultValue),
+    })
+  })
+})
+
+describe('RuntimeProps.evaluatedProps', () => {
+  it('should evaluate state field expression', () => {
+    const { typeService } = rootApplicationStore
+    const { element, runtimeElement } = setupRuntimeElement(testbed)
+    const runtimeProps = runtimeElement?.runtimeProps
+    const fieldKey = 'fieldKey'
+    const fieldDefaultValue = 'some-value'
+    const propKey = 'propKey'
+    const storeApi = element.store.current.api.current
+
+    const field = testbed.addField({
+      api: storeApi,
+      defaultValues: JSON.stringify(fieldDefaultValue),
+      fieldType: typeService.typeDomainService.typesList.find(
+        (type) =>
+          type.kind === ITypeKind.PrimitiveType &&
+          type.primitiveKind === IPrimitiveTypeKind.String,
+      ),
+      key: fieldKey,
+    })
+
+    storeApi.writeCache({ fields: [field] })
+
+    element.props.set(propKey, `{{state.${fieldKey}}}`)
+
+    expect(runtimeProps?.evaluatedProps).toMatchObject({
+      [propKey]: fieldDefaultValue,
+    })
+
+    field.writeCache({ defaultValues: JSON.stringify('another-value') })
+
+    expect(runtimeProps?.evaluatedProps).toMatchObject({
+      [propKey]: 'another-value',
+    })
+  })
+
+  it('should evaluate action expression', () => {
+    const { element, runtimeElement } = setupRuntimeElement(testbed)
+    const runtimeProps = runtimeElement?.runtimeProps
+    const actionName = 'sum'
+    const propKey = 'propKey'
+
+    testbed.addCodeAction({
+      code: `function run(a,b){
           return a + b;
         }`,
-        name: actionName,
-        store: element.store,
-      })
-
-      element.props.set(propKey, `{{actions.${actionName}}}`)
-
-      expect(runtimeProps?.evaluatedProps).toMatchObject({
-        [propKey]: expect.any(Function),
-      })
-
-      const actionRunner = runtimeProps?.evaluatedProps[propKey]
-
-      expect(actionRunner?.(5, 9)).toBe(14)
+      name: actionName,
+      store: element.store,
     })
 
-    it('should bind action with context', () => {
-      const { element, runtimeElement } = setupRuntimeElement(testbed)
-      const runtimeProps = runtimeElement?.runtimeProps
-      const actionName = 'sum'
-      const propKey = 'propKey'
+    element.props.set(propKey, `{{actions.${actionName}}}`)
 
-      testbed.addCodeAction({
-        code: `function run(){
+    expect(runtimeProps?.evaluatedProps).toMatchObject({
+      [propKey]: expect.any(Function),
+    })
+
+    const actionRunner = runtimeProps?.evaluatedProps[propKey]
+
+    expect(actionRunner?.(5, 9)).toBe(14)
+  })
+
+  it('should bind action with context', () => {
+    const { element, runtimeElement } = setupRuntimeElement(testbed)
+    const runtimeProps = runtimeElement?.runtimeProps
+    const actionName = 'sum'
+    const propKey = 'propKey'
+
+    testbed.addCodeAction({
+      code: `function run(){
           return {
             props,
             state,
@@ -142,37 +145,156 @@ describe('Runtime Element props', () => {
             rootRefs
           };
         }`,
-        name: actionName,
-        store: element.store,
-      })
-
-      element.props.set(propKey, `{{actions.${actionName}}}`)
-
-      const actionRunner = runtimeProps?.getActionRunner(actionName)
-
-      expect(actionRunner?.()).toMatchObject({
-        actions: {
-          [actionName]: expect.any(Function),
-        },
-        props: {
-          [propKey]: expect.any(Function),
-        },
-      })
+      name: actionName,
+      store: element.store,
     })
 
-    it('should evaluate ref expression', () => {
-      const { element, runtimeElement } = setupRuntimeElement(testbed)
-      const runtimeProps = runtimeElement?.runtimeProps
-      const propKey = 'propKey'
+    element.props.set(propKey, `{{actions.${actionName}}}`)
 
-      element.props.set(propKey, `{{refs.${element.slug}}}`)
+    const actionRunner = runtimeProps?.getActionRunner(actionName)
 
-      expect(runtimeProps?.evaluatedProps).toMatchObject({
-        [propKey]: {
-          // we get a null because we are not rendering
-          current: null,
-        },
-      })
+    expect(actionRunner?.()).toMatchObject({
+      actions: {
+        [actionName]: expect.any(Function),
+      },
+      props: {
+        [propKey]: expect.any(Function),
+      },
+    })
+  })
+
+  it('should evaluate ref expression', () => {
+    const { element, runtimeElement } = setupRuntimeElement(testbed)
+    const runtimeProps = runtimeElement?.runtimeProps
+    const propKey = 'propKey'
+
+    element.props.set(propKey, `{{refs.${element.slug}}}`)
+
+    expect(runtimeProps?.evaluatedProps).toMatchObject({
+      [propKey]: {
+        // we get a null because we are not rendering
+        current: null,
+      },
+    })
+  })
+
+  it('should evaluate root state field expression', () => {
+    const { typeService } = rootApplicationStore
+    const { element, page, runtimeElement } = setupRuntimeElement(testbed)
+    const runtimeProps = runtimeElement?.runtimeProps
+    const fieldKey = 'fieldKey'
+    const fieldDefaultValue = 'some-value'
+    const propKey = 'propKey'
+    const storeApi = page.providerPage?.store.current.api.current
+
+    const field = testbed.addField({
+      api: storeApi,
+      defaultValues: JSON.stringify(fieldDefaultValue),
+      fieldType: typeService.typeDomainService.typesList.find(
+        (type) =>
+          type.kind === ITypeKind.PrimitiveType &&
+          type.primitiveKind === IPrimitiveTypeKind.String,
+      ),
+      key: fieldKey,
+    })
+
+    storeApi.writeCache({ fields: [field] })
+
+    element.props.set(propKey, `{{rootState.${fieldKey}}}`)
+
+    expect(runtimeProps?.evaluatedProps).toMatchObject({
+      [propKey]: fieldDefaultValue,
+    })
+
+    field.writeCache({ defaultValues: JSON.stringify('another-value') })
+
+    expect(runtimeProps?.evaluatedProps).toMatchObject({
+      [propKey]: 'another-value',
+    })
+  })
+
+  it('should evaluate root action expression', () => {
+    const { element, page, runtimeElement } = setupRuntimeElement(testbed)
+    const runtimeProps = runtimeElement?.runtimeProps
+    const actionName = 'sum'
+    const propKey = 'propKey'
+
+    testbed.addCodeAction({
+      code: `function run(a,b){
+          return a + b;
+        }`,
+      name: actionName,
+      store: page.providerPage?.store,
+    })
+
+    element.props.set(propKey, `{{rootActions.${actionName}}}`)
+
+    expect(runtimeProps?.evaluatedProps).toMatchObject({
+      [propKey]: expect.any(Function),
+    })
+
+    const actionRunner = runtimeProps?.evaluatedProps[propKey]
+
+    expect(actionRunner?.(5, 9)).toBe(14)
+  })
+
+  it('should bind root action with context', () => {
+    const { element, page, runtimeElement } = setupRuntimeElement(testbed)
+    const runtimeProps = runtimeElement?.runtimeProps
+    const actionName = 'sum'
+    const propKey = 'propKey'
+
+    testbed.addCodeAction({
+      code: `function run(){
+          return {
+            props,
+            state,
+            refs,
+            actions,
+            rootState,
+            rootActions,
+            rootRefs
+          };
+        }`,
+      name: actionName,
+      store: page.providerPage?.store,
+    })
+
+    element.props.set(propKey, `{{rootActions.${actionName}}}`)
+
+    const actionRunner = runtimeProps?.getActionRunner(actionName)
+
+    expect(actionRunner?.()).toMatchObject({
+      props: {
+        [propKey]: expect.any(Function),
+      },
+      rootActions: {
+        [actionName]: expect.any(Function),
+      },
+    })
+  })
+
+  it('should evaluate root ref expression', () => {
+    const { element, page, runtimeElement } = setupRuntimeElement(testbed)
+    const runtimeProps = runtimeElement?.runtimeProps
+    const propKey = 'propKey'
+
+    page.providerPage?.rootElement.current.writeCache({
+      renderType: {
+        __typename: IElementRenderTypeKind.Atom,
+        id: element.renderType.id,
+      },
+    })
+
+    const slug = page.providerPage?.rootElement.current.slug
+
+    element.props.set(propKey, `{{rootRefs.${slug}}}`)
+
+    expect(runtimeProps?.evaluatedProps).toMatchObject({
+      [propKey]: {
+        // we get a null because we are not rendering
+        current: null,
+      },
     })
   })
 
