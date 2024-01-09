c74a84d8fa83f6ddd63063182fb10eadd069c090
Webber Wang <webber@codelab.app>
2024-01-09 16:48:45 +0800

    wip: testing deployment

---
+++ terraform/modules/digitalocean-codelab/platform-api.tf
@@ -0,0 +1,48 @@
+resource "digitalocean_app" "platform-api" {
+  spec {
+    name = "platform-api"
+    region = "sfo"
+
+    domain {
+      name = "api.codelab.ai"
+    }
+
+    service {
+      name = "platform-api"
+
+      github {
+        branch         = "terraform-do"
+        deploy_on_push = true
+        repo           = "codelab-app/platform"
+      }
+
+      build_command = "scripts/digitalocean/platform-api/build.sh"
+      run_command = "scripts/digitalocean/platform-api/run.sh"
+
+      env {
+        key   = "NEO4J_USER"
+        value = var.neo4j_user
+      }
+
+      env {
+        key   = "NEO4J_URI"
+        value = var.neo4j_uri
+      }
+
+      env {
+        key   = "NEO4J_PASSWORD"
+        value = var.neo4j_password
+      }
+
+      env {
+        key   = "NEXT_PUBLIC_PLATFORM_API_HOST"
+        value = var.next_public_platform_api_host
+      }
+
+      env {
+        key   = "AUTH0_ISSUER_BASE_URL"
+        value = var.auth0_issuer_base_url
+      }
+    }
+  }
+}
