21216026f2854307d6f9aaeebb15b35adb5bb777
Webber Wang <webber@codelab.app>
2024-01-26 16:40:12 +0800

    wip: remove unused packages
    wip: remove unused packages
    
    wip: remove unused packages

--- libs/backend/infra/adapter/graphql/README.md
+++
@@ -1,3 +0,0 @@
-# backend-infra-adapter-graphql
-
-This library was generated with [Nx](https://nx.dev).
--- libs/backend/infra/adapter/graphql/src/types.ts
+++
@@ -1,7 +0,0 @@
-import type { NextApiHandler, NextApiRequest, NextApiResponse } from 'next'
-
-export type NextMiddleware = (
-  req: NextApiRequest,
-  res: NextApiResponse,
-  next: NextApiHandler,
-) => NextApiHandler | Promise<NextApiHandler>
