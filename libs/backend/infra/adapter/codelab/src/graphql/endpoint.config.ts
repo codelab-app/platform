b5efe7b0132f2f8b1fb09a9e3e1529518d3647f9
Vladyslav Polishchuk <gm.vpolishchuk@gmail.com>
2024-02-08 09:24:48 +0100

    fix: api server deployment on digital ocean

--- libs/backend/infra/adapter/codelab/src/graphql/endpoint.config.ts
+++ libs/backend/infra/adapter/codelab/src/graphql/endpoint.config.ts
@@ -8,9 +8,16 @@ export const ENDPOINT_CONFIG_KEY = 'graphql'
  */
 export const endpointConfig = registerAs(ENDPOINT_CONFIG_KEY, () => {
   return {
-    graphqlApiPort: env
-      .get('NEXT_PUBLIC_PLATFORM_API_HOST')
-      .required()
-      .asUrlObject().port,
+    get graphqlApiPort() {
+      const apiHost = env
+        .get('NEXT_PUBLIC_PLATFORM_API_HOST')
+        .required()
+        .asUrlObject()
+
+      // for local development and CI, port is specified as part of NEXT_PUBLIC_PLATFORM_API_HOST,
+      // e.g. http://127.0.0.1:4000, but for production, the value of NEXT_PUBLIC_PLATFORM_API_HOST
+      // is plain URL, e.g. https://api.codelab.app, so need to fallback to value from DO env variable
+      return apiHost.port || env.get('PORT').required().asPortNumber()
+    },
   }
 })
