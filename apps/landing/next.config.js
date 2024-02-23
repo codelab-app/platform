a1d3974f079215edccbc044022cc702165d71c47
Webber Wang <webber@codelab.app>
2024-02-23 16:19:16 +0800

    revert: nx upgrade

--- apps/landing/next.config.js
+++ apps/landing/next.config.js
@@ -4,54 +4,11 @@ const nextConfig = {
   compiler: {
     styledComponents: true,
   },
-  experimental: {
-    // appDir: true,
-  },
   nx: {
     // Set this to true if you would like to to use SVGR
     // See: https://github.com/gregberge/svgr
     svgr: false,
   },
-  /**
-   * https://github.com/vercel/next.js/issues/58817
-   */
-  transpilePackages: [
-    'antd',
-    '@ant-design/icons',
-    '@ant-design/icons-svg',
-    'rc-cascader',
-    'rc-checkbox',
-    'rc-collapse',
-    'rc-dialog',
-    'rc-drawer',
-    'rc-dropdown',
-    'rc-field-form',
-    'rc-image',
-    'rc-input',
-    'rc-input-number',
-    'rc-mentions',
-    'rc-menu',
-    'rc-motion',
-    'rc-notification',
-    'rc-pagination',
-    'rc-picker',
-    'rc-progress',
-    'rc-rate',
-    'rc-resize-observer',
-    'rc-segmented',
-    'rc-select',
-    'rc-slider',
-    'rc-steps',
-    'rc-switch',
-    'rc-table',
-    'rc-tabs',
-    'rc-textarea',
-    'rc-tooltip',
-    'rc-tree',
-    'rc-tree-select',
-    'rc-upload',
-    'rc-util',
-  ],
 }
 
 const plugins = [
--- apps/websites/next.config.js
+++ apps/websites/next.config.js
@@ -29,51 +29,7 @@ const nextConfig = {
   compiler: {
     styledComponents: true,
   },
-  experimental: {
-    // appDir: true,
-    // css: true,
-  },
   nx: { svgr: true },
-  /**
-   * https://github.com/vercel/next.js/issues/58817
-   */
-  transpilePackages: [
-    'antd',
-    '@ant-design/icons',
-    '@ant-design/icons-svg',
-    'rc-cascader',
-    'rc-checkbox',
-    'rc-collapse',
-    'rc-dialog',
-    'rc-drawer',
-    'rc-dropdown',
-    'rc-field-form',
-    'rc-image',
-    'rc-input',
-    'rc-input-number',
-    'rc-mentions',
-    'rc-menu',
-    'rc-motion',
-    'rc-notification',
-    'rc-pagination',
-    'rc-picker',
-    'rc-progress',
-    'rc-rate',
-    'rc-resize-observer',
-    'rc-segmented',
-    'rc-select',
-    'rc-slider',
-    'rc-steps',
-    'rc-switch',
-    'rc-table',
-    'rc-tabs',
-    'rc-textarea',
-    'rc-tooltip',
-    'rc-tree',
-    'rc-tree-select',
-    'rc-upload',
-    'rc-util',
-  ],
 }
 
 /*
