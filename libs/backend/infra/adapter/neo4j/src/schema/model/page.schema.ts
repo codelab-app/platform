919888f594eb3140d112936b30d1a3be7a26b81c
Webber Wang <webber@codelab.app>
2024-03-07 22:33:29 +0800

    wip: temp disable authorization

--- libs/backend/infra/adapter/neo4j/src/schema/model/page.schema.ts
+++ libs/backend/infra/adapter/neo4j/src/schema/model/page.schema.ts
@@ -6,6 +6,12 @@ const pageKindSchema = `enum PageKind {
   ${Object.values(__PageKind).join('\n')}
 }`
 
+const allowFullAccessForPageOwner = `
+{
+  operations: [UPDATE, CREATE, DELETE]
+  where: { node: { app: { owner: { auth0Id: "$jwt.sub" } } } }
+}`
+
 export const pageSchema = gql`
   ${pageKindSchema}
 
@@ -36,16 +42,13 @@ export const pageSchema = gql`
     # when the app will be deployed - the page will be available on this URL
     url: String!
   }
-
-  extend type Page
-    @authorization(
-      validate: [
-        ${allowReadAccess}
-        ${allowFullAccessForAdmin}
-        {
-          operations: [UPDATE, CREATE, DELETE]
-          where: { node: { app: { owner: { auth0Id: "$jwt.sub" } } } }
-        }
-      ]
-    )
 `
+
+// extend type Page
+//   @authorization(
+//     validate: [
+//       ${allowReadAccess}
+//       ${allowFullAccessForAdmin}
+//       ${allowFullAccessForPageOwner}
+//     ]
+//   )
