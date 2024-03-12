4ce1b9b6c24607bdc8aecc0b2483cb81e37fa5fd
assim habeek <assimhabe@gmail.com>
2024-03-12 15:25:25 +0100

    fix: build errors

--- libs/frontend/abstract/application/src/renderer/runtime.component.service.interface.ts
+++ libs/frontend/abstract/application/src/renderer/runtime.component.service.interface.ts
@@ -17,6 +17,6 @@ export interface IRuntimeComponentService {
     childMapperIndex?: number,
     isTypedProp?: boolean,
   ): IRuntimeComponentModel
-  component(compositeKey: string): Maybe<IRuntimeComponentModel>
+  component(component: IComponentModel): Maybe<IRuntimeComponentModel>
   delete(runtimeComponent: IRuntimeComponentModel): void
 }
