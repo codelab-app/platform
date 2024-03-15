f6166f18c8f88d4b33f4bab359728dce6a495d5e
jethrocabaluna <jethrocabaluna310@gmail.com>
2024-03-15 16:58:53 +0800

    chore: moving setupTest out wip

--- apps/platform-e2e/src/e2e/store/pre-post-api-action.cy.ts
+++ apps/platform-e2e/src/e2e/store/pre-post-api-action.cy.ts
@@ -1,100 +1,24 @@
 import { MODEL_ACTION } from '@codelab/frontend/abstract/types'
 import { FIELD_TYPE } from '@codelab/frontend/test/cypress/antd'
-import type { App } from '@codelab/shared/abstract/codegen'
-import type {
-  IApiActionDto,
-  IAppDto,
-  ICodeActionDto,
-  IPageDto,
-  IResourceDto,
-} from '@codelab/shared/abstract/core'
-import { IPageKind, IPageKindName } from '@codelab/shared/abstract/core'
-import { findOrFail, slugify } from '@codelab/shared/utils'
+import type { IAppDto, IPageDto } from '@codelab/shared/abstract/core'
+import { IPageKindName } from '@codelab/shared/abstract/core'
+import { slugify } from '@codelab/shared/utils'
 import { createResourceData } from '../preview/resource.data'
 import {
-  postRenderApiActionCreateData,
   postRenderApiActionUrlSegment,
-  postRenderCodeActionCreateData,
-  preRenderApiActionCreateData,
   preRenderApiActionUrlSegment,
-  preRenderCodeActionCreateData,
   providerPageElement,
   stateKeyPostRender,
   stateKeyPreRender,
 } from './pre-post-api-action.data'
-
-let app: IAppDto
-let page: IPageDto
-
-const setupTest = () => {
-  cy.postApiRequest<App>('/app/seed-cypress-app')
-    .then(({ body }) => {
-      app = body
-      page = findOrFail(app.pages, (_page) => _page.kind === IPageKind.Provider)
-
-      cy.wrap(page).should('have.property', 'store')
-
-      return cy.wrap(app)
-    })
-    .as('createdApp')
-
-  cy.get('@createdApp').then(() =>
-    cy
-      .postApiRequest<IResourceDto>(
-        '/resource/create-resource',
-        createResourceData,
-      )
-      .as('createdResource'),
-  )
-
-  cy.get('@createdResource')
-    .then(() =>
-      cy.postApiRequest<ICodeActionDto>(
-        '/action/create-action',
-        preRenderCodeActionCreateData(page),
-      ),
-    )
-    .as('createdPreRenderCodeAction')
-
-  cy.get('@createdPreRenderCodeAction')
-    .then(() =>
-      cy.postApiRequest<IApiActionDto>(
-        '/action/create-action',
-        preRenderApiActionCreateData(page),
-      ),
-    )
-    .as('createdPreRenderApiAction')
-
-  cy.get('@createdPreRenderApiAction')
-    .then(() =>
-      cy.postApiRequest<ICodeActionDto>(
-        '/action/create-action',
-        postRenderCodeActionCreateData(page),
-      ),
-    )
-    .as('createdPostRenderCodeAction')
-
-  cy.get('@createdPostRenderCodeAction')
-    .then(() =>
-      cy.postApiRequest<IApiActionDto>(
-        '/action/create-action',
-        postRenderApiActionCreateData(page),
-      ),
-    )
-    .as('createdPostRenderApiAction')
-
-  cy.get('@createdPostRenderApiAction').then(() =>
-    cy.visit(
-      `/apps/cypress/${slugify(app.name)}/pages/${slugify(
-        IPageKindName.Provider,
-      )}/builder`,
-    ),
-  )
-}
+import { setupTest } from './pre-post-api-action.setup'
 
 describe('Running API actions that updates state via element pre-render and post-render', () => {
+  let app: IAppDto
+  let page: IPageDto
+
   before(() => {
-    setupTest()
+    setupTest(app, page)
   })
 
   it('should add the state variables where the api responses will be stored to', () => {
