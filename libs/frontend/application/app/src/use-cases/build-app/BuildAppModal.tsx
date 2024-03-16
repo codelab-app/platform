8cef825d0d6b825e7ee74fd0b7500a118ea676e2
Vladyslav Polishchuk <gm.vpolishchuk@gmail.com>
2024-03-16 10:33:41 +0100

    fix: error and success handling when app is built

--- libs/frontend/application/app/src/use-cases/build-app/BuildAppModal.tsx
+++ libs/frontend/application/app/src/use-cases/build-app/BuildAppModal.tsx
@@ -2,7 +2,10 @@ import { MODEL_ACTION } from '@codelab/frontend/abstract/types'
 import { useStore } from '@codelab/frontend/application/shared/store'
 import { regeneratePages } from '@codelab/frontend/domain/domain'
 import { emptyJsonSchema, ModalForm } from '@codelab/frontend/presentation/view'
-import { createFormErrorNotificationHandler } from '@codelab/frontend/shared/utils'
+import {
+  useErrorNotify,
+  useSuccessNotify,
+} from '@codelab/frontend/shared/utils'
 import { observer } from 'mobx-react-lite'
 import React from 'react'
 import { AutoFields } from 'uniforms-antd'
@@ -11,21 +14,48 @@ export const BuildAppModal = observer(() => {
   const { appService, domainService } = useStore()
   const app = appService.buildModal.app
 
+  const successNotify = useSuccessNotify({
+    description: 'It might take up to an hour for the changes to be visible',
+    title: 'App was successfully rebuilt',
+  })
+
+  const errorNotify = useErrorNotify({
+    description: (error: Error) => error.message,
+    title: 'Error while rebuilding app',
+  })
+
   const onSubmit = async () => {
-    if (app) {
-      const domain = domainService.domainsList.find(
-        (_domain) => _domain.app.id === app.id,
-      )
+    if (!app) {
+      return
+    }
 
+    const domains = domainService.domainsList.filter(
+      (_domain) => _domain.app.id === app.id,
+    )
+
+    for (const domain of domains) {
       const pages = app.pages.map((page) => page.url)
+      const response = await regeneratePages(pages, domain.name)
 
-      if (domain) {
-        await regeneratePages(pages, domain.name)
+      if (!response.ok) {
+        const error = await response.text()
+
+        throw new Error(error)
       }
     }
   }
 
   const closeModal = () => appService.buildModal.close()
+
+  const onSubmitError = (error: unknown) => {
+    errorNotify(error as Error)
+    successNotify()
+  }
+
+  const onSubmitSuccess = () => {
+    closeModal()
+    successNotify()
+  }
 
   return (
     <ModalForm.Modal
@@ -36,10 +66,8 @@ export const BuildAppModal = observer(() => {
       <ModalForm.Form
         model={{}}
         onSubmit={onSubmit}
-        onSubmitError={createFormErrorNotificationHandler({
-          title: 'Error while rebuilding app',
-        })}
-        onSubmitSuccess={closeModal}
+        onSubmitError={onSubmitError}
+        onSubmitSuccess={onSubmitSuccess}
         schema={emptyJsonSchema}
         uiKey={MODEL_ACTION.BuildApp.key}
       >
