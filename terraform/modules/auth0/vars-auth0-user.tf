ced4851510e05a89a3c5154d6dcdf7cb190ce8ff
Webber Wang <webber@codelab.app>
2024-03-29 13:13:24 +0800

    ci: adding context for prod

--- terraform/modules/auth0/vars-auth0-user.tf
+++
@@ -1,9 +0,0 @@
-variable "auth0_cypress_username" {
-  type        = string
-  description = "Cypress username for E2e specs"
-}
-
-variable "auth0_cypress_password" {
-  type        = string
-  description = "Cypress user for Auth0, used in e2e testing"
-}
