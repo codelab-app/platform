c74931f8a5b91762b852d06b1efaa90b1bdfab76
assim habeek <assimhabe@gmail.com>
2024-04-08 22:47:29 +0100

    fix: correct comment

--- libs/frontend/abstract/application/src/builder/tree.interface.ts
+++ libs/frontend/abstract/application/src/builder/tree.interface.ts
@@ -8,12 +8,12 @@ export enum IRuntimeNodeType {
 
 export interface IElementTreeViewDataNode {
   children: Array<IElementTreeViewDataNode>
-  // use store ids instead of models because modes gets detached/deleted which causes issues
+  // use ids instead of models because models gets detached/deleted which causes issues
   component?: IRef
-  // use store ids instead of models because modes gets detached/deleted which causes issues
+  // use ids instead of models because models gets detached/deleted which causes issues
   element?: IRef
   isChildMapperComponentInstance?: boolean
-  // use store ids instead of models because modes gets detached/deleted which causes issues
+  // use ids instead of models because models gets detached/deleted which causes issues
   key: string
   primaryTitle?: string
   rootKey: string | null
