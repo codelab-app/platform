8f9da23d3c9797abe6906f30dfc8694505380581
Webber Wang <webber@codelab.app>
2024-04-27 16:30:14 +0800

    ci: deploy api with http

--- scripts/docker/prod/build-web.sh
+++ scripts/docker/prod/build-web.sh
@@ -20,4 +20,4 @@ docker build -f ${PWD}/.docker/prod/web.Dockerfile -t registry.digitalocean.com/
   --build-arg AUTH0_DOMAIN=${AUTH0_DOMAIN} \
   --build-arg AUTH0_CLIENT_ID=${AUTH0_CLIENT_ID} \
   --build-arg AUTH0_CLIENT_SECRET=${AUTH0_CLIENT_SECRET} \
-  --no-cache=$NO_CACHE_FILTER .
+  --no-cache-filter=$NO_CACHE_FILTER .
