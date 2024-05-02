c57c2c3f43b2a0709b1ed5f7a01d21673382fcb2
Vladyslav Polishchuk <gm.vpolishchuk@gmail.com>
2024-05-02 09:10:35 +0200

    fix: theme prop was lost for some components

--- libs/frontend/shared/utils/src/dynamic-loader.ts
+++ libs/frontend/shared/utils/src/dynamic-loader.ts
@@ -2,6 +2,19 @@ import type { LoaderComponent } from 'next/dynamic'
 import dynamic from 'next/dynamic'
 import React from 'react'
 
+interface ForwardedComponentProps extends Record<string, unknown> {
+  // Some of the AntD components (Menu, Sider) have prop with name "theme",
+  // but it overlaps the default "theme" prop of the styled-component.
+  // So when Menu or Sider is wrapped with `styled` component - "theme" prop gets lost.
+  // There is a bunch of issue created for that, and the only workaround for now
+  // is to pass the prop down with different name. See issues for more details:
+  // https://github.com/styled-components/styled-components/issues/4182
+  // https://github.com/styled-components/styled-components/issues/4280
+  // https://github.com/styled-components/styled-components/issues/4267
+  antdTheme?: string
+  forwarded: React.ForwardedRef<unknown>
+}
+
 /**
  * a workaround for : https://github.com/vercel/next.js/issues/4957
  */
@@ -14,10 +27,16 @@ export const dynamicLoader = (loadingFn: () => LoaderComponent<any>) => {
       const Component = 'default' in result ? result.default : result
 
       const ForwardedComponent = ({
+        antdTheme,
         forwarded,
         ...rest
-      }: object & { forwarded: React.ForwardedRef<unknown> }) =>
-        React.createElement(Component, { ...rest, ref: forwarded })
+      }: ForwardedComponentProps) =>
+        React.createElement(Component, {
+          ...rest,
+          ref: forwarded,
+          // need to rename prop back to original name, please see comment above
+          theme: antdTheme,
+        })
 
       ForwardedComponent.displayName = 'ForwardedComponent'
 
