f0144310cf7348b5102d7e774d8ff67c12e14b3e
Webber Wang <webber@codelab.app>
2024-06-04 18:48:22 +0800

    wip: adding codegen for react query

--- libs/frontend/application/app/src/use-cases/build-app/BuildAppModal.tsx
+++ libs/frontend/application/app/src/use-cases/build-app/BuildAppModal.tsx
@@ -1,35 +1,41 @@
 'use client'
 
+import type { IAppModel } from '@codelab/frontend/abstract/domain'
+import { MODEL_ACTION } from '@codelab/frontend/abstract/types'
+import { useRegeneratePages } from '@codelab/frontend-application-domain/services'
+import { useStore } from '@codelab/frontend-application-shared-store/provider'
+import {
+  emptyJsonSchema,
+  ModalForm,
+} from '@codelab/frontend-presentation-view/components'
 import { observer } from 'mobx-react-lite'
 import React from 'react'
+import { AutoFields } from 'uniforms-antd'
 
 export const BuildAppModal = observer(() => {
-  // const store = useStore()
+  const store = useStore()
+  const { appService } = useStore()
+  const app = appService.buildModal.app
+  const { regenerate } = useRegeneratePages(appService)
+  const onSubmit = async () => regenerate(app as IAppModel)
+  const closeModal = () => appService.buildModal.close()
 
-  // console.log(store)
-  // const { appService } = useStore()
-  // const app = appService.buildModal.app
-  // const { regenerate } = useRegeneratePages(appService)
-  // const onSubmit = async () => regenerate(app as IAppModel)
-  // const closeModal = () => appService.buildModal.close()
-
-  // return (
-  //   <ModalForm.Modal
-  //     okText="Build App"
-  //     onCancel={closeModal}
-  //     open={appService.buildModal.isOpen}
-  //   >
-  //     <ModalForm.Form
-  //       model={{}}
-  //       onSubmit={onSubmit}
-  //       onSubmitSuccess={closeModal}
-  //       schema={emptyJsonSchema}
-  //       uiKey={MODEL_ACTION.BuildApp.key}
-  //     >
-  //       <h4>Are you sure you want to build all pages for "{app?.name}"?</h4>
-  //       <AutoFields />
-  //     </ModalForm.Form>
-  //   </ModalForm.Modal>
-  // )
-  return <div>Hi</div>
+  return (
+    <ModalForm.Modal
+      okText="Build App"
+      onCancel={closeModal}
+      open={appService.buildModal.isOpen}
+    >
+      <ModalForm.Form
+        model={{}}
+        onSubmit={onSubmit}
+        onSubmitSuccess={closeModal}
+        schema={emptyJsonSchema}
+        uiKey={MODEL_ACTION.BuildApp.key}
+      >
+        <h4>Are you sure you want to build all pages for "{app?.name}"?</h4>
+        <AutoFields />
+      </ModalForm.Form>
+    </ModalForm.Modal>
+  )
 })
