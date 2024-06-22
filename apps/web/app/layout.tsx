a764de3d4777871431bfef60382ce0709674ecb9
assim habeek <assimhabe@gmail.com>
2024-06-22 17:22:51 +0100

    refactor: remove duplicated providers

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -2,31 +2,38 @@
 
 import '../styles/global.css'
 import { UserProvider } from '@auth0/nextjs-auth0/client'
+import { createDomainStore } from '@codelab/frontend/infra/mobx'
 import { CuiProvider } from '@codelab/frontend/presentation/codelab-ui'
+import { StoreProvider } from '@codelab/frontend-application-shared-store/provider'
 import { App, ConfigProvider } from 'antd'
-import React, { Suspense } from 'react'
-import { CuiStoreProvider } from './components/provider'
-import { ReactQueryProvider } from './components/react-query.provider'
+import { registerRootStore } from 'mobx-keystone'
+import React, { Suspense, useState } from 'react'
 import { StyledComponentsRegistry } from './registry'
 import { theme } from './theme'
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
+  const [store] = useState(() => {
+    const coreStore = createDomainStore()
+
+    registerRootStore(coreStore)
+
+    return coreStore
+  })
+
   return (
     <html lang="en">
       <body>
         <Suspense>
           <StyledComponentsRegistry>
-            {/* <ReactQueryProvider> */}
-            {/* <CuiStoreProvider> */}
-            <UserProvider>
-              <CuiProvider>
-                <ConfigProvider theme={theme}>
-                  <App className="size-full">{children}</App>
-                </ConfigProvider>
-              </CuiProvider>
-            </UserProvider>
-            {/* </CuiStoreProvider> */}
-            {/* </ReactQueryProvider> */}
+            <StoreProvider value={store}>
+              <UserProvider>
+                <CuiProvider>
+                  <ConfigProvider theme={theme}>
+                    <App className="size-full">{children}</App>
+                  </ConfigProvider>
+                </CuiProvider>
+              </UserProvider>
+            </StoreProvider>
           </StyledComponentsRegistry>
         </Suspense>
       </body>
