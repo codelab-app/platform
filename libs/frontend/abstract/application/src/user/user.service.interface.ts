34a69057b2c92d4bc1d97e2f2f8db24ccf09b4b9
assim habeek <assimhabe@gmail.com>
2024-06-24 16:12:37 +0100

    fix: buid web project

--- libs/frontend/abstract/application/src/user/user.service.interface.ts
+++ libs/frontend/abstract/application/src/user/user.service.interface.ts
@@ -1,3 +1,7 @@
+import type {
+  IUserDomainService,
+  IUserModel,
+} from '@codelab/frontend/abstract/domain'
 import type { Auth0IdToken } from '@codelab/shared/abstract/core'
 import type { Nullish } from '@codelab/shared/abstract/types'
 import type { AxiosResponse } from 'axios'
@@ -13,18 +17,18 @@ export interface IUserPreference extends Record<string, unknown> {
   explorerExpandedNodes?: Record<string, Array<string>>
 }
 export interface IUserService {
-  // preferences: IUserPreference
-  // user: IUserModel
-  // userDomainService: IUserDomainService
+  preferences: IUserPreference
+  user: IUserModel
+  userDomainService: IUserDomainService
 
   saveUser(data: Auth0IdToken): Promise<AxiosResponse>
-  // setElementTreeExpandedKeys(
-  //   containerId: string,
-  //   expandedKeys: Array<string>,
-  // ): void
-  // setSelectedBuilderBreakpoint(
-  //   containerId: string,
-  //   breakpoint: BuilderWidthBreakPoint,
-  // ): void
-  // setSelectedBuilderWidth(containerId: string, width: BuilderWidth): void
+  setElementTreeExpandedKeys(
+    containerId: string,
+    expandedKeys: Array<string>,
+  ): void
+  setSelectedBuilderBreakpoint(
+    containerId: string,
+    breakpoint: BuilderWidthBreakPoint,
+  ): void
+  setSelectedBuilderWidth(containerId: string, width: BuilderWidth): void
 }
