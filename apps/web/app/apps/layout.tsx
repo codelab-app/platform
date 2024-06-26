5da3e1847dac68f30444aafc2f154416e3077a0d
assim habeek <assimhabe@gmail.com>
2024-06-26 06:34:10 +0100

    fix: passing user to layout

--- apps/web/app/apps/layout.tsx
+++ apps/web/app/apps/layout.tsx
@@ -1,19 +1,16 @@
 import { AntdRegistry } from '@ant-design/nextjs-registry'
 import { StoreProvider } from '@codelab/frontend-application-shared-store/provider'
-import type { IUserDto } from '@codelab/shared/abstract/core'
+import { getServerUser } from '@codelab/frontend-application-user/use-cases/server-user'
 import { App, ConfigProvider } from 'antd'
+import type { PropsWithChildren } from 'react'
 import React from 'react'
 import { CuiProvider } from '../providers/CuiProvider'
 import { StyledComponentsRegistry } from '../registry'
 import { theme } from '../theme'
 
-const Layout = ({
-  children,
-  user,
-}: {
-  user: IUserDto
-  children: React.ReactNode
-}) => {
+const Layout = async ({ children }: PropsWithChildren) => {
+  const user = await getServerUser()
+
   return (
     <StyledComponentsRegistry>
       <CuiProvider>
