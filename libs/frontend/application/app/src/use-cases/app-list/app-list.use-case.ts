44b6240873eef59c75ff3c492760c550a5b3a186
assim habeek <33001357+assimhabeek@users.noreply.github.com>
2024-07-16 08:21:42 +0100

    refactor: replace axios with fetch (#3419)

--- libs/frontend/application/app/src/use-cases/app-list/app-list.use-case.ts
+++ libs/frontend/application/app/src/use-cases/app-list/app-list.use-case.ts
@@ -1,3 +1,5 @@
+'use server'
+
 import { CACHE_TAGS } from '@codelab/frontend/abstract/domain'
 import { getServerUser } from '@codelab/frontend-application-user/use-cases/server-user'
 import { revalidateTag } from 'next/cache'
