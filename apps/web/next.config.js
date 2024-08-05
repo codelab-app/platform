2434a8f3f516acae2ec44b6710aab26a92d84bea
Webber Wang <webber@codelab.app>
2024-08-05 15:21:03 +0800

    fix: form stopped working

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -79,7 +79,7 @@ const nextConfig = {
   nx: { svgr: true },
   // disable to support uniforms.
   // https://github.com/vazco/uniforms/issues/1194
-  // reactStrictMode: false,
+  reactStrictMode: false,
   rewrites: async () => ({
     afterFiles: [
       {
---
+++ libs/frontend/application/app/src/use-cases/create-app/TestForm.tsx
@@ -0,0 +1,53 @@
+'use client'
+
+import type { JSONSchemaType } from 'ajv'
+import Ajv from 'ajv'
+import React from 'react'
+import { AutoForm } from 'uniforms-antd'
+import { JSONSchemaBridge } from 'uniforms-bridge-json-schema'
+
+interface FormData {
+  firstName: string
+  lastName: string
+  workExperience: number
+}
+
+const schema: JSONSchemaType<FormData> = {
+  properties: {
+    firstName: { type: 'string' },
+    lastName: { type: 'string' },
+    workExperience: {
+      description: 'Work experience in years',
+      maximum: 100,
+      minimum: 0,
+      type: 'integer',
+    },
+  },
+  required: [],
+  // required: ['firstName', 'lastName'],
+  title: 'Guest',
+  type: 'object',
+}
+
+const ajv = new Ajv({
+  allErrors: true,
+  keywords: ['uniforms'],
+  useDefaults: true,
+})
+
+const createValidator = <T,>(_schema: JSONSchemaType<T>) => {
+  const validator = ajv.compile(_schema)
+
+  return (model: Record<string, unknown>) => {
+    validator(model)
+
+    return validator.errors?.length ? { details: validator.errors } : null
+  }
+}
+
+const schemaValidator = createValidator(schema)
+const bridge = new JSONSchemaBridge(schema, schemaValidator)
+
+export const GuestFormBasic = () => {
+  return <AutoForm onSubmit={console.log} schema={bridge} />
+}
