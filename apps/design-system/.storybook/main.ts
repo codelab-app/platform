4757349aaa1eed30bb84ac670022cb3fb7cf31cb
Mohammed S. Yaseen <51242349+mohasarc@users.noreply.github.com>
2024-08-23 09:45:19 +0300

    feat: add design system storybook app

---
+++ apps/design-system/.storybook/main.ts
@@ -0,0 +1,23 @@
+import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin'
+import type { StorybookConfig } from '@storybook/react-vite'
+import { mergeConfig } from 'vite'
+
+const storybookConfig: StorybookConfig = {
+  addons: ['@storybook/addon-essentials', '@storybook/addon-interactions'],
+  framework: {
+    name: '@storybook/react-vite',
+    options: {},
+  },
+  stories: ['../../../libs/frontend/cui/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
+
+  viteFinal: async (config) =>
+    mergeConfig(config, {
+      plugins: [nxViteTsPaths()],
+    }),
+}
+
+export default storybookConfig
+
+// To customize your Vite configuration you can use the viteFinal field.
+// Check https://storybook.js.org/docs/react/builders/vite#configuration
+// and https://nx.dev/recipes/storybook/custom-builder-configs
---
+++ apps/design-system/README.md
@@ -0,0 +1,7 @@
+# design-system
+
+This library is used to group all design system related documentation within a single storybook instance.
+
+## To run
+
+Run `nx storybook design-system`.
