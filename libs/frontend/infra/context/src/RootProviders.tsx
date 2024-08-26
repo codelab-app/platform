0a7cf1adf937f6f4add62d52f770357c333c744a
Webber Wang <webber@codelab.app>
2024-08-26 12:40:38 +0800

    fix: domain store provider

--- libs/frontend/infra/context/src/RootProviders.tsx
+++ libs/frontend/infra/context/src/RootProviders.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import { AntdRegistry } from '@ant-design/nextjs-registry'
 import { UserProvider } from '@auth0/nextjs-auth0/client'
 import { CuiProvider } from '@codelab/frontend/presentation/codelab-ui'
 import {
@@ -8,13 +7,10 @@ import {
   DomainStoreProvider,
 } from '@codelab/frontend-infra-mobx/store'
 import type { IUserDto } from '@codelab/shared/abstract/core'
-import App from 'antd/lib/app'
-import ConfigProvider from 'antd/lib/config-provider'
 import { Provider } from 'jotai'
-import type { PropsWithChildren, ReactNode } from 'react'
+import type { PropsWithChildren } from 'react'
 import React from 'react'
-import { StyledComponentsRegistry } from './StyledComponentsRegistry'
-import { theme } from './theme'
+import { StyleProviders } from './StyleProviders'
 
 export const RootProviders = ({
   children,
@@ -24,9 +20,9 @@ export const RootProviders = ({
     <UserProvider>
       <CuiProvider>
         <Provider>
-          <ApplicationStoreProvider>
-            <DomainStoreProvider user={user}>{children}</DomainStoreProvider>
-          </ApplicationStoreProvider>
+          <DomainStoreProvider user={user}>
+            <ApplicationStoreProvider>{children}</ApplicationStoreProvider>
+          </DomainStoreProvider>
         </Provider>
       </CuiProvider>
     </UserProvider>
