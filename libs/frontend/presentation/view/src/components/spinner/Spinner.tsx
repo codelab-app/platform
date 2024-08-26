158311dcf6022d207b4d714accc79cd9b252c4fc
Webber Wang <webber@codelab.app>
2024-08-26 14:36:06 +0800

    e2e: optimize timeout

--- libs/frontend/presentation/view/src/components/spinner/Spinner.tsx
+++ libs/frontend/presentation/view/src/components/spinner/Spinner.tsx
@@ -17,9 +17,5 @@ export const Spinner = ({ center, children, isLoading }: SpinnerProps) => {
     style.transform = 'translate(-50%, -50%)'
   }
 
-  return isLoading ? (
-    <Spin aria-label="Loading" role="status" style={style} />
-  ) : (
-    children
-  )
+  return isLoading ? <Spin aria-label="Loading" style={style} /> : children
 }
