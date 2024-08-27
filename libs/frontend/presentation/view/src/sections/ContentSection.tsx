abec22887b484e8ff268ee2ab722fb1d3d71a7d0
assim habeek <assimhabe@gmail.com>
2024-08-27 15:22:41 +0100

    fix: remove section overflow

--- libs/frontend/presentation/view/src/sections/ContentSection.tsx
+++ libs/frontend/presentation/view/src/sections/ContentSection.tsx
@@ -1,15 +1,10 @@
 import type { PropsWithChildren } from 'react'
 import React from 'react'
-import { padding } from '../style'
 
 export const ContentSection = ({
   children,
 }: PropsWithChildren<
   React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>
 >) => {
-  return (
-    <section className="size-full" style={{ marginTop: padding.sm }}>
-      {children}
-    </section>
-  )
+  return <section className="size-full">{children}</section>
 }
