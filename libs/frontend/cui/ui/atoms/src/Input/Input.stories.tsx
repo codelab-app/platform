6dd684f9f74b46d118cbdbb65b7d66eee3c56403
Mohammed S. Yaseen <51242349+mohasarc@users.noreply.github.com>
2024-08-28 14:40:18 +0300

    feat: add stories for card input and select

---
+++ libs/frontend/cui/ui/atoms/src/Input/Input.stories.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Input } from './Input'
+
+export default {
+  component: Input,
+  title: 'Atoms/Input',
+}
+
+export const Default = {
+  args: {},
+  render: () => <Input placeholder="Email" type="email" />,
+}
