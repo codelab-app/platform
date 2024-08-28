debb652be17359e5d0165769794f2bf03cf507b3
Mohammed S. Yaseen <51242349+mohasarc@users.noreply.github.com>
2024-08-28 15:01:10 +0300

    feat: add stories for checkbox and collapsible

---
+++ libs/frontend/cui/ui/atoms/src/Checkbox/Checkbox.stories.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Checkbox } from './Checkbox'
+
+export default {
+  component: Checkbox,
+  title: 'Atoms/Checkbox',
+}
+
+export const Default = {
+  args: {},
+  render: () => (
+    <div className="flex items-center space-x-2">
+      <Checkbox id="terms" />
+      <label
+        className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+        htmlFor="terms"
+      >
+        Accept terms and conditions
+      </label>
+    </div>
+  ),
+}
