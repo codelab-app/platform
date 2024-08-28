b8ccb0a0f70c66aa8a66dd3acdb9e4c1bb51a34f
Mohammed S. Yaseen <51242349+mohasarc@users.noreply.github.com>
2024-08-28 13:57:46 +0300

    feat: add badge stories

---
+++ libs/frontend/cui/ui/atoms/src/Badge/Badge.stories.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Badge } from './Badge'
+
+export default {
+  component: Badge,
+  title: 'Atoms/Badge',
+}
+
+export const Default = {
+  args: {},
+  render: () => <Badge>Badge</Badge>,
+}
