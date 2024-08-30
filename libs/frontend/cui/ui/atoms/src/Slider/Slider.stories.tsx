d6fc352da2a2739d14d4cf63940e5d1d6426ecb0
Mohammed S. Yaseen <51242349+mohasarc@users.noreply.github.com>
2024-08-30 10:37:46 +0300

    feat: add Slider stories

---
+++ libs/frontend/cui/ui/atoms/src/Slider/Slider.stories.tsx
@@ -0,0 +1,25 @@
+import { cn } from '@cui/utils'
+import React from 'react'
+import { Slider } from './Slider'
+
+export default {
+  component: Slider,
+  title: 'Atoms/Slider',
+}
+
+type SliderProps = React.ComponentProps<typeof Slider>
+
+export const Default = {
+  args: {},
+  render: ({ className, ...props }: SliderProps) => {
+    return (
+      <Slider
+        className={cn('w-[60%]', className)}
+        defaultValue={[50]}
+        max={100}
+        step={1}
+        {...props}
+      />
+    )
+  },
+}
