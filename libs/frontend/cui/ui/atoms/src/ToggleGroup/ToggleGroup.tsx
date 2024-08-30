411d74e02016d33ccbdc1f762f35b44be57a3d77
Mohammed S. Yaseen <51242349+mohasarc@users.noreply.github.com>
2024-08-30 11:20:39 +0300

    feat: add ToggleGroup stories

--- libs/frontend/cui/ui/atoms/src/ToggleGroup/ToggleGroup.tsx
+++ libs/frontend/cui/ui/atoms/src/ToggleGroup/ToggleGroup.tsx
@@ -4,7 +4,7 @@ import { cn } from '@cui/utils'
 import * as ToggleGroupPrimitive from '@radix-ui/react-toggle-group'
 import type { VariantProps } from 'class-variance-authority'
 import React, { createContext, forwardRef } from 'react'
-import { toggleVariants } from './Toggle'
+import { toggleVariants } from '../Toggle'
 
 const ToggleGroupContext = createContext<VariantProps<typeof toggleVariants>>({
   size: 'default',
