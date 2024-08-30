46c5038190dff429ecadb4e78cc2bb57c39095c8
Mohammed S. Yaseen <51242349+mohasarc@users.noreply.github.com>
2024-08-30 11:11:01 +0300

    feat: add Textarea stories

---
+++ libs/frontend/cui/ui/atoms/src/Textarea/Textarea.stories.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Textarea } from './Textarea'
+
+export default {
+  component: Textarea,
+  title: 'Atoms/Textarea',
+}
+
+export const Default = {
+  args: {},
+  render: () => <Textarea placeholder="Type your message here." />,
+}
