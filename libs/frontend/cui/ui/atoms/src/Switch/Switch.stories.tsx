33081e9645d7ebd1c0ae576a6c2db8b4f4619b9d
Mohammed S. Yaseen <51242349+mohasarc@users.noreply.github.com>
2024-08-30 10:46:51 +0300

    feat: add Switch stories

---
+++ libs/frontend/cui/ui/atoms/src/Switch/Switch.stories.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Label } from '../Label'
+import { Switch } from './Switch'
+
+export default {
+  component: Switch,
+  title: 'Atoms/Switch',
+}
+
+export const Default = {
+  args: {},
+  render: () => (
+    <div className="flex items-center space-x-2">
+      <Switch id="airplane-mode" />
+      <Label htmlFor="airplane-mode">Airplane Mode</Label>
+    </div>
+  ),
+}
