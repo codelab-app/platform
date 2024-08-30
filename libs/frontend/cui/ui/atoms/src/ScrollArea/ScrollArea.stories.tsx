b8f307410bc32dc2227a78da82a5a6169a8905c6
Mohammed S. Yaseen <51242349+mohasarc@users.noreply.github.com>
2024-08-30 10:25:31 +0300

    feat: add Separator stories

--- libs/frontend/cui/ui/atoms/src/ScrollArea/ScrollArea.stories.tsx
+++ libs/frontend/cui/ui/atoms/src/ScrollArea/ScrollArea.stories.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Separator } from '../Separator'
+import { Separator } from '../Separator/Separator'
 import { ScrollArea } from './ScrollArea'
 
 export default {
