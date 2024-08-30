1a492eed3da9b5b0c3c0b716024b06334f1e74fb
Mohammed S. Yaseen <51242349+mohasarc@users.noreply.github.com>
2024-08-30 10:33:09 +0300

    feat: add Skeleton stories

---
+++ libs/frontend/cui/ui/atoms/src/Skeleton/Skeleton.stories.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Skeleton } from './Skeleton'
+
+export default {
+  component: Skeleton,
+  title: 'Atoms/Skeleton',
+}
+
+export const Default = {
+  args: {},
+  render: () => (
+    <div className="flex items-center space-x-4">
+      <Skeleton className="size-12 rounded-full" />
+      <div className="space-y-2">
+        <Skeleton className="h-4 w-[250px]" />
+        <Skeleton className="h-4 w-[200px]" />
+      </div>
+    </div>
+  ),
+}
