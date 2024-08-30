bd05480358235c65f49acc764a180f6d8f68cf43
Mohammed S. Yaseen <51242349+mohasarc@users.noreply.github.com>
2024-08-30 11:27:38 +0300

    feat: add Tooltip stories

---
+++ libs/frontend/cui/ui/atoms/src/Tooltip/Tooltip.stories.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Button } from '../Button'
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from './Tooltip'
+
+export default {
+  component: Tooltip,
+  title: 'Atoms/Tooltip',
+}
+
+export const Default = {
+  args: {},
+  render: () => (
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <Button variant="outline">Hover</Button>
+        </TooltipTrigger>
+        <TooltipContent>
+          <p>Add to library</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  ),
+}
