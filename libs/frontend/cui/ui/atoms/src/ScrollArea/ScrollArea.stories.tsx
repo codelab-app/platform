56021224bbf282baa3f493cac47a250fa4e3c9b3
Mohammed S. Yaseen <51242349+mohasarc@users.noreply.github.com>
2024-08-30 10:21:02 +0300

    feat: add ScrollArea stories

---
+++ libs/frontend/cui/ui/atoms/src/ScrollArea/ScrollArea.stories.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Separator } from '../Separator'
+import { ScrollArea } from './ScrollArea'
+
+export default {
+  component: ScrollArea,
+  title: 'Atoms/ScrollArea',
+}
+
+const tags = Array.from({ length: 50 }).map(
+  (_, i, a) => `v1.2.0-beta.${a.length - i}`,
+)
+
+export const Default = {
+  args: {},
+  render: () => (
+    <ScrollArea className="h-72 w-48 rounded-md border">
+      <div className="p-4">
+        <h4 className="mb-4 text-sm font-medium leading-none">Tags</h4>
+        {tags.map((tag) => (
+          <>
+            <div className="text-sm" key={tag}>
+              {tag}
+            </div>
+            <Separator className="my-2" />
+          </>
+        ))}
+      </div>
+    </ScrollArea>
+  ),
+}
