06549d5a45883dd345e6c3bf2c24d9ffbdf01b9c
Vladyslav Polishchuk <gm.vpolishchuk@gmail.com>
2024-09-09 06:52:55 +0200

    feat: add 404 error boundary

---
+++ apps/web/app/not-found.tsx
@@ -0,0 +1,24 @@
+'use client'
+
+import { Button, Result } from 'antd'
+import { useRouter } from 'next/navigation'
+import React from 'react'
+
+const NotFound = () => {
+  const router = useRouter()
+
+  return (
+    <Result
+      extra={
+        <Button onClick={() => router.push('/apps')} type="primary">
+          Back Home
+        </Button>
+      }
+      status="404"
+      subTitle="Sorry, the page you visited does not exist."
+      title="404"
+    />
+  )
+}
+
+export default NotFound
