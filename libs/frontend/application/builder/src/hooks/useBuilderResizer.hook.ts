3392200623c013ead03334549f8e0e35564a4920
assim habeek <assimhabe@gmail.com>
2024-09-10 12:02:48 +0100

    fix: builder resizer

---
+++ libs/frontend/application/builder/src/hooks/useBuilderResizer.hook.ts
@@ -0,0 +1,55 @@
+import { usePreferenceService } from '@codelab/frontend-application-preference/services'
+import { useDomainStore } from '@codelab/frontend-infra-mobx/context'
+import type { PointerEvent } from 'react'
+import { useState } from 'react'
+
+export type ResizeHandlerSide = 'after' | 'before'
+
+export const useBuilderResizer = (side: ResizeHandlerSide) => {
+  const [pointerDown, setPointerDown] = useState<boolean>(false)
+  const { preferenceDomainService } = useDomainStore()
+  const { update } = usePreferenceService()
+  const preference = preferenceDomainService.preference
+  const breakpoint = preference.builderBreakpoint
+
+  const onPointerDown = ({
+    currentTarget,
+    pointerId,
+  }: PointerEvent<HTMLElement>) => {
+    // listen to pointer move event
+    currentTarget.setPointerCapture(pointerId)
+    setPointerDown(true)
+  }
+
+  const onPointerMove = ({ movementX }: PointerEvent) => {
+    if (!pointerDown) {
+      return
+    }
+
+    // The builder is centered, so we have to move twice as fast
+    // because the width changes from both sides
+    const difference = side === 'after' ? movementX * 2 : -movementX * 2
+    // this newWidth may be ignored if it exceed limits
+    const newWidth = preference.builderWidth + difference
+
+    const appliedWidth = Math.max(
+      breakpoint.min,
+      Math.min(newWidth, breakpoint.max),
+    )
+
+    preference.writeCache({ builderWidth: appliedWidth })
+  }
+
+  const onPointerUp = ({
+    currentTarget,
+    pointerId,
+  }: PointerEvent<HTMLElement>) => {
+    setPointerDown(false)
+    // persist changes to database
+    void update(preference)
+
+    currentTarget.releasePointerCapture(pointerId)
+  }
+
+  return { onPointerDown, onPointerMove, onPointerUp }
+}
--- libs/frontend/application/builder/src/use-cases/base-builder/BuilderResizeHandle.tsx
+++ libs/frontend/application/builder/src/use-cases/base-builder/BuilderResizeHandle.tsx
@@ -1,102 +1,67 @@
-import { preferenceRepository } from '@codelab/frontend-domain-preference/repositories'
-import { useDomainStore } from '@codelab/frontend-infra-mobx/context'
 import { observer } from 'mobx-react-lite'
 import type { PropsWithChildren } from 'react'
-import React, { useEffect, useRef, useState } from 'react'
-import { useDebounce } from 'react-use'
-import type { Side } from '../../sections/content/builder-resize-controller'
-import { builderResizeController } from '../../sections/content/builder-resize-controller'
+import React, { useState } from 'react'
+import type { ResizeHandlerSide } from '../../hooks'
+import { useBuilderResizer } from '../../hooks'
 
-const useResizer = ({ side }: { side: Side }) => {
-  const ref = useRef<HTMLDivElement>(null)
-  const { preferenceDomainService } = useDomainStore()
-  const preference = preferenceDomainService.preference
-  const breakpoint = preference.builderBreakpoint
+type ResizeHandlerProps = PropsWithChildren<{
+  side: ResizeHandlerSide
+  hovered: boolean
+  setHovered(hover: boolean): void
+}>
 
-  // debounce api calls
-  useDebounce(
-    () => {
-      // only when drag is stopped update call api
-      void preferenceRepository.update(preference)
-    },
-    800,
-    [preference.builderWidth],
-  )
+const ResizeHandler = observer<ResizeHandlerProps>(
+  ({ children, hovered, setHovered, side }) => {
+    const { onPointerDown, onPointerMove, onPointerUp } =
+      useBuilderResizer(side)
 
-  useEffect(() => {
-    if (ref.current === null) {
-      return
-    }
+    const defaultClassNames = 'h-full w-[3px] bg-gray-200'
 
-    const disposeResizeController = builderResizeController(ref.current, {
-      getDefaultValue: () => preference.builderWidth,
+    let className = hovered
+      ? `${defaultClassNames} bg-blue-400 cursor-col-resize`
+      : defaultClassNames
 
-      getMaxValue: () => breakpoint.max,
+    className = side === 'after' ? `${className} relative` : className
 
-      getMinValue: () => breakpoint.min,
-
-      getSide: () => side,
-
-      onValueChanged: (builderWidth) => {
-        // we want direct state update without debounce
-        preference.writeCache({ builderWidth })
-      },
-    })
-
-    return () => {
-      disposeResizeController()
-    }
-  }, [side, breakpoint])
-
-  return ref
-}
+    return (
+      <div
+        className={className}
+        onMouseLeave={() => {
+          setHovered(false)
+        }}
+        onMouseOver={() => {
+          setHovered(true)
+        }}
+        onPointerDown={onPointerDown}
+        onPointerMove={onPointerMove}
+        onPointerUp={onPointerUp}
+      >
+        {children}
+      </div>
+    )
+  },
+)
 
 export const BuilderResizeHandle = observer<PropsWithChildren>(
   ({ children }) => {
-    const beforeElementRef = useResizer({ side: 'before' })
-    const afterElementRef = useResizer({ side: 'after' })
-    const [hovered, setHovered] = React.useState(false)
-
-    const getClassNames = () => {
-      const defaultClassNames = 'h-full w-[3px] bg-gray-200'
-
-      if (hovered) {
-        return `${defaultClassNames} bg-blue-400 cursor-col-resize`
-      }
-
-      return defaultClassNames
-    }
+    const [hovered, setHovered] = useState(false)
 
     return (
       <>
-        <div
-          className={getClassNames()}
-          onMouseLeave={() => {
-            setHovered(false)
-          }}
-          onMouseOver={() => {
-            setHovered(true)
-          }}
-          ref={beforeElementRef}
+        <ResizeHandler
+          hovered={hovered}
+          setHovered={setHovered}
+          side="before"
         />
         {children}
-        <div
-          className={`${getClassNames()} relative`}
-          onMouseLeave={() => {
-            setHovered(false)
-          }}
-          onMouseOver={() => {
-            setHovered(true)
-          }}
-          ref={afterElementRef}
-        >
+        <ResizeHandler hovered={hovered} setHovered={setHovered} side="after">
           <div className="absolute left-1/2 top-1/2 -translate-y-1/2 bg-inherit">
             <div className="flex space-x-0.5 rounded-r bg-inherit p-1.5 pl-1">
               <div className="h-6 w-0.5 bg-black"></div>
               <div className="h-6 w-0.5 bg-black"></div>
             </div>
           </div>
-        </div>
+        </ResizeHandler>
       </>
     )
   },
