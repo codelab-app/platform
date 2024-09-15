aebc15c1cf4281acdd40f5cf27b8e95ca69bc2c5
Vladyslav Polishchuk <gm.vpolishchuk@gmail.com>
2024-09-15 20:59:48 +0200

    fix: erorr on Tag page

--- apps/web/app/(protected)/tags/@primarySidebar/page.tsx
+++ apps/web/app/(protected)/tags/@primarySidebar/page.tsx
@@ -1,3 +1,23 @@
-import { TagsPrimarySidebar } from '@codelab/frontend-application-tag/views'
+'use client'
 
-export default TagsPrimarySidebar
+import type { UrlQueryParamsString } from '@codelab/frontend/abstract/types'
+import { ApplicationStoreHydrator } from '@codelab/frontend/infra/context'
+import { TagsPrimarySidebar } from '@codelab/frontend-application-tag/views'
+import { Spinner } from '@codelab/frontend-presentation-view/components/spinner'
+
+const TagsPrimarySidebarPage = ({
+  searchParams: { filter, page, pageSize, search },
+}: {
+  searchParams: UrlQueryParamsString
+}) => {
+  return (
+    <ApplicationStoreHydrator
+      fallback={<Spinner />}
+      queryParams={{ filter, page, pageSize, search }}
+    >
+      <TagsPrimarySidebar />
+    </ApplicationStoreHydrator>
+  )
+}
+
+export default TagsPrimarySidebarPage
