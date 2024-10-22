41318f7e221a39a95d6111f3e47aa661ea46e66a
Vladyslav Polishchuk <gm.vpolishchuk@gmail.com>
2024-10-22 07:27:08 +0200

    fix: element and field popovers layout

--- apps/web/app/(protected)/apps/[appId]/pages/[pageId]/builder/@secondaryPopover/create-field/page.tsx
+++ apps/web/app/(protected)/apps/[appId]/pages/[pageId]/builder/@secondaryPopover/create-field/page.tsx
@@ -1,9 +1,14 @@
 'use client'
 
 import { CreateFieldPopover } from '@codelab/frontend-application-type/use-cases/create-field'
+import { DashboardPopover } from '@codelab/frontend-presentation-view/templates'
 
 const Page = () => {
-  return <CreateFieldPopover />
+  return (
+    <DashboardPopover>
+      <CreateFieldPopover />
+    </DashboardPopover>
+  )
 }
 
 export default Page
