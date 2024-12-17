f4e1268c4d3b219c549a6a52037342ae35397a89
Webber Wang <webber@codelab.app>
2024-12-17 10:55:50 +0800

    wip: fix actions

--- libs/shared/domain/module/field/src/api/field.api.ts
+++ libs/shared/domain/module/field/src/api/field.api.ts
@@ -1,7 +1,18 @@
 import { graphqlClient } from '@codelab/shared/infra/gql-client'
 
 import { getSdk } from './field.api.graphql.api.gen'
+import {
+  CreateFields,
+  DeleteFields,
+  GetFields,
+  UpdateFields,
+} from './field.api.graphql.web.gen'
 
 export const fieldApi = () => getSdk(graphqlClient)
 
-export const fieldServerActions = () => import('./field.api.graphql.web.gen')
+export const fieldServerActions = {
+  CreateFields,
+  DeleteFields,
+  GetFields,
+  UpdateFields,
+}
