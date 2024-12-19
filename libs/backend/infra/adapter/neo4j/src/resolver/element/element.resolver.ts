a410067c5eaa0f9872df1b2f05ee166b2e276a5a
Webber Wang <webber@codelab.app>
2024-12-19 14:28:20 +0800

    refactor: implement dependant types resolver for element

--- libs/backend/infra/adapter/neo4j/src/resolver/element/element.resolver.ts
+++ libs/backend/infra/adapter/neo4j/src/resolver/element/element.resolver.ts
@@ -1,5 +1,6 @@
+import type { IRef } from '@codelab/shared/abstract/core'
 import type { Element } from '@codelab/shared/infra/gql'
-import type { IResolvers } from '@graphql-tools/utils'
+import type { IFieldResolver, IResolvers } from '@graphql-tools/utils'
 import type { FactoryProvider } from '@nestjs/common'
 import type { GraphQLRequestContext } from 'graphql-request/build/cjs/types'
 
@@ -9,24 +10,9 @@ import { getElementDescendants } from '../../cypher'
 import { Neo4jService } from '../../infra'
 import { name } from './field/element-name'
 import { slug } from './field/element-slug'
+import { getDependantTypes } from './get-dependant-types'
 
 export const ELEMENT_RESOLVER_PROVIDER = 'ELEMENT_RESOLVER_PROVIDER'
-
-export const descendants =
-  (neo4jService: Neo4jService, elementRepository: ElementRepository) =>
-  (element: Element) => {
-    return neo4jService.withReadTransaction(async (txn) => {
-      const { records } = await txn.run(getElementDescendants, {
-        rootId: element.id,
-      })
-
-      const descendantIds = records[0]?.get(0) || []
-
-      return elementRepository.find({
-        where: { id_IN: [element.id].concat(descendantIds) },
-      })
-    })
-  }
 
 export const ElementResolverProvider: FactoryProvider<
   Promise<IResolvers<GraphQLRequestContext, unknown>>
@@ -37,19 +23,13 @@ export const ElementResolverProvider: FactoryProvider<
     neo4jService: Neo4jService,
     elementRepository: ElementRepository,
   ) => {
+    const dependantTypes: IFieldResolver<IRef, unknown> = (parent) =>
+      getDependantTypes(neo4jService, parent)
+
     return {
       Element: {
         __typename: 'Element',
-        // We only use the OGM resolver, but we set a dummy resolver here to hide the console errors
-        dependantTypes: (node) => {
-          console.warn(
-            'Fetching dependant types field resolver, not yet implemented!',
-            node,
-          )
-
-          return []
-        },
-        descendants: descendants(neo4jService, elementRepository),
+        dependantTypes,
         name,
         // renderType,
         slug,
