1a7ca1c48ca99370faf53ee71acf653428fdf902
assim habeek <assimhabe@gmail.com>
2025-01-24 15:18:46 +0100

    fix: unit specs

--- libs/frontend/application/builder/src/use-cases/base-builder/DeleteButton.tsx
+++ libs/frontend/application/builder/src/use-cases/base-builder/DeleteButton.tsx
@@ -1,31 +1,27 @@
 import type { IElementModel } from '@codelab/frontend/abstract/domain'
 
 import DeleteOutlined from '@ant-design/icons/DeleteOutlined'
-import { useDeleteElementModal } from '@codelab/frontend-application-element/use-cases/delete-element'
+import { DeleteElementPopconfirm } from '@codelab/frontend-application-element/use-cases/delete-element'
 
 export const DeleteButton = ({ element }: { element: IElementModel }) => {
-  const deleteElementModal = useDeleteElementModal()
-
   return (
-    <div
-      className={`
-        flex size-7 cursor-pointer
-        items-center justify-center align-middle
-      `}
-      onClick={(event) => {
-        event.stopPropagation()
-        deleteElementModal.open(element)
-      }}
-    >
+    <DeleteElementPopconfirm element={element} placement="leftBottom">
       <div
         className={`
-          flex size-5 items-center
-          justify-center rounded-full align-middle
+          flex size-7 cursor-pointer
+          items-center justify-center align-middle
         `}
-        style={{ backgroundColor: '#375583', color: 'red' }}
       >
-        <DeleteOutlined />
+        <div
+          className={`
+            flex size-5 items-center
+            justify-center rounded-full align-middle
+          `}
+          style={{ backgroundColor: '#375583', color: 'red' }}
+        >
+          <DeleteOutlined />
+        </div>
       </div>
-    </div>
+    </DeleteElementPopconfirm>
   )
 }
