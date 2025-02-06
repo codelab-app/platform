afc8492adc9372066cfebf41f895129d6cf3aaf0
assim habeek <assimhabe@gmail.com>
2025-02-06 22:12:40 +0100

    wip: fixing unit specs

--- libs/frontend/test/application/jest.mocks.ts
+++ libs/frontend/test/application/jest.mocks.ts
@@ -1,6 +1,16 @@
 // mock server modules that are not supposed to be used in client env
 jest.mock('@codelab/shared/infra/fetch', () => ({
   fetchWithAuth: () => null,
+  gqlRequest: () => null,
+}))
+
+jest.mock('@codelab/shared/infra/fetch-server', () => ({
+  gqlServerRequest: () => null,
+  serverFetchWithAuth: () => null,
+}))
+
+jest.mock('next/cache', () => ({
+  revalidateTag: () => null,
 }))
 
 // mock hooks that are not supported in jsdom env
