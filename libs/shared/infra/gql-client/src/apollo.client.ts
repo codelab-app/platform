23e30a5b5f52535172436061ebd753d8e59f5e0f
Webber Wang <webber@codelab.app>
2025-02-20 16:10:03 +0800

    refactor: add custom agent to graphql client

---
+++ libs/shared/infra/gql-client/src/apollo.client.ts
@@ -0,0 +1,65 @@
+import { ApolloClient, HttpLink, InMemoryCache, split } from '@apollo/client'
+import { GraphQLWsLink } from '@apollo/client/link/subscriptions'
+import { getMainDefinition } from '@apollo/client/utilities'
+import { env } from '@codelab/shared/config/env'
+import { createClient } from 'graphql-ws'
+import WebSocket from 'ws'
+
+enum Environment {
+  Browser = 'browser',
+  Node = 'node',
+}
+
+interface CreateApolloClientOptions {
+  environment?: Environment
+}
+
+export const createApolloClient = ({
+  environment = Environment.Browser,
+}: CreateApolloClientOptions = {}) => {
+  // Move port getter to a function to avoid early evaluation
+  const getApiPort = () =>
+    env.get('NEXT_PUBLIC_API_PORT').required().asPortNumber()
+
+  const httpLink = new HttpLink({
+    uri: `http://127.0.0.1:${getApiPort()}/api/v1/graphql`,
+  })
+
+  const wsLink = new GraphQLWsLink(
+    createClient({
+      url: `ws://127.0.0.1:${getApiPort()}/api/v1/graphql`,
+      webSocketImpl: environment === Environment.Node ? WebSocket : undefined,
+    }),
+  )
+
+  const splitLink = split(
+    ({ query }) => {
+      const definition = getMainDefinition(query)
+
+      return (
+        definition.kind === 'OperationDefinition' &&
+        definition.operation === 'subscription'
+      )
+    },
+    wsLink,
+    httpLink,
+  )
+
+  return new ApolloClient({
+    cache: new InMemoryCache(),
+    link: splitLink,
+  })
+}
+
+/**
+ * Lazy load so we create the client when the environment is known
+ */
+export const nodeApolloClient = () =>
+  createApolloClient({
+    environment: Environment.Node,
+  })
+
+export const browserApolloClient = () =>
+  createApolloClient({
+    environment: Environment.Browser,
+  })
