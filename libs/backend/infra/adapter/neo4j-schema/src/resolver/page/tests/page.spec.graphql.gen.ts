fdd2e573f2a6419d8c908d90bf325c3297ab87d7
Vladyslav Polishchuk <gm.vpolishchuk@gmail.com>
2025-03-11 07:58:16 +0100

    test: update integration tests to check dependantType resolver

--- libs/backend/infra/adapter/neo4j-schema/src/resolver/page/tests/page.spec.graphql.gen.ts
+++ libs/backend/infra/adapter/neo4j-schema/src/resolver/page/tests/page.spec.graphql.gen.ts
@@ -6,7 +6,7 @@ type GraphQLClientRequestHeaders = RequestOptions['requestHeaders'];
 export type PageResolverPagesQueryVariables = Types.Exact<{ [key: string]: never; }>;
 
 
-export type PageResolverPagesQuery = { pages: Array<{ id: string, name: string, slug: string, elements: Array<{ id: string }>, rootElement: { id: string } }> };
+export type PageResolverPagesQuery = { pages: Array<{ id: string, name: string, slug: string, elements: Array<{ id: string }>, rootElement: { id: string }, dependantTypes: Array<{ __typename: 'ArrayType', id: string } | { __typename: 'EnumType', id: string } | { __typename: 'UnionType', id: string } | {}> }> };
 
 
 export const PageResolverPagesDocument = gql`
@@ -21,6 +21,20 @@ export const PageResolverPagesDocument = gql`
       id
     }
     slug
+    dependantTypes {
+      ... on EnumType {
+        __typename
+        id
+      }
+      ... on ArrayType {
+        __typename
+        id
+      }
+      ... on UnionType {
+        __typename
+        id
+      }
+    }
   }
 }
     `;
