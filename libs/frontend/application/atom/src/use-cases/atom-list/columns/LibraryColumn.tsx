c212f59125e9b5660dac919b9dfa8e36606fef16
Vladyslav Polishchuk <gm.vpolishchuk@gmail.com>
2025-03-22 22:13:58 +0100

    fix: required parents validation

--- libs/frontend/application/atom/src/use-cases/atom-list/columns/LibraryColumn.tsx
+++
@@ -1,9 +0,0 @@
-import type { IAtomModel } from '@codelab/frontend/abstract/domain'
-
-import { Tag } from 'antd'
-
-export const LibraryColumn = ({ library }: Pick<IAtomModel, 'library'>) => (
-  <Tag color={library.color} key={library.name}>
-    {library.name}
-  </Tag>
-)
