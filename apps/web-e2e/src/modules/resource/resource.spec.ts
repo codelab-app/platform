07abcc82f7b750a5ff11bc7b1179c56db8b85fb8
assim habeek <assimhabe@gmail.com>
2025-04-02 18:07:01 +0100

    fix: resource specs

--- apps/web-e2e/src/modules/resource/resource.spec.ts
+++ apps/web-e2e/src/modules/resource/resource.spec.ts
@@ -1,4 +1,3 @@
-import { RoutePaths } from '@codelab/frontend/abstract/application'
 import { expect } from '@playwright/test'
 
 import { globalBeforeAll } from '../../setup/before-all'
@@ -20,9 +19,6 @@ test('should be able to create resource', async ({ resourcePage: page }) => {
 
   await page.createResource()
 
-  await page.expectNotificationSuccess('Resource created successfully')
-  await page.waitForPage(RoutePaths.Resource.base())
-
   await expect(page.getTree().getTreeItem()).toHaveCount(1)
   await expect(page.getByExactText(page.resourceName)).toBeVisible()
 })
@@ -31,9 +27,6 @@ test('should be able to update resource name', async ({
   resourcePage: page,
 }) => {
   await page.updateResource()
-
-  await page.expectNotificationSuccess('Resource updated successfully')
-  await page.waitForPage(RoutePaths.Resource.base())
 
   await expect(page.getByExactText(page.resourceName)).toBeHidden()
   await expect(page.getByExactText(page.updatedResourceName)).toBeVisible()
