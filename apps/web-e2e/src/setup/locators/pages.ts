d02586157fb952f2d8c9199e434946b24ec00b5d
assim habeek <assimhabe@gmail.com>
2025-04-03 18:45:04 +0100

    fix: increase test timeout

--- apps/web-e2e/src/setup/locators/pages.ts
+++ apps/web-e2e/src/setup/locators/pages.ts
@@ -153,14 +153,12 @@ export class BasePage {
 
       // It's hoisted outside so we don't scope it to the previous locator
       // Then click on the specific option with matching text
-      const option = await this.page
+      await this.page
         .locator(
           '.ant-select-dropdown:not(.ant-select-dropdown-hidden) .ant-select-item.ant-select-item-option',
         )
         .filter({ hasText: value })
-
-      await option.waitFor({ state: 'visible' })
-      await option.click()
+        .click()
 
       if (options?.waitForAutosave) {
         await this.waitForProgressBar()
