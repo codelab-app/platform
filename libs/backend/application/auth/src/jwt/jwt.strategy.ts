bee2ceb330b642181aac88f1f27820e63840307b
Webber Wang <webber@codelab.app>
2025-04-05 16:15:31 +0900

    ci: fix cache

--- libs/backend/application/auth/src/jwt/jwt.strategy.ts
+++ libs/backend/application/auth/src/jwt/jwt.strategy.ts
@@ -24,10 +24,6 @@ interface IPassportStrategy {
 
 export const JWT_STRATEGY = 'JWT_STRATEGY'
 
-// Track all cached keys
-const CACHE_PREFIX = 'userInfo:'
-const cachedKeys = new Set<string>()
-
 @Injectable()
 export class JwtStrategy
   extends PassportStrategy(Strategy)
@@ -102,7 +98,7 @@ export class JwtStrategy
    * https://community.auth0.com/t/too-many-requests-when-calling-userinfo/26685
    */
   private async getCachedUserInfo(auth0Id: string, token: string) {
-    const cacheKey = `${CACHE_PREFIX}${auth0Id}`
+    const cacheKey = `userInfo:${auth0Id}`
 
     this.logger.debug(`Getting cached user info for key: ${cacheKey}`)
 
@@ -128,35 +124,7 @@ export class JwtStrategy
     // one drawback that the user picture and nickname may be out of date for a bit
     await this.cacheManager.set(cacheKey, userInfo, 60 * 60 * 1000)
 
-    // Store serializable version of userInfo
-    await this.cacheManager.set(
-      cacheKey,
-      JSON.parse(JSON.stringify(userInfo)),
-      3600,
-    )
-
     return userInfo
-  }
-
-  /**
-   * Retrieves all cached user info
-   * Note: This only returns items cached since the application started
-   */
-  async getAllCachedUserInfo() {
-    const result = new Map<string, UserInfoResponse>()
-
-    for (const key of cachedKeys) {
-      const value = await this.cacheManager.get<UserInfoResponse>(key)
-
-      if (value) {
-        // Extract the auth0Id from the cache key
-        const auth0Id = key.replace(CACHE_PREFIX, '')
-
-        result.set(auth0Id, value)
-      }
-    }
-
-    return result
   }
 
   private readonly logger = new Logger(JwtStrategy.name)
