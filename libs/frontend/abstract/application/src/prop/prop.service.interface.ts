f9d910a80adc76a8fd78cefda4d09d7c5eb63026
Webber Wang <webber@codelab.app>
2025-04-13 16:53:03 +0800

    fix: form updating

--- libs/frontend/abstract/application/src/prop/prop.service.interface.ts
+++ libs/frontend/abstract/application/src/prop/prop.service.interface.ts
@@ -11,7 +11,10 @@ import type { ICrudService } from '../services'
 
 export interface IPropService
   extends ICrudService<IRef, IPropCreateData, IPropUpdateData> {
-  reset(props: IPropDto): void
+  /**
+   * Since used in auto-save form, we must not revalidate cache anywhere in the call tree, otherwise form may re-render
+   */
+  reset(props: IPropDto): Promise<IRef>
   updateWithDefaultValuesApplied(
     props: IPropModel,
     data: IUpdatePropDataWithDefaultValues,
