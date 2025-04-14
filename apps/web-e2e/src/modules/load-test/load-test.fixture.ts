90487fda891ba92b0db7f1bc9008ab2074b4975c
assim habeek <33001357+assimhabeek@users.noreply.github.com>
2025-04-14 17:36:37 +0100

    wip: fix tree expand (#3680)

--- apps/web-e2e/src/modules/load-test/load-test.fixture.ts
+++ apps/web-e2e/src/modules/load-test/load-test.fixture.ts
@@ -41,7 +41,7 @@ export class LoadTestPage extends BuilderPage {
 
   private async checkBuilderPage() {
     await this.checkRootElementExists()
-    await this.page.locator('span[aria-label="plus-square"]').click()
+    await this.page.getByLabel('plus-square').click()
 
     await expect(this.page.getByRole('treeitem')).toHaveCount(
       // +1 for the root ReactFragment element
@@ -53,7 +53,7 @@ export class LoadTestPage extends BuilderPage {
     ).toHaveCount(E2E_ATOM_TYPES.length)
 
     // close it for next check
-    await this.page.locator('span[aria-label="minus-square"]').click()
+    await this.page.getByLabel('minus-square').click()
   }
 
   private async checkPageListPage(appName: string) {
