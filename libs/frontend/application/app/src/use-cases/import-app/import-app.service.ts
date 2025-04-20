348d56b584a0c6fcdfa487501182a7451c08bf25
Vladyslav Polishchuk <gm.vpolishchuk@gmail.com>
2025-04-20 17:52:39 +0200

    refactor: use sockets for app import to avoid timeout error

--- libs/frontend/application/app/src/use-cases/import-app/import-app.service.ts
+++ libs/frontend/application/app/src/use-cases/import-app/import-app.service.ts
@@ -1,19 +1,14 @@
 'use server'
 
-import { invalidateAppListQuery } from '@codelab/frontend-domain-app/repositories'
 import { getEnv } from '@codelab/shared/config/env'
 import { serverFetchWithAuth } from '@codelab/shared/infra/fetch-server'
 
-export const importAppService = async (formData: FormData) => {
+export const queueImportAppAction = async (formData: FormData) => {
   const response = await serverFetchWithAuth(getEnv().endpoint.app.import, {
     body: formData,
     headers: { ContentType: 'multipart/form-data' },
     method: 'POST',
   })
 
-  const data = await response.json()
-
-  await invalidateAppListQuery()
-
-  return data
+  return response.json()
 }
