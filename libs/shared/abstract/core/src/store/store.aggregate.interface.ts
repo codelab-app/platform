c2517eb598fe4d768b350252f6b3f027eb15c3fb
Vladyslav Polishchuk <gm.vpolishchuk@gmail.com>
2025-04-21 20:40:27 +0200

    fix: remove duplicated "api" data in export file

--- libs/shared/abstract/core/src/store/store.aggregate.interface.ts
+++ libs/shared/abstract/core/src/store/store.aggregate.interface.ts
@@ -2,12 +2,12 @@ import { type Static, Type } from '@sinclair/typebox'
 
 import { ActionSchema } from '../action'
 import { ApiAggregateSchema } from '../type'
-import { StoreSchema } from './store.model.interface'
+import { StoreDtoSchema } from './store.dto.interface'
 
 export const StoreAggregateSchema = Type.Object({
   actions: Type.Array(ActionSchema),
   api: ApiAggregateSchema,
-  store: StoreSchema,
+  store: Type.Omit(StoreDtoSchema, ['component', 'page', 'actions']),
 })
 
 export type IStoreAggregate = Static<typeof StoreAggregateSchema>
