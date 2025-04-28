6f0c148cfe25e3c29f5c1a9fd0ff1c03bdb6f96c
Webber Wang <webber@codelab.app>
2025-04-28 17:05:32 +0800

    refactor: workspace lib

--- libs/tools/plugin/src/buildable-vite/schema.d.ts
+++
@@ -1,3 +0,0 @@
-export interface BuildableViteGeneratorSchema {
-  name: string
-}
--- scripts/nx/cli.sh
+++ scripts/nx/cli.sh
@@ -5,4 +5,4 @@ set -x
 # Extract the arguments after "pnpm cli"
 args=${@#pnpm cli}
 
-node apps/cli/dist/main.js $args
+node dist/apps/cli/main.js $args
