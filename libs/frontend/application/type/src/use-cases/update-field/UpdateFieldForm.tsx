462014c1496b2691e4cf217b66c7c2a15bb1df7b
assim habeek <assimhabe@gmail.com>
2025-05-20 14:51:17 +0100

    fix: field default value validation problem

--- libs/frontend/application/type/src/use-cases/update-field/UpdateFieldForm.tsx
+++ libs/frontend/application/type/src/use-cases/update-field/UpdateFieldForm.tsx
@@ -1,9 +1,15 @@
 'use client'
 import type {
-  IFieldModel,
-  IInterfaceTypeModel,
+  type IFieldModel,
+  type IInterfaceTypeModel,
+  ITypeModel,
+  JsonSchema,
 } from '@codelab/frontend/abstract/domain'
-import type { IFieldUpdateData } from '@codelab/shared/abstract/core'
+import type {
+  IFieldUpdateData,
+  IValidationRules,
+} from '@codelab/shared/abstract/core'
+import type { Nullable, Nullish } from '@codelab/shared/abstract/types'
 
 import { type IFormController, UiKey } from '@codelab/frontend/abstract/types'
 import {
@@ -19,9 +25,12 @@ import {
 } from '@codelab/frontend-presentation-components-form'
 import { DisplayIf } from '@codelab/frontend-presentation-view/components/conditionalView'
 import { PrimitiveTypeKind } from '@codelab/shared/infra/gqlgen'
+import { observer } from 'mobx-react-lite'
+import { useMemo, useState } from 'react'
+import { useAsync } from 'react-use'
 import { AutoFields } from 'uniforms-antd'
 
-import { useFieldService } from '../../services'
+import { useFieldService, useTypeService } from '../../services'
 import {
   canSetDefaultValue,
   createFieldSchema,
@@ -39,159 +48,197 @@ export interface UpdateFieldFormProps extends IFormController {
   field: IFieldModel
 }
 
-export const UpdateFieldForm = ({
-  field,
-  onSubmitSuccess,
-  showFormControl = true,
-  submitRef,
-}: UpdateFieldFormProps) => {
-  const fieldService = useFieldService()
-  const { typeDomainService } = useDomainStore()
-  const fieldSchema = useFieldSchema(createFieldSchema, field)
-  const type = field.type.current as IInterfaceTypeModel
-
-  const onSubmit = async (input: IFieldUpdateData) => {
-    console.log('onSubmit', input)
-
-    const validationRules = filterValidationRules(
-      input.validationRules,
-      typeDomainService.primitiveKind(input.fieldType),
-    )
-
-    const updatedField = { ...input, validationRules }
-
-    const interfaceType = typeDomainService.type(
-      input.interfaceTypeId,
-    ) as IInterfaceTypeModel
-
-    const firstField = interfaceType.fields.find(
-      ({ prevSibling }) => !prevSibling,
-    )
-
-    if (updatedField.prevSibling?.id) {
-      await fieldService.moveFieldAsNextSibling({
-        field: updatedField,
-        targetFieldId: updatedField.prevSibling.id,
-      })
-    } else if (firstField) {
-      await fieldService.moveFieldAsPrevSibling({
-        field: updatedField,
-        targetFieldId: firstField.id,
-      })
-    }
-
-    return fieldService.update({ ...input, validationRules })
-  }
-
-  /**
-   * Each type has a different way of creating default values based on validation rules
-   */
-  const defaultValues = type.toJsonSchema({
+const getDefaultValuesSchema = (
+  type: ITypeModel,
+  validationRules: Nullish<IValidationRules>,
+) =>
+  type.toJsonSchema({
     uniformSchema: uniformSchemaFactory,
-    validationRules: field.validationRules,
+    validationRules,
   })
 
-  const schema = {
-    ...fieldSchema,
-    properties: { ...fieldSchema.properties, defaultValues },
-  }
+export const UpdateFieldForm = observer(
+  ({
+    field,
+    onSubmitSuccess,
+    showFormControl = true,
+    submitRef,
+  }: UpdateFieldFormProps) => {
+    const fieldService = useFieldService()
+    const typeService = useTypeService()
+    const { typeDomainService } = useDomainStore()
+    const fieldSchema = useFieldSchema(createFieldSchema, field)
 
-  return (
-    <Form<IFieldUpdateData>
-      errorMessage="Error while updating field"
-      model={{
-        defaultValues: field.defaultValues,
-        description: field.description,
-        fieldType: field.type.id,
-        id: field.id,
-        interfaceTypeId: field.api.id,
-        key: field.key,
-        name: field.name,
-        prevSibling: field.prevSibling,
-        validationRules: field.validationRules,
-      }}
-      modelTransform={(mode, model) => {
-        // This automatically sets the `defaultValue` to be nullable for types
-        // where we don't set a default value like ReactNodeType, InterfaceType
-        if (
-          mode === 'form' &&
-          model.fieldType &&
-          !canSetDefaultValue(typeDomainService, model.fieldType)
-        ) {
-          return {
-            ...model,
-            validationRules: {
-              general: {
-                nullable: true,
+    const [defaultValuesSchema, setDefaultValuesSchema] =
+      useState<Nullable<JsonSchema>>(null)
+
+    const { loading } = useAsync(async () => {
+      if (!typeDomainService.types.has(field.type.id)) {
+        await typeService.getAll([field.type.id])
+      }
+
+      setDefaultValuesSchema(
+        getDefaultValuesSchema(field.type.current, field.validationRules),
+      )
+    }, [])
+
+    const onSubmit = async (input: IFieldUpdateData) => {
+      const validationRules = filterValidationRules(
+        input.validationRules,
+        typeDomainService.primitiveKind(input.fieldType),
+      )
+
+      const updatedField = { ...input, validationRules }
+
+      const interfaceType = typeDomainService.type(
+        input.interfaceTypeId,
+      ) as IInterfaceTypeModel
+
+      const firstField = interfaceType.fields.find(
+        ({ prevSibling }) => !prevSibling,
+      )
+
+      if (updatedField.prevSibling?.id) {
+        await fieldService.moveFieldAsNextSibling({
+          field: updatedField,
+          targetFieldId: updatedField.prevSibling.id,
+        })
+      } else if (firstField) {
+        await fieldService.moveFieldAsPrevSibling({
+          field: updatedField,
+          targetFieldId: firstField.id,
+        })
+      }
+
+      return fieldService.update({ ...input, validationRules })
+    }
+
+    const schema = useMemo(
+      () => ({
+        ...fieldSchema,
+        properties: {
+          ...fieldSchema.properties,
+          defaultValues: defaultValuesSchema,
+        },
+      }),
+      [defaultValuesSchema],
+    )
+
+    if (loading) {
+      return null
+    }
+
+    return (
+      <Form<IFieldUpdateData>
+        errorMessage="Error while updating field"
+        model={{
+          defaultValues: field.defaultValues,
+          description: field.description,
+          fieldType: field.type.id,
+          id: field.id,
+          interfaceTypeId: field.api.id,
+          key: field.key,
+          name: field.name,
+          prevSibling: field.prevSibling,
+          validationRules: field.validationRules,
+        }}
+        modelTransform={(mode, model) => {
+          // This automatically sets the `defaultValue` to be nullable for types
+          // where we don't set a default value like ReactNodeType, InterfaceType
+          if (
+            mode === 'form' &&
+            model.fieldType &&
+            !canSetDefaultValue(typeDomainService, model.fieldType)
+          ) {
+            return {
+              ...model,
+              validationRules: {
+                general: {
+                  nullable: true,
+                },
               },
-            },
+            }
           }
-        }
 
-        return model
-      }}
-      onSubmit={onSubmit}
-      onSubmitSuccess={onSubmitSuccess}
-      schema={schema}
-      submitRef={submitRef}
-      successMessage="Field updated successfully"
-      uiKey={UiKey.FieldFormUpdate}
-    >
-      <AutoFields fields={['id', 'key', 'name', 'description']} />
-      <TypeSelect label="Type" name="fieldType" />
-      <SelectFieldSibling field={field} name="prevSibling" />
+          return model
+        }}
+        onChangeModel={(model) => {
+          if (model.fieldType) {
+            const newFieldType = typeDomainService.type(model.fieldType)
 
-      <DisplayIfField<IFieldUpdateData>
-        condition={({ model }) =>
-          !isBoolean(typeDomainService, model.fieldType) &&
-          canSetDefaultValue(typeDomainService, model.fieldType)
-        }
+            setDefaultValuesSchema(
+              getDefaultValuesSchema(newFieldType, model.validationRules),
+            )
+          }
+        }}
+        onSubmit={onSubmit}
+        onSubmitSuccess={onSubmitSuccess}
+        schema={schema}
+        submitRef={submitRef}
+        successMessage="Field updated successfully"
+        uiKey={UiKey.FieldFormUpdate}
       >
-        <AutoFields fields={['validationRules.general']} />
-      </DisplayIfField>
-      <DisplayIfField<IFieldUpdateData>
-        condition={({ model }) =>
-          isPrimitive(typeDomainService, model.fieldType)
-        }
-      >
+        <AutoFields fields={['id', 'key', 'name', 'description']} />
+        <TypeSelect label="Type" name="fieldType" />
+        <SelectFieldSibling field={field} name="prevSibling" />
+
         <DisplayIfField<IFieldUpdateData>
           condition={({ model }) =>
-            isString(typeDomainService, model.fieldType)
+            !isBoolean(typeDomainService, model.fieldType) &&
+            canSetDefaultValue(typeDomainService, model.fieldType)
           }
         >
-          <AutoFields
-            fields={[`validationRules.${PrimitiveTypeKind.String}`]}
-          />
+          <AutoFields fields={['validationRules.general']} />
         </DisplayIfField>
         <DisplayIfField<IFieldUpdateData>
           condition={({ model }) =>
-            isInteger(typeDomainService, model.fieldType)
+            isPrimitive(typeDomainService, model.fieldType)
           }
         >
-          <AutoFields
-            fields={[`validationRules.${PrimitiveTypeKind.Integer}`]}
-          />
+          <DisplayIfField<IFieldUpdateData>
+            condition={({ model }) =>
+              isString(typeDomainService, model.fieldType)
+            }
+          >
+            <AutoFields
+              fields={[`validationRules.${PrimitiveTypeKind.String}`]}
+            />
+          </DisplayIfField>
+          <DisplayIfField<IFieldUpdateData>
+            condition={({ model }) =>
+              isInteger(typeDomainService, model.fieldType)
+            }
+          >
+            <AutoFields
+              fields={[`validationRules.${PrimitiveTypeKind.Integer}`]}
+            />
+          </DisplayIfField>
+          <DisplayIfField<IFieldUpdateData>
+            condition={({ model }) =>
+              isFloat(typeDomainService, model.fieldType)
+            }
+          >
+            <AutoFields
+              fields={[`validationRules.${PrimitiveTypeKind.Number}`]}
+            />
+          </DisplayIfField>
         </DisplayIfField>
+
         <DisplayIfField<IFieldUpdateData>
-          condition={({ model }) => isFloat(typeDomainService, model.fieldType)}
+          condition={({ model }) =>
+            canSetDefaultValue(typeDomainService, model.fieldType)
+          }
         >
-          <AutoFields
-            fields={[`validationRules.${PrimitiveTypeKind.Number}`]}
-          />
+          <SelectDefaultValue />
         </DisplayIfField>
-      </DisplayIfField>
 
-      <DisplayIfField<IFieldUpdateData>
-        condition={({ model }) =>
-          canSetDefaultValue(typeDomainService, model.fieldType)
-        }
-      >
-        <SelectDefaultValue />
-      </DisplayIfField>
-
-      <DisplayIf condition={showFormControl}>
-        <FormController onCancel={onSubmitSuccess} submitLabel="Update Field" />
-      </DisplayIf>
-    </Form>
-  )
-}
+        <DisplayIf condition={showFormControl}>
+          <FormController
+            onCancel={onSubmitSuccess}
+            submitLabel="Update Field"
+          />
+        </DisplayIf>
+      </Form>
+    )
+  },
+)
