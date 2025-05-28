1771b130cdcaca83965838751ca10af875b5e1b5
assim habeek <assimhabe@gmail.com>
2025-05-28 13:18:13 +0100

    fix: codemirror input

--- libs/frontend/presentation/components/form/src/fields/CodeMirrorField.tsx
+++ libs/frontend/presentation/components/form/src/fields/CodeMirrorField.tsx
@@ -1,24 +1,28 @@
 'use client'
 
+import type { ICodeMirrorLanguage } from '@codelab/shared/abstract/core'
 import type { Completion } from '@codemirror/autocomplete'
-import type { TextFieldProps } from 'uniforms-antd'
 
 import { CodeMirrorEditor } from '@codelab/frontend-presentation-components-codemirror'
 import { connectField } from 'uniforms'
+import { type TextFieldProps, wrapField } from 'uniforms-antd'
 
 export type CodeMirrorFieldProps = TextFieldProps & {
   customOptions?: Array<Completion>
+  language: ICodeMirrorLanguage
 }
 
 export const CodeMirrorField = connectField<CodeMirrorFieldProps>(
   (props) => {
-    return (
+    return wrapField(
+      props,
       <CodeMirrorEditor
         {...props}
         height="auto"
         maxHeight="150px"
+        title={props.label?.toString()}
         width="100%"
-      />
+      />,
     )
   },
   {
