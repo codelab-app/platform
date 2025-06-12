b091e51f5962f377aae3734f070a0c25f9857e76
assim habeek <assimhabe@gmail.com>
2025-06-12 12:35:50 +0100

    wip: union type field

---
+++ libs/frontend/presentation/components/interface-form/src/interface-form/fields/union-type-field/UnionTypeField.tsx
@@ -0,0 +1,47 @@
+'use client'
+
+import type { ITypeKind } from '@codelab/shared/abstract/core'
+import type { ObjectLike } from '@codelab/shared/abstract/types'
+import type { GuaranteedProps } from 'uniforms'
+
+import {
+  type IUnionTypeModel,
+  PropKind,
+  type TypedProp,
+} from '@codelab/frontend/abstract/domain'
+import { Form } from 'antd'
+import { joinName, useField } from 'uniforms'
+import { AutoField } from 'uniforms-antd'
+
+type UnionTypeFieldProps = GuaranteedProps<TypedProp> & {
+  unionType: IUnionTypeModel
+}
+
+export const UnionTypeField = (props: UnionTypeFieldProps) => {
+  const { name } = props
+  const typeFieldName = joinName(name, 'type')
+  const kindFieldName = joinName(name, 'kind')
+  const [field, context] = useField<UnionTypeFieldProps, ITypeKind>(name, props)
+  const [kindField] = useField<ObjectLike, ITypeKind>(kindFieldName, {})
+  const activeValueFieldName = joinName(name, kindField.value)
+  const unionType = field.unionType
+
+  const typeToKind = unionType.typesOfUnionType
+    .map((type) => ({ [type.current.id]: type.current.kind }))
+    .reduce((all, current) => ({ ...all, ...current }), {})
+
+  const onTypeChange = (value: string) => {
+    context.onChange(name, {
+      kind: typeToKind[value],
+      propKind: PropKind.UnionTypeProp,
+      type: value,
+    })
+  }
+
+  return (
+    <Form.Item label={field.label}>
+      <AutoField name={typeFieldName} onChange={onTypeChange} />
+      {kindField.value && <AutoField name={activeValueFieldName} />}
+    </Form.Item>
+  )
+}
