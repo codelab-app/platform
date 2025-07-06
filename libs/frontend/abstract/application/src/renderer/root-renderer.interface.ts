f9bb8725f2b93f478bf2acce3a251e846dd831a9
Webber Wang <webber@codelab.app>
2025-07-06 11:07:33 +0800

    refactor: root renderer

--- libs/frontend/abstract/application/src/renderer/root-renderer.interface.ts
+++ libs/frontend/abstract/application/src/renderer/root-renderer.interface.ts
@@ -1,6 +1,12 @@
 import type { IRendererModel } from './renderer.model.interface'
 
-export type IRootRenderer = React.JSXElementConstructor<{
-  ref?: React.RefObject<HTMLDivElement | null>
+export interface IRootRendererProps {
+  containerStyle?: React.CSSProperties
   renderer: IRendererModel
-}>
+}
+
+export type IRootRenderer = React.JSXElementConstructor<
+  {
+    ref?: React.RefObject<HTMLDivElement | null>
+  } & IRootRendererProps
+>
