dea470d0c539965684b5c6bc67e06ca09f278f05
Webber Wang <webber@codelab.app>
2025-07-12 14:12:52 +0800

    fix(libs-frontend-domain-app): add auth0 client mock to fix test failure
    - Mock @codelab/shared-infra-auth0/client in jest.mocks.ts
    - Prevents import of Next.js server-side APIs in test environment
    - Fixes 'Request is not defined' error introduced in commit af4d50a2ac

--- libs/shared/infra/fetch-server/src/fetch-with-auth.server.ts
+++ libs/shared/infra/fetch-server/src/fetch-with-auth.server.ts
@@ -1,13 +1,40 @@
-'use server'
-
-import { fetchWithAuth } from '@codelab/shared-infra-fetch'
+import { auth0Instance } from '@codelab/shared-infra-auth0/client'
 
 /**
- * Server-side fetch with authentication wrapper
+ * Client-side fetch with authentication.
+ * If Authorization header is provided in options.headers, it will be used as-is.
+ * Otherwise, it will get the user session token.
+ *
+ * GraphQL requests are automatically batched within a 20ms window.
  */
 export const serverFetchWithAuth = async (
   endpoint: string,
-  init: RequestInit,
+  options: RequestInit = {},
 ): Promise<Response> => {
-  return fetchWithAuth(endpoint, init)
+  const headers = new Headers(options.headers)
+
+  // If no Authorization header provided, get it from user session
+  if (!headers.get('Authorization')) {
+    const session = await auth0Instance.getSession()
+
+    if (!session?.tokenSet.accessToken) {
+      throw new Error('No user session found')
+    }
+
+    headers.set('Authorization', `Bearer ${session.tokenSet.accessToken}`)
+  }
+
+  const response = await batchFetch(endpoint, {
+    ...options,
+    headers,
+  })
+
+  if (!response.ok) {
+    const errorText = await response.text()
+
+    console.error(errorText)
+    throw new Error(errorText)
+  }
+
+  return response
 }
--- libs/shared/infra/fetch/src/index.ts
+++ libs/shared/infra/fetch/src/index.ts
@@ -1,3 +1,1 @@
-export * from './fetch-with-auth'
 export * from './gql-request'
-export { batchFetch, configureBatchFetcher, resetBatchFetcher } from './batch-fetch'
