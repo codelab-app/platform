5568b3f02057c155df13099a72fdf070fe18f183
assim habeek <assimhabe@gmail.com>
2025-07-14 08:23:12 +0100

    fix: build error

--- libs/shared/abstract/core/src/action/action.data.interface.ts
+++ libs/shared/abstract/core/src/action/action.data.interface.ts
@@ -1,4 +1,4 @@
-import type { ObjectLike } from '@codelab/shared/abstract/types'
+import type { ObjectLike } from '@codelab/shared-abstract-types'
 
 import type { IRef } from '../model'
 import type { IResourceFetchConfig } from '../resource'
