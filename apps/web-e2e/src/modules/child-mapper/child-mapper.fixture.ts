236bc8a18a5461ede4e2da0c94d980d6df01f256
assim habeek <assimhabe@gmail.com>
2025-08-04 15:42:32 +0100

    fix: child mapper e2e specs

--- apps/web-e2e/src/modules/child-mapper/child-mapper.fixture.ts
+++ apps/web-e2e/src/modules/child-mapper/child-mapper.fixture.ts
@@ -180,9 +180,9 @@ export class ChildMapperPage extends BuilderPage {
       const updateElementForm = this.getUpdateElementForm()
 
       await this.selectTreeElement(pageRowElement)
-      await updateElementForm
-        .locator('.ant-collapse-header', { hasText: 'Child Mapper' })
-        .click()
+      // await updateElementForm
+      //   .locator('.ant-collapse-header', { hasText: 'Child Mapper' })
+      //   .click()
       await this.fillInputText({ label: 'Prop Key' }, '{{false}}', {
         locator: updateElementForm,
         waitForAutosave: true,
